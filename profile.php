4d4b07138747e959ed7cbc7de9245baa7c1ac03c
Matt Mullenweg <matt@git.wordpress.org>
2004-12-29 20:10:25 +0000

    Better handling if nothing's there
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@29 9866e705-20ec-0310-96e7-cbb4277adcfb

--- profile.php
+++ profile.php
@@ -10,29 +10,24 @@ if ( !$user )
 
 $ts = strtotime( $user->user_regdate );
 
-/*
-$posts = $bbdb->get_results("SELECT DISTINCT $bbdb->posts.topic_id, $bbdb->posts.forum_id, topic_title, MAX(post_time) AS m, UNIX_TIMESTAMP(MAX(post_time))
-AS posted FROM $bbdb->posts, $bbdb->topics WHERE $bbdb->posts.poster_id=$user->user_id AND
-$bbdb->posts.topic_id=$bbdb->topics.topic_id GROUP BY $bbdb->posts.topic_id ORDER BY m desc LIMIT 25");
-*/
-
 $posts = $bbdb->get_results("SELECT * FROM $bbdb->posts WHERE poster_id = $user_id GROUP BY topic_id ORDER BY post_time DESC LIMIT 25");
 $threads = $bbdb->get_results("SELECT * FROM $bbdb->topics WHERE topic_poster = $user_id ORDER BY topic_time DESC LIMIT 25");
 
 // Cache topic names
-foreach ($posts as $post) :
-	$topics[] = $post->topic_id;
-endforeach;
-foreach ($threads as $thread) :
-	$topics[] = $thread->topic_id;
-endforeach;
-$topic_ids = join(',', $topics);
+if ( $posts ) :
+	foreach ($posts as $post)
+		$topics[] = $post->topic_id;
+endif;
+if ( $threads ) :
+	foreach ($threads as $thread)
+		$topics[] = $thread->topic_id;
+endif;
 
 if ( $posts || $threads ) :
+	$topic_ids = join(',', $topics);
 	$topics = $bbdb->get_results("SELECT * FROM $bbdb->topics WHERE topic_id IN ($topic_ids)");
-	foreach ($topics as $topic) :
+	foreach ($topics as $topic)
 		$topic_cache[$topic->topic_id] = $topic;
-	endforeach;
 endif;
 
 bb_add_filter('post_time', 'strtotime');
