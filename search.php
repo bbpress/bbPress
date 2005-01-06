31c5ff037830f9b6176d86e7f3bb35e9184d5e1f
Matt Mullenweg <matt@git.wordpress.org>
2005-01-06 02:40:32 +0000

    Don't show deleted things in searches.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@38 9866e705-20ec-0310-96e7-cbb4277adcfb

--- search.php
+++ search.php
@@ -6,14 +6,14 @@ $likeit = preg_replace('/\s+/', '%', $q);
 
 if ( !empty( $q ) ) :
 
-$titles = $bbdb->get_results("SELECT * FROM $bbdb->topics JOIN $bbdb->posts ON topic_last_post_id = post_id WHERE LOWER(topic_title) LIKE ('%$likeit%') ORDER BY post_time DESC LIMIT 5");
+$titles = $bbdb->get_results("SELECT * FROM $bbdb->topics JOIN $bbdb->posts ON topic_last_post_id = post_id WHERE LOWER(topic_title) LIKE ('%$likeit%') AND topic_status = 0 ORDER BY post_time DESC LIMIT 5");
 
 $recent = $bbdb->get_results("SELECT * FROM $bbdb->posts JOIN $bbdb->topics ON
-topic_last_post_id = post_id WHERE LOWER(post_text) LIKE ('%$likeit%') ORDER BY post_time DESC LIMIT 5");
+topic_last_post_id = post_id WHERE LOWER(post_text) LIKE ('%$likeit%') AND post_status = 0 ORDER BY post_time DESC LIMIT 5");
 
 $relevant = $bbdb->get_results("SELECT $bbdb->posts.forum_id, $bbdb->posts.topic_id, post_text, topic_title, UNIX_TIMESTAMP(post_time)
 AS posttime, post_id FROM $bbdb->posts RIGHT JOIN $bbdb->topics ON topic_last_post_id = post_id
-WHERE MATCH(post_text) AGAINST ('$q') LIMIT 5");
+WHERE MATCH(post_text) AGAINST ('$q') AND post_status = 0 LIMIT 5");
 
 bb_do_action('do_search', $q);
 
