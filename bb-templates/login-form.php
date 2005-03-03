407257e165a9fc82115a5091d927c78a205d1d3b
Matt Mullenweg <matt@git.wordpress.org>
2005-03-03 08:49:15 +0000

    No name in XHTML 1.1.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@63 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-templates/login-form.php
+++ bb-templates/login-form.php
@@ -1,4 +1,4 @@
-<form name="login" method="post" action="<?php option('uri'); ?>bb-login.php">
+<form class="login" method="post" action="<?php option('uri'); ?>bb-login.php">
 <p> <a href="<?php option('uri'); ?>register.php">Register</a> or login:<br />
   <label>Username: 
   <input name="username" type="text" id="username" size="15" maxlength="40" value="<?php echo bb_specialchars($_COOKIE[ $bb->usercookie ], 1); ?>" />
