a485db1c3a8fdd33d9a218c7dee903affece3b63
Matt Mullenweg <matt@git.wordpress.org>
2005-04-24 19:09:25 +0000

    Slim up form
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@94 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-templates/tag-form.php
+++ bb-templates/tag-form.php
@@ -1,8 +1,8 @@
 
 <form method="post" action="<?php option('uri'); ?>tag-add.php">
-<fieldset id="addtag">
-Add tag: <input name="tag" type="text" id="tag" size="10" maxlength="30" /> 
+<p>
+<input name="tag" type="text" id="tag" size="10" maxlength="30" /> 
 <input type="hidden" name="id" value="<?php topic_id(); ?>" />
 <input type="submit" name="Submit" value="Add" />
-</fieldset>
+</p>
 </form>
