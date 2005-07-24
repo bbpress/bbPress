7badf7664b4021cff3dfabc757dfe8d0832a20dd
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2005-07-24 23:56:30 +0000

    Addendum to [192].
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@193 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ profile-base.php
@@ -0,0 +1,14 @@
+<?php
+require_once('bb-config.php');
+
+if ( !is_bb_profile() ) {
+	$sendto = get_profile_tab_link( $current_user->ID, 'edit' );
+	header("Location: $sendto");
+}
+
+$self_template = call_user_func($self);
+
+if ( function_exists($self_template) )
+	require('bb-templates/profile-base.php');
+exit();
+?>
