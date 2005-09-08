48b5281300c1307b7ebae6741c29bc8b15419a67
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2005-09-08 19:10:51 +0000

    view=deleted is now view=all.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@321 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-admin/index.php
+++ bb-admin/index.php
@@ -16,9 +16,9 @@
 <h3>Recently Moderated</h3>
 <ul class="posts">
 <?php if ( $objects = get_recently_moderated_objects() ) : foreach ( $objects as $object ) : if ( 'post' == $object['type'] ) : global $bb_post; $bb_post = $object['data']; ?>
- <li><a href="<?php echo bb_add_query_arg( 'view', 'deleted', get_post_link() ); ?>">Post</a> on <a href="<?php topic_link( $bb_post->topic_id ); ?>"><?php topic_title( $bb_post->topic_id ); ?></a> by <a href="<?php user_profile_link( $bb_post->poster_id ); ?>"><?php post_author(); ?></a>.</li>
+ <li><a href="<?php echo bb_add_query_arg( 'view', 'all', get_post_link() ); ?>">Post</a> on <a href="<?php topic_link( $bb_post->topic_id ); ?>"><?php topic_title( $bb_post->topic_id ); ?></a> by <a href="<?php user_profile_link( $bb_post->poster_id ); ?>"><?php post_author(); ?></a>.</li>
 <?php elseif ( 'topic' == $object['type'] ) : global $topic; $topic = $object['data']; ?>
- <li>Topic titled <a href="<?php echo bb_add_query_arg( 'view', 'deleted', get_topic_link() ); ?>"><?php topic_title(); ?></a> started by <a href="<?php user_profile_link( $topic->topic_poster ); ?>"><?php echo $topic->topic_poster_name; ?></a>.</li>
+ <li>Topic titled <a href="<?php echo bb_add_query_arg( 'view', 'all', get_topic_link() ); ?>"><?php topic_title(); ?></a> started by <a href="<?php user_profile_link( $topic->topic_poster ); ?>"><?php echo $topic->topic_poster_name; ?></a>.</li>
 <?php endif; endforeach; endif; ?>
 </ul>
 </div>
