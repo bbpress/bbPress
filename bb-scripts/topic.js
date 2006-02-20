4bc41d94eb16b550bfc39034bf3697385ffd70b0
Matt Mullenweg <matt@git.wordpress.org>
2006-02-20 20:32:31 +0000

    Always redirect on posting, it makes the URLs right.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@332 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-scripts/topic.js
+++ bb-scripts/topic.js
@@ -62,7 +62,7 @@ function newPostAddIn() {
 addLoadEvent(newTagAddIn);
 addLoadEvent(favoritesAddIn);
 addLoadEvent(resolutionAddIn);
-addLoadEvent(newPostAddIn);
+//addLoadEvent(newPostAddIn);
 
 function getResponseElement(type) {
 	switch (type) {
