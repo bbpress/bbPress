9eef0d72f7f49de740e8b0b06f1edcc257daa459
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2006-10-13 07:05:01 +0000

    first stab at rewrite rules
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@471 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-admin/rewrite-rules.php
+++ bb-admin/rewrite-rules.php
@@ -12,7 +12,23 @@ if ( !bb_current_user_can('manage_options') ) {
 
 <IfModule mod_rewrite.c>
 RewriteEngine On
-RewriteBase <?php echo $bb->path; ?> 
-RewriteRule ^forum/(0-9+)$ <?php echo $bb->path; ?>forum.php?id=$1 [QSA]
-RewriteRule ^topic/(0-9+)$ <?php echo $bb->path; ?>topic.php?id=$1 [QSA]
+RewriteBase <?php echo $bb->path; ?>
+
+RewriteRule ^forum/([0-9]+)/page/([0-9]+)$ <?php echo $bb->path; ?>forum.php?id=$1&page=$2 [L,QSA]
+RewriteRule ^forum/([0-9]+)$ <?php echo $bb->path; ?>forum.php?id=$1 [L,QSA]
+RewriteRule ^topic/([0-9]+)/page/([0-9]+)$ <?php echo $bb->path; ?>topic.php?id=$1&page=$2 [L,QSA]
+RewriteRule ^topic/([0-9]+)$ <?php echo $bb->path; ?>topic.php?id=$1 [L,QSA]
+RewriteRule ^tags/(.+)/page/([0-9]+)$ <?php echo $bb->path; ?>ags.php?tag=$1&page=$2 [L,QSA]
+RewriteRule ^tags/(.+)/?$ <?php echo $bb->path; ?>tags.php?tag=$1 [L,QSA]
+RewriteRule ^tags/? <?php echo $bb->path; ?>tags.php [L,QSA]
+RewriteRule ^profile/([0-9]+)/page/([0-9]+)$ <?php echo $bb->path; ?>profile.php?id=$1&page=$2 [L,QSA]
+RewriteRule ^profile/([0-9]+)/([a-z]+)$ <?php echo $bb->path; ?>profile.php?id=$1&tab=$2 [L,QSA]
+RewriteRule ^profile/([0-9]+)/([a-z]+)/page/([0-9]+)$ <?php echo $bb->path; ?>profile.php?id=$1&tab=$2&page=$3 [L,QSA]
+RewriteRule ^profile/([0-9]+)$ <?php echo $bb->path; ?>profile.php?id=$1 [L,QSA]
+RewriteRule ^view/([a-z-]+)/page/([0-9]+)$ <?php echo $bb->path; ?>view.php?view=$1&page=$2 [L,QSA]
+RewriteRule ^view/([a-z-]+)$ <?php echo $bb->path; ?>view.php?view=$1 [L,QSA]
+RewriteRule ^rss/forum/([0-9]+)$ <?php echo $bb->path; ?>rss.php?forum=$1 [L,QSA]
+RewriteRule ^rss/topic/([0-9]+)$ <?php echo $bb->path; ?>rss.php?topic=$1 [L,QSA]
+RewriteRule ^rss/tags/([a-z]+)$ <?php echo $bb->path; ?>rss.php?tag=$1 [L,QSA]
+RewriteRule ^rss/profile/([0-9]+)$ <?php echo $bb->path; ?>rss.php?profile=$1 [L,QSA]
 </IfModule>
