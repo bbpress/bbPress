02054d225c1309a88d7495a4d38f0167bc6b87b8
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2007-01-23 18:14:01 +0000

    action for admin-base.php before headers sent.  Fixes #554
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@643 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-admin/admin-base.php
+++ bb-admin/admin-base.php
@@ -1,7 +1,11 @@
-<?php require_once('admin.php'); ?>
-<?php bb_get_admin_header(); ?>
+<?php
+require_once('admin.php');
 
-<?php if ( function_exists($bb_admin_page) ) : $bb_admin_page(); else : ?>
+do_action( $bb_admin_page . '_pre_head' );
+
+bb_get_admin_header(); 
+
+if ( function_exists($bb_admin_page) ) : $bb_admin_page(); else : ?>
 
 <p><?php _e('Nothing to see here.'); ?><p>
 
