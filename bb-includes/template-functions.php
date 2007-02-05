fd5782684fef66a0c572ad8d389784f2d7adfdf1
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2007-02-05 21:40:21 +0000

    abstract profile-edit.php stuff into template tags.  Code still ugly.  Fixes #467
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@668 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/template-functions.php
+++ bb-includes/template-functions.php
@@ -1120,10 +1120,15 @@ function profile_pages() {
 	echo apply_filters( 'topic_pages', get_page_number_links( $page, $user->topics_replied + $add ) );
 }
 
-function bb_profile_data() {
+function bb_profile_data( $id = 0 ) {
 	global $user_id;
 
-	$user = bb_get_user( $user_id );
+	if ( !$id )
+		$id =& $user_id;
+
+	if ( !$user = bb_get_user( $id ) )
+		return;
+
 	$reg_time = bb_gmtstrtotime( $user->user_registered );
 	$profile_info_keys = get_profile_info_keys();
 	echo "<dl id='userinfo'>\n";
@@ -1145,6 +1150,97 @@ function bb_profile_base_content() {
 	if ( !is_callable( $self ) )
 		return; // should never happen
 	call_user_func( $self );
+}
+
+function bb_profile_data_form( $id = 0 ) {
+	global $user_id, $bb_current_user;
+
+	if ( !$id )
+		$id =& $user_id;
+
+	if ( !$user = bb_get_user( $id ) )
+		return;
+
+	if ( !bb_current_user_can( 'edit_user', $user->ID ) )
+		return;
+
+	$profile_info_keys = get_profile_info_keys();
+	$required = false;
+?>
+<table id="userinfo">
+<?php if ( is_array($profile_info_keys) ) : foreach ( $profile_info_keys as $key => $label ) : if ( 'user_email' != $key || $bb_current_user->ID == $user->ID ) : ?>
+<tr<?php if ( $label[0] ) { echo ' class="required"'; $label[1] .= '<sup>*</sup>'; $required = true; } ?>>
+  <th scope="row"><?php echo $label[1]; ?>:</th>
+  <td><input name="<?php echo $key; ?>" type="<?php if ( isset($label[2]) ) echo $label[2]; else echo 'text" size="30" maxlength="140'; ?>" id="<?php echo $key; ?>" value="<?php echo wp_specialchars( $user->$key, 1); ?>" /><?php
+if ( isset($$key) && false === $$key) :
+	if ( $key == 'user_email' )
+		_e('<br />There was a problem with your email; please check it.');
+	else
+		_e('<br />The above field is required.');
+endif;
+?></td>
+</tr>
+<?php endif; endforeach; endif; ?>
+</table>
+<?php bb_nonce_field( 'edit-profile_' . $user->ID ); if ( $required ) : ?>
+<p><sup>*</sup><?php _e('These items are <span class="required">required</span>.') ?></p>
+<?php endif;
+}
+
+function bb_profile_admin_form( $id = 0 ) {
+	global $user_id, $bb_current_user, $bb_roles;
+
+	if ( !$id )
+		$id =& $user_id;
+
+	if ( !$user = bb_get_user( $id ) )
+		return;
+
+	if ( !bb_current_user_can( 'edit_user', $user->ID ) )
+		return;
+
+	$profile_admin_keys = get_profile_admin_keys();
+	$assignable_caps = get_assignable_caps();
+	$required = false;
+?>
+<table id="admininfo">
+<tr>
+  <th scope="row"><?php _e('User Type:'); ?></th>
+  <td><select name="role">
+<?php foreach( $bb_roles->role_names as $r => $n ) : if ( 'keymaster' != $r || bb_current_user_can('keep_gate') ) : ?>
+       <option value="<?php echo $r; ?>"<?php if ( array_key_exists($r, $user->capabilities) ) echo ' selected="selected"'; ?>><?php echo $n; ?></option>
+<?php endif; endforeach; ?>
+      </select>
+  </td>
+</tr>
+<tr class="extra-caps-row">
+  <th scope="row"><?php _e('Allow this user to:'); ?></th>
+  <td>
+<?php foreach( $assignable_caps as $cap => $label ) : ?>
+      <label><input name="<?php echo $cap; ?>" value="1" type="checkbox"<?php if ( array_key_exists($cap, $user->capabilities) ) echo ' checked="checked"'; ?> /> <?php echo $label; ?></label><br />
+<?php endforeach; ?>
+  </td>
+</tr>
+<?php if ( is_array($profile_admin_keys) ) : foreach ( $profile_admin_keys as $key => $label ) : ?>
+<tr<?php if ( $label[0] ) { echo ' class="required"'; $label[1] .= '<sup>*</sup>'; $required = true; } ?>>
+  <th scope="row"><?php echo $label[1]; ?>:</th>
+  <td><input name="<?php echo $key; ?>" id="<?php echo $key; ?>" type=<?php
+	if ( isset($label[2]) )
+		echo $label[2];
+	else
+		echo '"text" size="30" maxlength="140" value="' . wp_specialchars( $user->$key, 1 ). '"';
+	?> />
+<?php if ( isset($$key) && false === $$key ) _e('<br />The above field is required.'); ?></td>
+</tr>
+<?php endforeach; endif; ?>
+</table>
+<?php if ( $required ) : ?>
+<p><sup>*</sup><?php _e('These items are <span class="required">required</span>.') ?></p>
+<?php endif; ?>
+<p><?php _e('Inactive users can login and look around but not do anything.
+Blocked users just see a simple error message when they visit the site.</p>
+<p><strong>Note</strong>: Blocking a user does <em>not</em> block any IP addresses.'); ?></p>
+<?php
 }
 
 //TAGS
--- bb-templates/kakumei/profile-edit.php
+++ bb-templates/kakumei/profile-edit.php
@@ -5,68 +5,15 @@
 <form method="post" action="<?php profile_tab_link($user->ID, 'edit');  ?>">
 <fieldset>
 <legend><?php _e('Profile Info'); ?></legend>
-<table width="100%">
-<?php if ( is_array($profile_info_keys) ) : foreach ( $profile_info_keys as $key => $label ) : if ( 'user_email' != $key || $bb_current_user->ID == $user_id ) : ?>
-<tr<?php if ( $label[0] ) { echo ' class="required"'; $label[1] .= '<sup>*</sup>'; $required = true; } ?>>
-  <th scope="row"><?php echo $label[1]; ?>:</th>
-  <td><input name="<?php echo $key; ?>" type="<?php if ( isset($label[2]) ) echo $label[2]; else echo 'text" size="30" maxlength="140'; ?>" id="<?php echo $key; ?>" value="<?php echo wp_specialchars( $user->$key, 1); ?>" /><?php
-if ( isset($$key) && false === $$key) :
-	if ( $key == 'user_email' )
-		_e('<br />There was a problem with your email; please check it.');
-	else
-		_e('<br />The above field is required.');
-endif;
-?></td>
-</tr>
-<?php endif; endforeach; endif; ?>
-</table>
-<?php if ( $required ) : ?>
-<p><sup>*</sup><?php _e('These items are <span class="required">required</span>.') ?></p>
-<?php endif; ?>
+<?php bb_profile_data_form(); ?>
 </fieldset>
 
 <?php do_action('extra_profile_info', $user); ?>
 
-<?php if ( bb_current_user_can('edit_users') ) : $required = false; ?>
+<?php if ( bb_current_user_can( 'edit_users' ) ) : ?>
 <fieldset>
 <legend><?php _e('Administration'); ?></legend>
-<table width="100%">
-<tr>
-  <th scope="row"><?php _e('User Type:'); ?></th>
-  <td><select name="role">
-<?php foreach( $bb_roles->role_names as $r => $n ) : if ( 'keymaster' != $r || bb_current_user_can('keep_gate') ) : ?>
-       <option value="<?php echo $r; ?>"<?php if ( array_key_exists($r, $user->capabilities) ) echo ' selected="selected"'; ?>><?php echo $n; ?></option>
-<?php endif; endforeach; ?>
-      </select>
-  </td>
-</tr>
-<tr class="extra-caps-row">
-  <th scope="row"><?php _e('Allow this user to:'); ?></th>
-  <td>
-<?php foreach( $assignable_caps as $cap => $label ) : ?>
-      <label><input name="<?php echo $cap; ?>" value="1" type="checkbox"<?php if ( array_key_exists($cap, $user->capabilities) ) echo ' checked="checked"'; ?> /> <?php echo $label; ?></label><br />
-<?php endforeach; ?>
-  </td>
-</tr>
-<?php if ( is_array($profile_admin_keys) ) : foreach ( $profile_admin_keys as $key => $label ) : ?>
-<tr<?php if ( $label[0] ) { echo ' class="required"'; $label[1] .= '<sup>*</sup>'; $required = true; } ?>>
-  <th scope="row"><?php echo $label[1]; ?>:</th>
-  <td><input name="<?php echo $key; ?>" id="<?php echo $key; ?>" type=<?php
-	if ( isset($label[2]) )
-		echo $label[2];
-	else
-		echo '"text" size="30" maxlength="140" value="' . wp_specialchars( $user->$key, 1 ). '"';
-	?> />
-<?php if ( isset($$key) && false === $$key ) _e('<br />The above field is required.'); ?></td>
-</tr>
-<?php endforeach; endif; ?>
-</table>
-<?php if ( $required ) : ?>
-<p><sup>*</sup><?php _e('These items are <span class="required">required</span>.') ?></p>
-<?php endif; ?>
-<p><?php _e('Inactive users can login and look around but not do anything.
-Blocked users just see a simple error message when they visit the site.</p>
-<p><strong>Note</strong>: Blocking a user does <em>not</em> block any IP addresses.'); ?></p>
+<?php bb_profile_admin_form(); ?>
 </fieldset>
 <?php endif; ?>
 
@@ -85,7 +32,7 @@ Blocked users just see a simple error message when they visit the site.</p>
 </tr>
 </table>
 </fieldset>
-<?php endif; bb_nonce_field( 'edit-profile_' . $user->ID ); ?>
+<?php endif; ?>
 <p class="submit right">
   <input type="submit" name="Submit" value="<?php _e('Update Profile &raquo;'); ?>" />
 </p>
