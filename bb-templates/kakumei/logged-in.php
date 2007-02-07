4c4bb9331a39538160cfba28d245a6aec72faa93
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2007-02-07 00:30:13 +0000

    slight tweak to logged-in template
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@686 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-templates/kakumei/logged-in.php
+++ bb-templates/kakumei/logged-in.php
@@ -1,4 +1,4 @@
 <p class="login">
 	<?php printf('<p class="login">' . __('Welcome, %1$s!'), bb_get_current_user_info( 'name' ));?> <?php bb_profile_link(); ?>
-	<small>(<?php bb_admin_link();?> | <?php bb_logout_link(); ?>)</small>
+	<small>(<?php bb_admin_link( 'after= | ' );?><?php bb_logout_link(); ?>)</small>
 </p>
