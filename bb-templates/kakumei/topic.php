31b51deec1f54a56a8f3f2217cbd2a42ef4bd34c
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2007-02-12 20:54:03 +0000

    typo re #469
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@709 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-templates/kakumei/topic.php
+++ bb-templates/kakumei/topic.php
@@ -9,7 +9,7 @@
 <ul class="topicmeta">
 	<li><?php printf(__('Started %1$s ago by %2$s'), get_topic_start_time(), get_topic_author()) ?></li>
 <?php if ( 1 < get_topic_posts() ) : ?>
-	<li><?php printf(__('<a href="%1$s">Latest reply</a> from %2$s'), attrtibute_escape( get_topic_last_post_link() ), get_topic_last_poster()) ?></li>
+	<li><?php printf(__('<a href="%1$s">Latest reply</a> from %2$s'), attribute_escape( get_topic_last_post_link() ), get_topic_last_poster()) ?></li>
 <?php endif; ?>
 <?php if ( bb_is_user_logged_in() ) : $class = 0 === is_user_favorite( bb_get_current_user_info( 'id' ) ) ? ' class="is-not-favorite"' : ''; ?>
 	<li<?php echo $class;?> id="favorite-toggle"><?php user_favorites_link() ?></li>
