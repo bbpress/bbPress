caae4cb693aff2bbd4f4e808a3d51e67d56c4508
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2007-03-14 23:46:33 +0000

    bb_nonce_field() update from [WP5039]
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@775 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/functions.php
+++ bb-includes/functions.php
@@ -1709,9 +1709,11 @@ function bb_nonce_url($actionurl, $action = -1) {
 	return add_query_arg( '_wpnonce', bb_create_nonce( $action ), $actionurl );
 }
 
-function bb_nonce_field($action = -1) {
-	echo '<input type="hidden" name="_wpnonce" value="' . bb_create_nonce($action) . '" />';
-	wp_referer_field();
+function wp_nonce_field($action = -1, $name = "_wpnonce", $referer = true) {
+	$name = attribute_escape($name);
+	echo '<input type="hidden" name="' . $name . '" value="' . wp_create_nonce($action) . '" />';
+	if ( $referer )
+		wp_referer_field();
 }
 
 function bb_nonce_ays($action) {
