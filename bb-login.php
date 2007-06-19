3c93ab36d0460a0a05d12e9fb0a92fbf99ab5f4c
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2007-06-19 20:02:37 +0000

    merge [861] to branches/0.8: sanitize login template.  Ory Segal, Dragos LUNGU, p0rk.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/0.8@862 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-login.php
+++ bb-login.php
@@ -14,6 +14,8 @@ if ( 0 === strpos($ref, bb_get_option( 'uri' )) ) {
 if ( 0 === strpos($re, bb_get_option( 'uri' ) . 'register.php') )
 	$re = bb_get_option( 'uri' );
 
+$re = clean_url( $re );
+
 nocache_headers();
 
 if ( isset( $_REQUEST['logout'] ) ) {
@@ -24,9 +26,9 @@ if ( isset( $_REQUEST['logout'] ) ) {
 
 if ( !bb_is_user_logged_in() && !$user = bb_login( @$_POST['user_login'], @$_POST['password'] ) ) {
 	$user_exists = bb_user_exists( @$_POST['user_login'] );
-	$user_login  = bb_user_sanitize ( @$_POST['user_login'] );
-	$redirect_to = wp_specialchars( $re, 1 );
-	bb_load_template( 'login.php', array('re', 'user_exists', 'user_login', 'redirect_to', 'ref') );
+	$user_login  = bb_user_sanitize( @$_POST['user_login'] );
+	$re = $redirect_to = attribute_escape( $re );
+	bb_load_template( 'login.php', array('user_exists', 'user_login', 'redirect_to', 're') );
 	exit;
 }
 
