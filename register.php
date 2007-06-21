e306fecd86f704a1d29ff344bdc4020b226ed2d1
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2007-06-21 22:33:21 +0000

    globalize $user_login and friends for register-success template.  Fixes #669
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@870 9866e705-20ec-0310-96e7-cbb4277adcfb

--- register.php
+++ register.php
@@ -7,6 +7,9 @@ $profile_info_keys = get_profile_info_keys();
 
 $user_login = $user_safe = true;
 
+$_globals = array('profile_info_keys', 'user_safe', 'user_login', 'user_email', 'user_url', 'bad_input');
+$_globals = array_merge($_globals, array_keys($profile_info_keys));
+
 if ($_POST) :
 	$user_login = bb_user_sanitize  ( $_POST['user_login'], true );
 	$user_email = bb_verify_email( $_POST['user_email'] );
@@ -34,7 +37,7 @@ if ($_POST) :
 				bb_update_usermeta( $user_id, $key, $$key );
 		do_action('register_user', $user_id);
 
-		bb_load_template( 'register-success.php' );
+		bb_load_template( 'register-success.php', $_globals );
 		exit();	
 	endif;
 endif;
@@ -43,9 +46,6 @@ if ( isset( $_GET['user'] ) )
 	$user_login = bb_user_sanitize( $_GET['user'], true ) ;
 elseif ( isset( $_POST['user_login'] ) && !is_string($user_login) )
 	$user_login = '';
-
-$_globals = array('profile_info_keys', 'user_safe', 'user_login', 'user_email', 'user_url', 'bad_input');
-$_globals = array_merge($_globals, array_keys($profile_info_keys));
 
 bb_load_template( 'register.php', $_globals );
 
