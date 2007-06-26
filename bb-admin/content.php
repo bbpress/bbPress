eecf75549c2cf390a9922d9e26a4024c56aca8c7
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2007-06-26 23:46:16 +0000

    Topic search in admin side.  See #657 #602
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@881 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-admin/content.php
+++ bb-admin/content.php
@@ -1,15 +1,43 @@
 <?php require_once('admin.php'); ?>
 <?php bb_get_admin_header(); ?>
 
-<?php	if ( !bb_current_user_can('browse_deleted') )
+<?php
+	if ( !bb_current_user_can('browse_deleted') )
 		die(__("Now how'd you get here?  And what did you think you'd being doing?")); //This should never happen.
 	add_filter( 'topic_link', 'bb_make_link_view_all' );
-	$topic_query = new BB_Query('topic', array('topic_status' => 1) );
+	$topic_query_vars = array('topic_status' => 1, 'open' => 'all');
+	if ( isset($_REQUEST['search']) )
+		$topic_query_vars['post_status'] = 'all';
+	$topic_query = bb_query_from_env( 'topic', $topic_query_vars );
 	$topics = $topic_query->results;
-
 ?>
 
-<h2><?php _e('Deleted Topics') ?></h2>
+<h2><?php
+$h2_search = $topic_query->get( 'search' );
+$h2_forum  = $topic_query->get( 'forum_id' );
+$h2_tag    = $topic_query->get( 'tag_id' );
+$h2_author = $topic_query->get( 'topic_author_id' );
+$h2_status = $topic_query->get( 'topic_status' );
+$h2_open   = $topic_query->get( 'open' );
+
+$h2_search = $h2_search ? ' ' . sprintf( __('matching &#8220;%s&#8221;'), wp_specialchars( $h2_search ) ) : '';
+$h2_forum  = $h2_forum  ? ' ' . sprintf( __('in &#8220;%s&#8221;')      , get_forum_name( $h2_forum ) ) : '';
+$h2_tag    = $h2_tag    ? ' ' . sprintf( __('with tag &#8220;%s&#8221;'), wp_specialchars( get_tag_name( $h2_tag ) ) ) : '';
+$h2_author = $h2_author ? ' ' . sprintf( __('by %s')                    , wp_specialchars( get_user_name( $h2_author ) ) ) : '';
+
+$topic_stati = array( 0 => __('Normal') . ' ', 1 => __('Deleted') . ' ', 'all' => '' );
+$topic_open  = array( 0 => __('Closed') . ' ', 1 => __('Open') . ' '   , 'all' => '' );
+
+if ( 'all' == $h2_status && 'all' == $h2_open )
+	$h2_noun = __('Topics');
+else
+	$h2_noun = sprintf( __( '%1$s%2$stopics'), $topic_stati[$h2_status], $topic_open[$h2_open] );
+
+printf( __( '%1$s%2$s%3$s%4$s%5$s' ), $h2_noun, $h2_search, $h2_forum, $h2_tag, $h2_author );
+
+?></h2>
+
+<?php bb_topic_search_form( '', $topic_query->query_vars ); ?>
 
 <table class="widefat">
 <tr class="thead">
@@ -24,7 +52,11 @@
 	<td class="num"><?php topic_last_poster(); ?></td>
 	<td class="num"><small><?php topic_time(); ?></small></td>
 </tr>
-<?php endforeach; endif; ?>
+<?php endforeach; else : ?>
+<tr>
+	<td colspan="3"><?php _e('No Topics Found'); ?></td>
+</tr>
+<?php endif; ?>
 </table>
 
 <?php echo get_page_number_links( $page, $topic_query->row_count ); ?>
