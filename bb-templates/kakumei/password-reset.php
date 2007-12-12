164743a088436cfc703206a74d3f021adbd46c20
Sam Bauers <sambauers@git.wordpress.org>
2007-12-12 09:01:56 +0000

    Fix typo in password reset text.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@977 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-templates/kakumei/password-reset.php
+++ bb-templates/kakumei/password-reset.php
@@ -7,7 +7,7 @@
 <?php if ( $reset ) : ?>
 <p><?php _e('Your password has been reset and a new one has been mailed to you.'); ?></p>
 <?php else : ?>
-<p><?php _e('An email has been sent to the address we have on file for you. If you don&#8217;t get anything with a few minutes, or your email has changed, you may want to get in touch with the webmaster or forum administrator here.'); ?></p>
+<p><?php _e('An email has been sent to the address we have on file for you. If you don&#8217;t get anything within a few minutes, or your email has changed, you may want to get in touch with the webmaster or forum administrator here.'); ?></p>
 <?php endif; ?>
 
 <?php bb_get_footer(); ?>
