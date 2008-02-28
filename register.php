7ddc2511506a7c090e6975ddd57c8c25a895de55
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2008-02-28 10:46:44 +0000

    clean up blocks in register.php for branches/0.8
    git-svn-id: https://bbpress.svn.wordpress.org/branches/0.8@1152 9866e705-20ec-0310-96e7-cbb4277adcfb

--- register.php
+++ register.php
@@ -10,29 +10,29 @@ $user_login = $user_safe = true;
 $_globals = array('profile_info_keys', 'user_safe', 'user_login', 'user_email', 'user_url', 'bad_input');
 $_globals = array_merge($_globals, array_keys($profile_info_keys));
 
-if ($_POST) :
+if ( $_POST && 'post' == strtolower($_SERVER['REQUEST_METHOD']) ) {
 	$_POST = stripslashes_deep( $_POST );
 	$user_login = sanitize_user( $_POST['user_login'], true );
 	$user_email = bb_verify_email( $_POST['user_email'] );
 	$user_url   = bb_fix_link( $_POST['user_url'] );
 
-	foreach ( $profile_info_keys as $key => $label ) :
-		if ( is_string($$key) ) :
+	foreach ( $profile_info_keys as $key => $label ) {
+		if ( is_string($$key) )
 			$$key = attribute_escape( $$key );
-		elseif ( is_null($$key) ) :
+		elseif ( is_null($$key) )
 			$$key = attribute_escape( $_POST[$key] );
-		endif;
-		if ( !$$key && $label[0] == 1 ) :
+
+		if ( !$$key && $label[0] == 1 ) {
 			$bad_input = true;
 			$$key = false;
-		endif;
-	endforeach;
+		}
+	}
 
 	if ( empty($user_login) || bb_user_exists($user_login) )
 		$user_safe = false;
 	
-	if ( $user_login && $user_safe && $user_email && !$bad_input) :
-		if ( $user_id = bb_new_user( $user_login, $user_email, $user_url ) ) :
+	if ( $user_login && $user_safe && $user_email && !$bad_input) {
+		if ( $user_id = bb_new_user( $user_login, $user_email, $user_url ) ) {
 			foreach( $profile_info_keys as $key => $label )
 				if ( strpos($key, 'user_') !== 0 && $$key !== '' )
 					bb_update_usermeta( $user_id, $key, $$key );
@@ -40,9 +40,9 @@ if ($_POST) :
 
 			bb_load_template( 'register-success.php', $_globals );
 			exit();	
-		endif;
-	endif;
-endif;
+		}
+	}
+}
 
 if ( isset( $_GET['user'] ) )
 	$user_login = sanitize_user( $_GET['user'], true ) ;
