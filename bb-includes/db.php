854b38934f7af1db813b019d65e1e3f457f3307e
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2008-02-28 08:24:24 +0000

    content type before title, noindex,nofollow on bb_die() for branches/0.8
    git-svn-id: https://bbpress.svn.wordpress.org/branches/0.8@1139 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/db.php
+++ bb-includes/db.php
@@ -416,8 +416,8 @@ class bbdb_base {
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>bbPress &rsaquo; Error</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>bbPress &rsaquo; Error</title>
 		<style media="screen" type="text/css">
 		<!--
 		html {
