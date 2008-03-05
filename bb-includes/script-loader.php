0f3d03aee8eadf18dec652a01c7310ecd685fa3b
Sam Bauers <sambauers@git.wordpress.org>
2008-03-05 04:33:06 +0000

    Bump jQuery version in script-loader
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@1222 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/script-loader.php
+++ bb-includes/script-loader.php
@@ -8,7 +8,7 @@ function bb_default_scripts( $scripts ) {
 	$scripts->add( 'listman', $base . BB_INC . 'js/list-manipulation-js.php', array('add-load-event', 'wp-ajax', 'fat'), '440' );
 	$scripts->add( 'topic', $base . BB_INC . 'js/topic-js.php', array('add-load-event', 'listman', 'jquery'), '433' );
 	$scripts->add( 'jquery', $base . BB_INC . 'js/jquery/jquery.js', false, '1.1.3.1');
-	$scripts->add( 'interface', $base . BB_INC . 'js/jquery/interface.js', array('jquery'), '1.2');
+	$scripts->add( 'interface', $base . BB_INC . 'js/jquery/interface.js', array('jquery'), '1.2.3');
 	$scripts->add( 'jquery-color', $base . BB_INC . 'js/jquery/jquery.color.js', array('jquery'), '1.0' );
 	$scripts->add( 'add-load-event', $base . BB_INC . 'js/add-load-event.js' );
 	$scripts->add( 'content-forums', $base . '/bb-admin/js/content-forums.js', array('listman', 'interface'), 4 );
