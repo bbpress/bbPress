e77afa53a827f7277e94824657e7d986ce4f3477
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2008-03-07 21:25:45 +0000

    [1198] [1200] I think will break more templates than it fixes
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@1249 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-templates/kakumei/post.php
+++ bb-templates/kakumei/post.php
@@ -6,7 +6,7 @@
 			</p>
 		</div>
 		
-		<div<?php alt_class('post', 'threadpost ' . $del_class); ?>>
+		<div class="threadpost">
 			<div class="post"><?php post_text(); ?></div>
 			<div class="poststuff"><?php printf( __('Posted %s ago'), bb_get_post_time() ); ?> <a href="<?php post_anchor_link(); ?>">#</a> <?php post_ip_link(); ?> <?php post_edit_link(); ?> <?php post_delete_link(); ?></div>
 		</div>
