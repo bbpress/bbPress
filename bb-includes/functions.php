985fe6d4217f8ad48f1710cd3b2822d81282ec97
Sam Bauers <sambauers@git.wordpress.org>
2008-03-24 03:38:48 +0000

    Remove extraneous whitespace in html tag.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@1367 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/functions.php
+++ bb-includes/functions.php
@@ -2222,7 +2222,7 @@ function bb_install_header( $title = '', $header = false ) {
 	header('Content-Type: text/html; charset=utf-8');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php if ( function_exists( 'bb_language_attributes' ) ) bb_language_attributes(); ?>>
+<html xmlns="http://www.w3.org/1999/xhtml"<?php if ( function_exists( 'bb_language_attributes' ) ) bb_language_attributes(); ?>>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title><?php echo $title; ?></title>
