fd182e46d91f0fd517a42f6f04595ab34d02fab2
Sam Bauers <sambauers@git.wordpress.org>
2008-04-17 18:32:03 +0000

    Better mail headers in bb_mail()
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@1422 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/pluggable.php
+++ bb-includes/pluggable.php
@@ -404,7 +404,16 @@ function bb_mail( $to, $subject, $message, $headers = '' ) {
 		$headers = preg_split('@\r(?:\n{0,1})|\n@', $headers, -1, PREG_SPLIT_NO_EMPTY);
 	}
 	
-	if (!count($headers) || !count(preg_grep('/^from:\s/im', $headers))) {
+	if (!count($headers) || !count(preg_grep('/^mime-version:\s/im', $headers))) {
+		$headers[] = "MIME-Version: 1.0";
+	
+	if (!count(preg_grep('/^content-type:\s/im', $headers))) {
+		$headers[] = "Content-Type: text/plain; Charset=UTF-8";
+	
+	if (!count(preg_grep('/^content-transfer-encoding:\s/im', $headers))) {
+		$headers[] = "Content-Transfer-Encoding: 8bit";
+	
+	if (!count(preg_grep('/^from:\s/im', $headers))) {
 		if (!$from = bb_get_option('from_email'))
 			if ($uri_parsed = parse_url(bb_get_option('uri')))
 				if ($uri_parsed['host'])
