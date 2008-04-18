c0ff87a11a5082e81feb29aad74a9aafdc9068e8
Sam Bauers <sambauers@git.wordpress.org>
2008-04-18 10:30:13 +0000

    Add keys to forum and topic slugs to improve performance.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@1426 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/functions.php
+++ bb-includes/functions.php
@@ -1440,7 +1440,7 @@ function bb_get_option( $option ) {
 		return '1.0-dev'; // Don't filter
 		break;
 	case 'bb_db_version' :
-		return '1234'; // Don't filter
+		return '1426'; // Don't filter
 		break;
 	case 'html_type' :
 		$r = 'text/html';
