586da1f7b6b2e31b3455ee95e37ef6d55c740fde
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2008-04-24 09:04:44 +0000

    don't dirty up the kakumei header with wp_enque_script() calls for the core JS for branches/0.9
    git-svn-id: https://bbpress.svn.wordpress.org/branches/0.9@1473 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/template-functions.php
+++ bb-includes/template-functions.php
@@ -2256,4 +2256,9 @@ function _bb_time_function_return( $time, $args ) {
 	return bb_gmdate_i18n( $format, $time );
 }
 
+function bb_template_scripts() {
+	if ( is_topic() && bb_is_user_logged_in() )
+		wp_enqueue_script( 'topic' );
+}
+
 ?>
