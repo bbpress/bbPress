1be15268b7d131b0c6d61eafda26bb437550a416
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2008-05-15 23:03:20 +0000

    move utf8_uri_encode() to backpress
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@1539 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/wp-functions.php
+++ bb-includes/wp-functions.php
@@ -49,49 +49,6 @@ function clean_pre($matches) {
 }
 endif;
 
-if ( !function_exists('utf8_uri_encode') ) : // [WP6314]
-function utf8_uri_encode( $utf8_string, $length = 0 ) {
-	$unicode = '';
-	$values = array();
-	$num_octets = 1;
-	$unicode_length = 0;
-
-	$string_length = strlen( $utf8_string );
-	for ($i = 0; $i < $string_length; $i++ ) {
-
-		$value = ord( $utf8_string[ $i ] );
-
-		if ( $value < 128 ) {
-			if ( $length && ( $unicode_length >= $length ) )
-				break;
-			$unicode .= chr($value);
-			$unicode_length++;
-		} else {
-			if ( count( $values ) == 0 ) $num_octets = ( $value < 224 ) ? 2 : 3;
-
-			$values[] = $value;
-
-			if ( $length && ( $unicode_length + ($num_octets * 3) ) > $length )
-				break;
-			if ( count( $values ) == $num_octets ) {
-				if ($num_octets == 3) {
-					$unicode .= '%' . dechex($values[0]) . '%' . dechex($values[1]) . '%' . dechex($values[2]);
-					$unicode_length += 9;
-				} else {
-					$unicode .= '%' . dechex($values[0]) . '%' . dechex($values[1]);
-					$unicode_length += 6;
-				}
-
-				$values = array();
-				$num_octets = 1;
-			}
-		}
-	}
-
-	return $unicode;
-}
-endif;
-
 // Escape single quotes, specialchar double quotes, and fix line endings.
 if ( !function_exists('js_escape') ) : // [WP5734]
 function js_escape($text) {
