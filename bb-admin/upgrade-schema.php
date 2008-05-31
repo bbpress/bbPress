979518f6761521815d52020347985ad50e4b496e
Sam Bauers <sambauers@git.wordpress.org>
2008-05-31 16:14:22 +0000

    Update addition of charset and collation to schema to use new DB classes and custom table setup.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@1550 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-admin/upgrade-schema.php
+++ bb-admin/upgrade-schema.php
@@ -1,28 +1,16 @@
 <?php
+
+// Globalise as this file is included within the functions bb_install() and bb_upgrade_all()
 global $bb_queries, $bbdb;
 
-$charset_collate = '';
-$user_charset_collate = '';
-
+// Die if no database class is loaded
 if ( !isset($bbdb) || !is_a( $bbdb, 'BPDB' ) )
 	die( __('Database class not loaded.') );
 
-if ( $bbdb->has_cap( 'collation', $bbdb->forums ) ) {
-	if ( ! empty($bbdb->charset) )
-		$charset_collate = "DEFAULT CHARACTER SET $bbdb->charset";
-	if ( ! empty($bbdb->collate) )
-		$charset_collate .= " COLLATE $bbdb->collate";
-}
-
-if ( $bbdb->has_cap( 'collation', $bbdb->users ) ) {
-	if ( ! empty($bbdb->user_charset) )
-		$user_charset_collate = "DEFAULT CHARACTER SET $bbdb->user_charset";
-	if ( ! empty($bbdb->collate) )
-		$user_charset_collate .= " COLLATE $bbdb->collate";
-}
-
+// Initialise the query array
 $bb_queries = array();
 
+// forums
 $bb_queries['forums'] = "CREATE TABLE $bbdb->forums (
   forum_id int(10) NOT NULL auto_increment,
   forum_name varchar(150)  NOT NULL default '',
@@ -34,8 +22,9 @@ $bb_queries['forums'] = "CREATE TABLE $bbdb->forums (
   posts bigint(20) NOT NULL default '0',
   PRIMARY KEY  (forum_id),
   KEY forum_slug (forum_slug)
-) $charset_collate;";
+);";
 
+// meta
 $bb_queries['meta'] = "CREATE TABLE $bbdb->meta (
   meta_id bigint(20) NOT NULL auto_increment,
   object_type varchar(16) NOT NULL default 'bb_option',
@@ -43,10 +32,11 @@ $bb_queries['meta'] = "CREATE TABLE $bbdb->meta (
   meta_key varchar(255) default NULL,
   meta_value longtext default NULL,
   PRIMARY KEY  (meta_id),
-  KEY object_type__meta_key (object_type, meta_key),
-  KEY object_type__object_id__meta_key (object_type, object_id, meta_key)
-) $charset_collate;";
+  KEY object_type__meta_key (object_type,meta_key),
+  KEY object_type__object_id__meta_key (object_type,object_id,meta_key)
+);";
 
+// posts
 $bb_queries['posts'] = "CREATE TABLE $bbdb->posts (
   post_id bigint(20) NOT NULL auto_increment,
   forum_id int(10) NOT NULL default '1',
@@ -62,8 +52,9 @@ $bb_queries['posts'] = "CREATE TABLE $bbdb->posts (
   KEY poster_time (poster_id,post_time),
   KEY post_time (post_time),
   FULLTEXT KEY post_text (post_text)
-) TYPE = MYISAM $charset_collate;";
+) TYPE = MYISAM;";
 
+// tagged - deprecated
 $bb_queries['tagged'] = "CREATE TABLE $bbdb->tagged (
   tagged_id bigint(20) unsigned NOT NULL auto_increment,
   tag_id bigint(20) unsigned NOT NULL default '0',
@@ -74,8 +65,9 @@ $bb_queries['tagged'] = "CREATE TABLE $bbdb->tagged (
   UNIQUE KEY tag_user_topic (tag_id,user_id,topic_id),
   KEY user_id_index (user_id),
   KEY topic_id_index (topic_id)
-) $charset_collate;";
+);";
 
+// tags - deprecated
 $bb_queries['tags'] = "CREATE TABLE $bbdb->tags (
   tag_id bigint(20) unsigned NOT NULL auto_increment,
   tag varchar(200) NOT NULL default '',
@@ -83,8 +75,9 @@ $bb_queries['tags'] = "CREATE TABLE $bbdb->tags (
   tag_count bigint(20) unsigned NOT NULL default '0',
   PRIMARY KEY  (tag_id),
   KEY name (tag)
-) $charset_collate;";
+);";
 
+// terms
 $bb_queries['terms'] = "CREATE TABLE $bbdb->terms (
  term_id bigint(20) NOT NULL auto_increment,
  name varchar(55) NOT NULL default '',
@@ -92,8 +85,9 @@ $bb_queries['terms'] = "CREATE TABLE $bbdb->terms (
  term_group bigint(10) NOT NULL default 0,
  PRIMARY KEY  (term_id),
  UNIQUE KEY slug (slug)
-) $charset_collate;";
+);";
 
+// term_relationships
 $bb_queries['term_relationships'] = "CREATE TABLE $bbdb->term_relationships (
  object_id bigint(20) NOT NULL default 0,
  term_taxonomy_id bigint(20) NOT NULL default 0,
@@ -101,8 +95,9 @@ $bb_queries['term_relationships'] = "CREATE TABLE $bbdb->term_relationships (
  term_order int(11) NOT NULL default 0,
  PRIMARY KEY  (object_id,term_taxonomy_id),
  KEY term_taxonomy_id (term_taxonomy_id)
-) $charset_collate;";
+);";
 
+// term_taxonomy
 $bb_queries['term_taxonomy'] = "CREATE TABLE $bbdb->term_taxonomy (
  term_taxonomy_id bigint(20) NOT NULL auto_increment,
  term_id bigint(20) NOT NULL default 0,
@@ -112,8 +107,9 @@ $bb_queries['term_taxonomy'] = "CREATE TABLE $bbdb->term_taxonomy (
  count bigint(20) NOT NULL default 0,
  PRIMARY KEY  (term_taxonomy_id),
  UNIQUE KEY term_id_taxonomy (term_id,taxonomy)
-) $charset_collate;";
+);";
 
+// topics
 $bb_queries['topics'] = "CREATE TABLE $bbdb->topics (
   topic_id bigint(20) NOT NULL auto_increment,
   topic_title varchar(100) NOT NULL default '',
@@ -136,8 +132,9 @@ $bb_queries['topics'] = "CREATE TABLE $bbdb->topics (
   KEY forum_time (forum_id,topic_time),
   KEY user_start_time (topic_poster,topic_start_time),
   KEY forum_stickies (topic_status,forum_id,topic_sticky,topic_time)
-) $charset_collate;";
+);";
 
+// topicmeta - deprecated
 $bb_queries['topicmeta'] = "CREATE TABLE $bbdb->topicmeta (
   meta_id bigint(20) NOT NULL auto_increment,
   topic_id bigint(20) NOT NULL default '0',
@@ -146,8 +143,9 @@ $bb_queries['topicmeta'] = "CREATE TABLE $bbdb->topicmeta (
   PRIMARY KEY  (meta_id),
   KEY topic_id (topic_id),
   KEY meta_key (meta_key)
-) $charset_collate;";
+);";
 
+// users - non-primary indices are inconsistent with WordPress
 $bb_queries['users'] = "CREATE TABLE $bbdb->users (
   ID bigint(20) unsigned NOT NULL auto_increment,
   user_login varchar(60) NOT NULL default '',
@@ -161,8 +159,9 @@ $bb_queries['users'] = "CREATE TABLE $bbdb->users (
   PRIMARY KEY  (ID),
   UNIQUE KEY user_login (user_login),
   UNIQUE KEY user_nicename (user_nicename)
-) $user_charset_collate;";
+);";
 
+// usermeta
 $bb_queries['usermeta'] = "CREATE TABLE $bbdb->usermeta (
   umeta_id bigint(20) NOT NULL auto_increment,
   user_id bigint(20) NOT NULL default '0',
@@ -171,7 +170,57 @@ $bb_queries['usermeta'] = "CREATE TABLE $bbdb->usermeta (
   PRIMARY KEY  (umeta_id),
   KEY user_id (user_id),
   KEY meta_key (meta_key)
-) $user_charset_collate;";
+);";
+
+// Set the charset and collation on each table
+foreach ($bb_queries as $_table_name => $_sql) {
+	// Skip SQL that isn't creating a table
+	if (!preg_match('@^\s*CREATE\s+TABLE\s+@im', $_sql)) {
+		continue;
+	}
+	
+	// Skip if the table's database doesn't support collation
+	if (!$bbdb->has_cap('collation', $bbdb->$_table_name)) {
+		continue;
+	}
+	
+	// Find out if the table has a custom database set
+	if (
+		isset($bbdb->db_tables) &&
+		is_array($bbdb->db_tables) &&
+		isset($bbdb->db_tables[$bbdb->$_table_name])
+	) {
+		// Set the database for this table
+		$_database = $bbdb->db_tables[$_prefixed_table_name];
+	} else {
+		// Set the default global database
+		$_database = 'dbh_global';
+	}
+	
+	// Make sure the database exists
+	if (
+		isset($bbdb->db_servers) &&
+		is_array($bbdb->db_servers) &&
+		isset($bbdb->db_servers[$_database]) &&
+		is_array($bbdb->db_servers[$_database])
+	) {
+		$_charset_collate = '';
+		if (isset($bbdb->db_servers[$_database]['charset'])) {
+			// Add a charset if set
+			$_charset_collate .= ' DEFAULT CHARACTER SET \'' . $bbdb->db_servers[$_database]['charset'] . '\'';
+		}
+		if (isset($bbdb->db_servers[$_database]['collate'])) {
+			// Add a collation if set
+			$_charset_collate .= ' COLLATE \'' . $bbdb->db_servers[$_database]['collate'] . '\'';
+		}
+		if ($_charset_collate) {
+			// Modify the SQL
+			$bb_queries[$_table_name] = str_replace(';', $_charset_collate . ';', $_sql);
+		}
+	}
+	unset($_database, $_charset_collate);
+}
+unset($_table_name, $_sql);
 
 $bb_queries = apply_filters( 'bb_schema', $bb_queries );
 
