16edaad5a832877c3eae51f669d435997252d4f1
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2008-08-11 20:38:15 +0000

    kakumei: validation fix
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@1641 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-templates/kakumei/search-form.php
+++ bb-templates/kakumei/search-form.php
@@ -1,5 +1,7 @@
-<form action="<?php bb_uri('search.php', null, BB_URI_CONTEXT_FORM_ACTION); ?>" method="get">
-	<label class="hidden" for="q"><?php _e('Search:'); ?></label>
-	<input class="text" type="text" size="14" maxlength="100" name="q" id="q" />
-	<input class="submit" type="submit" value="<?php echo attribute_escape( __('Search &raquo;') ); ?>" />
+<form class="search-form" action="<?php bb_uri('search.php', null, BB_URI_CONTEXT_FORM_ACTION); ?>" method="get">
+	<p>
+		<label class="hidden" for="q"><?php _e('Search:'); ?></label>
+		<input class="text" type="text" size="14" maxlength="100" name="q" id="q" />
+		<input class="submit" type="submit" value="<?php echo attribute_escape( __('Search &raquo;') ); ?>" />
+	</p>
 </form>
