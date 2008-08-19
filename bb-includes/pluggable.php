2a8328532b7bd6247b291616f2f795b2ba2085a8
Sam Bauers <sambauers@git.wordpress.org>
2008-08-19 00:43:16 +0000

    Inline documentation changes for bb_mail()
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@1653 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/pluggable.php
+++ bb-includes/pluggable.php
@@ -511,28 +511,26 @@ if ( !function_exists( 'bb_mail' ) ) :
  * email successfully. It just only means that the method used was able to
  * process the request without any errors.
  *
- * Using the two 'wp_mail_from' and 'wp_mail_from_name' hooks allow from
+ * Using the two 'bb_mail_from' and 'bb_mail_from_name' hooks allow from
  * creating a from address like 'Name <email@address.com>' when both are set. If
- * just 'wp_mail_from' is set, then just the email address will be used with no
+ * just 'bb_mail_from' is set, then just the email address will be used with no
  * name.
  *
  * The default content type is 'text/plain' which does not allow using HTML.
  * However, you can set the content type of the email by using the
- * 'wp_mail_content_type' filter.
+ * 'bb_mail_content_type' filter.
  *
  * The default charset is based on the charset used on the blog. The charset can
- * be set using the 'wp_mail_charset' filter.
+ * be set using the 'bb_mail_charset' filter.
  *
- * @since 1.2.1
- * @uses apply_filters() Calls 'wp_mail' hook on an array of all of the parameters.
- * @uses apply_filters() Calls 'wp_mail_from' hook to get the from email address.
- * @uses apply_filters() Calls 'wp_mail_from_name' hook to get the from address name.
- * @uses apply_filters() Calls 'wp_mail_content_type' hook to get the email content type.
- * @uses apply_filters() Calls 'wp_mail_charset' hook to get the email charset
- * @uses do_action_ref_array() Calls 'phpmailer_init' hook on the reference to
+ * @uses apply_filters() Calls 'bb_mail' hook on an array of all of the parameters.
+ * @uses apply_filters() Calls 'bb_mail_from' hook to get the from email address.
+ * @uses apply_filters() Calls 'bb_mail_from_name' hook to get the from address name.
+ * @uses apply_filters() Calls 'bb_mail_content_type' hook to get the email content type.
+ * @uses apply_filters() Calls 'bb_mail_charset' hook to get the email charset
+ * @uses do_action_ref_array() Calls 'bb_phpmailer_init' hook on the reference to
  *		phpmailer object.
  * @uses PHPMailer
- * @
  *
  * @param string $to Email address to send message
  * @param string $subject Email subject
