a6139781ae66a843e27dbdb11e139a363dfde6a3
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2008-11-25 00:01:41 +0000

    style and warning cleanup in bb_append_meta
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@1814 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/functions.bb-meta.php
+++ bb-includes/functions.bb-meta.php
@@ -429,39 +429,41 @@ function bb_append_meta( $object, $type ) {
 		$slug = 'post_slug';
 		break;
 	endswitch;
-	if ( is_array($object) && $object ) :
+	if ( is_array($object) && $object ) {
 		$trans = array();
 		foreach ( array_keys($object) as $i )
 			$trans[$object[$i]->$object_id_column] =& $object[$i];
 		$ids = join(',', array_map('intval', array_keys($trans)));
-		if ( $metas = $bbdb->get_results("SELECT object_id, meta_key, meta_value FROM $bbdb->meta WHERE object_type = '$object_type' AND object_id IN ($ids) /* bb_append_meta */") )
+		if ( $metas = $bbdb->get_results("SELECT object_id, meta_key, meta_value FROM $bbdb->meta WHERE object_type = '$object_type' AND object_id IN ($ids) /* bb_append_meta */") ) {
 			usort( $metas, '_bb_append_meta_sort' );
-			foreach ( $metas as $meta ) :
+			foreach ( $metas as $meta ) {
 				$trans[$meta->object_id]->{$meta->meta_key} = maybe_unserialize( $meta->meta_value );
 				if ( strpos($meta->meta_key, $bbdb->prefix) === 0 )
 					$trans[$meta->object_id]->{substr($meta->meta_key, strlen($bbdb->prefix))} = maybe_unserialize( $meta->meta_value );
-			endforeach;
+			}
+		}
 		foreach ( array_keys($trans) as $i ) {
 			wp_cache_add( $i, $trans[$i], $object_type );
 			if ($slug)
 				wp_cache_add( $trans[$i]->$slug, $i, 'bb_' . $slug );
 		}
 		return $object;
-	elseif ( $object ) :
-		if ( $metas = $bbdb->get_results( $bbdb->prepare( "SELECT meta_key, meta_value FROM $bbdb->meta WHERE object_type = '$object_type' AND object_id = %d /* bb_append_meta */", $object->$object_id_column ) ) )
+	} elseif ( $object ) {
+		if ( $metas = $bbdb->get_results( $bbdb->prepare( "SELECT meta_key, meta_value FROM $bbdb->meta WHERE object_type = '$object_type' AND object_id = %d /* bb_append_meta */", $object->$object_id_column ) ) ) {
 			usort( $metas, '_bb_append_meta_sort' );
-			foreach ( $metas as $meta ) :
+			foreach ( $metas as $meta ) {
 				$object->{$meta->meta_key} = maybe_unserialize( $meta->meta_value );
 				if ( strpos($meta->meta_key, $bbdb->prefix) === 0 )
 					$object->{substr($meta->meta_key, strlen($bbdb->prefix))} = $object->{$meta->meta_key};
-			endforeach;
+			}
+		}
 		if ( $object->$object_id_column ) {
 			wp_cache_set( $object->$object_id_column, $object, $object_type );
 			if ($slug)
 				wp_cache_add( $object->$slug, $object->$object_id_column, 'bb_' . $slug );
 		}
 		return $object;
-	endif;
+	}
 }
 
 /** 
