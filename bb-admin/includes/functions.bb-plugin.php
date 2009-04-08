be2d2253bbbf1783e8759fcd58e618240cb714de
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2009-04-08 21:04:38 +0000

    updates to bb_get_plugin_data(): parse plugin data block even if after PHPDoc block.  Allow translation of plugin data.  Imported from WP
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@2051 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-admin/includes/functions.bb-plugin.php
+++ bb-admin/includes/functions.bb-plugin.php
@@ -85,9 +85,72 @@ function bb_plugins_sort( $a, $b )
 	return strnatcasecmp( $a['name'], $b['name'] );
 }
 
-// Output sanitized for display
-function bb_get_plugin_data( $plugin_file )
-{
+/**
+ * Parse the plugin contents to retrieve plugin's metadata.
+ *
+ * The metadata of the plugin's data searches for the following in the plugin's
+ * header. All plugin data must be on its own line. For plugin description, it
+ * must not have any newlines or only parts of the description will be displayed
+ * and the same goes for the plugin data. The below is formatted for printing.
+ *
+ * <code>
+ * /*
+ * Plugin Name: Name of Plugin
+ * Plugin URI: Link to plugin information
+ * Description: Plugin Description
+ * Author: Plugin author's name
+ * Author URI: Link to the author's web site
+ * Version: Must be set
+ * Requires at least: Optional.  Minimum bbPress version this plugin requires
+ * Tested up to: Optional. Maximum bbPress version this plugin has been tested with
+ * Text Domain: Optional. Unique identifier, should be same as the one used in
+ *		load_plugin_textdomain()
+ * Domain Path: Optional. Only useful if the translations are located in a
+ *		folder above the plugin's base path. For example, if .mo files are
+ *		located in the locale folder then Domain Path will be "/locale/" and
+ *		must have the first slash. Defaults to the base folder the plugin is
+ *		located in.
+ *  * / # You must remove the space to close comment (the space is here only for documentation purposes).
+ * </code>
+ *
+ * Plugin data returned array contains the following:
+ *		'location' - Location of plugin file
+ *		'name' - Name of the plugin, must be unique.
+ *		'uri' - Plugin's web site.
+ *		'plugin_link' - Title of plugin linked to plugin's web site.
+ *		'description' - Description of what the plugin does and/or notes
+ *		from the author.
+ *		'author' - The author's name
+ *		'author_uri' - The author's web site address.
+ *		'author_link' - The author's name linked to the author's web site.
+ *		'version' - The plugin version number.
+ *		'requires' - Minimum bbPress version plugin requires
+ *		'tested' - Maximum bbPress version plugin has been tested with
+ *		'text_domain' - Plugin's text domain for localization.
+ *		'domain_path' - Plugin's relative directory path to .mo files.
+ *
+ * Some users have issues with opening large files and manipulating the contents
+ * for want is usually the first 1kiB or 2kiB. This function stops pulling in
+ * the plugin contents when it has all of the required plugin data.
+ *
+ * The first 8kiB of the file will be pulled in and if the plugin data is not
+ * within that first 8kiB, then the plugin author should correct their plugin
+ * and move the plugin data headers to the top.
+ *
+ * The plugin file is assumed to have permissions to allow for scripts to read
+ * the file. This is not checked however and the file is only opened for
+ * reading.
+ *
+ * @link http://trac.wordpress.org/ticket/5651 Previous Optimizations.
+ * @link http://trac.wordpress.org/ticket/7372 Further and better Optimizations.
+ * @since 1.5.0
+ *
+ * @param string $plugin_file Path to the plugin file
+ * @param bool $markup If the returned data should have HTML markup applied
+ * @param bool $translate If the returned data should be translated
+ * @return array See above for description.
+ */
+function bb_get_plugin_data( $plugin_file, $markup = true, $translate = true ) {
 	global $bb;
 
 	if ( preg_match( '/^([a-z0-9_-]+)#((?:[a-z0-9\/\\_-]+.)+)(php)$/i', $plugin_file, $_matches ) ) {
@@ -115,68 +178,25 @@ function bb_get_plugin_data( $plugin_file )
 		return false;
 	}
 
-	$plugin_code = implode( '', file( $plugin_file ) );
+	// We don't need to write to the file, so just open for reading.
+	$fp = fopen($plugin_file, 'r');
 
-	// Grab just the first commented area from the file
-	if ( !preg_match( '|/\*(.*)\*/|msU', $plugin_code, $plugin_block ) ) {
-		return false;
-	}
+	// Pull only the first 8kiB of the file in.
+	$plugin_data = fread( $fp, 8192 );
 
-	$plugin_data = trim( $plugin_block[1] );
+	// PHP will close file handle, but we are good citizens.
+	fclose($fp);
 
-	if ( !preg_match( '/Plugin Name:(.*)/i', $plugin_data, $plugin_name ) ) {
-		return false;
-	}
-
-	preg_match( '/Plugin URI:(.*)/i', $plugin_data, $plugin_uri );
-	preg_match( '/Description:(.*)/i', $plugin_data, $description );
-	preg_match( '/Author:(.*)/i', $plugin_data, $author_name );
-	preg_match( '/Author URI:(.*)/i', $plugin_data, $author_uri );
-
-	if ( preg_match( '/Requires at least:(.*)/i', $plugin_data, $requires ) ) {
-		$requires = wp_specialchars( trim( $requires[1] ) );
-	} else {
-		$requires = '';
-	}
-	if ( preg_match( '/Tested up to:(.*)/i', $plugin_data, $tested ) ) {
-		$tested = wp_specialchars( trim( $tested[1] ) );
-	} else {
-		$tested = '';
-	}
-	if ( preg_match( '/Version:(.*)/i', $plugin_data, $version ) ) {
-		$version = wp_specialchars( trim( $version[1] ) );
-	} else {
-		$version = '';
-	}
-
-	$plugin_name = wp_specialchars( trim( $plugin_name[1] ) );
-
-	if ( $plugin_uri ) {
-		$plugin_uri = clean_url( trim( $plugin_uri[1] ) );
-	} else {
-		$plugin_uri = '';
-	}
-	if ( $author_name ) {
-		$author_name = wp_specialchars( trim( $author_name[1] ) );
-	} else {
-		$author_name = '';
-	}
-	if ( $author_uri ) {
-		$author_uri = clean_url( trim( $author_uri[1] ) );
-	} else {
-		$author_uri = '';
-	}
-
-	if ( $description ) {
-		$description = trim( $description[1] );
-		$description = bb_encode_bad( $description );
-		$description = bb_code_trick( $description );
-		$description = force_balance_tags( $description );
-		$description = bb_filter_kses( $description );
-		$description = bb_autop( $description );
-	} else {
-		$description = '';
-	}
+	preg_match( '|Plugin Name:(.*)$|mi', $plugin_data, $name );
+	preg_match( '|Plugin URI:(.*)$|mi', $plugin_data, $uri );
+	preg_match( '|Version:(.*)|i', $plugin_data, $version );
+	preg_match( '|Description:(.*)$|mi', $plugin_data, $description );
+	preg_match( '|Author:(.*)$|mi', $plugin_data, $author );
+	preg_match( '|Author URI:(.*)$|mi', $plugin_data, $author_uri );
+	preg_match( '|Text Domain:(.*)$|mi', $plugin_data, $text_domain );
+	preg_match( '|Domain Path:(.*)$|mi', $plugin_data, $domain_path );
+	preg_match( '|Requires at least:(.*)$|mi', $plugin_data, $requires );
+	preg_match( '|Tested up to:(.*)$|mi', $plugin_data, $tested );
 
 	// Normalise the path to the plugin
 	$plugin_file = str_replace( '\\', '/', $plugin_file );
@@ -189,26 +209,71 @@ function bb_get_plugin_data( $plugin_file )
 		}
 	}
 
-	$r = array(
-		'location' => $location,
-		'name' => $plugin_name,
-		'uri' => $plugin_uri,
-		'description' => $description,
-		'author' => $author_name,
-		'author_uri' => $author_uri,
-		'requires' => $requires,
-		'tested' => $tested,
-		'version' => $version
+	$plugins_allowedtags = array('a' => array('href' => array(),'title' => array()),'abbr' => array('title' => array()),'acronym' => array('title' => array()),'code' => array(),'em' => array(),'strong' => array());
+
+	$fields = array(
+		'location' => '',
+		'name' => 'html',
+		'uri' => 'url',
+		'version' => 'text',
+		'description' => 'html',
+		'author' => 'html',
+		'author_uri' => 'url',
+		'text_domain' => '',
+		'domain_path' => '',
+		'requires' => 'text',
+		'tested' => 'text',
 	);
+	foreach ( $fields as $field => $san ) {
+		if ( !empty( ${$field} ) ) {
+			${$field} = trim(${$field}[1]);
+			switch ( $san ) {
+			case 'html' :
+				${$field} = bb_filter_kses( ${$field} );
+				break;
+			case 'text' :
+				${$field} = wp_specialchars(  ${$field} );
+				break;
+			case 'url' :
+				${$field} = clean_url( ${$field} );
+				break;
+			}
+		} else {
+			${$field} = '';
+		}
+	}
 
-	$r['plugin_link'] = ( $plugin_uri ) ?
-		"<a href='$plugin_uri' title='" . attribute_escape( __('Visit plugin homepage') ) . "'>$plugin_name</a>" :
-		$plugin_name;
-	$r['author_link'] = ( $author_name && $author_uri ) ?
-		"<a href='$author_uri' title='" . attribute_escape( __('Visit author homepage') ) . "'>$author_name</a>" :
-		$author_name;
+	$plugin_data = compact( array_keys( $fields ) );
 
-	return $r;
+	if ( $translate )
+		$plugin_data = _bb_get_plugin_data_translate( $plugin_data );
+
+	if ( $markup )
+		$plugin_data['description'] = bb_autop( $plugin_data['description'] );
+
+	$plugin_data['plugin_link'] = ( $plugin_data['uri'] ) ?
+		"<a href='{$plugin_data['plugin_uri']}' title='" . attribute_escape( __('Visit plugin homepage') ) . "'>{$plugin_data['name']}</a>" :
+		$plugin_data['name'];
+	$plugin_data['author_link'] = ( $plugin_data['author'] && $plugin_data['author_uri'] ) ?
+		"<a href='{$plugin_data['author_uri']}' title='" . attribute_escape( __('Visit author homepage') ) . "'>{$plugin_data['author']}</a>" :
+		$plugin_data['author'];
+
+	return $plugin_data;
+}
+
+function _bb_get_plugin_data_translate( $plugin_data ) {
+	//Translate fields
+	if( !empty($plugin_data['text_domain']) ) {
+		if( ! empty( $plugin_data['domain_path'] ) )
+			load_plugin_textdomain($plugin_data['text_domain'], dirname($plugin_file). $plugin_data['domain_path']);
+		else
+			load_plugin_textdomain($plugin_data['text_domain'], dirname($plugin_file));
+
+		foreach ( array('name', 'plugin_url', 'description', 'author', 'author_uri', 'version') as $field )
+			$plugin_data[$field] = translate($plugin_data[$field], $plugin_data['text_domain']);
+	}
+
+	return $plugin_data;
 }
 
 /**
