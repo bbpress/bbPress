1658d9603c90e28b784f29168a236c45db030278
Sam Bauers <sambauers@git.wordpress.org>
2009-05-15 00:54:00 +0000

    Better entities escaping 0.9 branch, props westi - see #WP9821
    git-svn-id: https://bbpress.svn.wordpress.org/branches/0.9@2088 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/wp-functions.php
+++ bb-includes/wp-functions.php
@@ -116,7 +116,7 @@ function wp_specialchars( $string, $quote_style = ENT_NOQUOTES, $charset = false
 	// Handle double encoding ourselves
 	if ( !$double_encode ) {
 		$string = wp_specialchars_decode( $string, $_quote_style );
-		$string = preg_replace( '/&(#?x?[0-9]+|[a-z]+);/i', '|wp_entity|$1|/wp_entity|', $string );
+		$string = preg_replace( '/&(#?x?[0-9a-z]+);/i', '|wp_entity|$1|/wp_entity|', $string );
 	}
 
 	$string = htmlspecialchars( $string, $quote_style, $charset );
