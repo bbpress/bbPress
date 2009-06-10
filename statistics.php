2d3ba1a7ee292769a313d76f0693670b3fbd6173
Sam Bauers <sambauers@git.wordpress.org>
2009-06-10 13:44:36 +0000

    Remove hardcoded title separator, fixes #1098, props timskii
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@2149 9866e705-20ec-0310-96e7-cbb4277adcfb

--- statistics.php
+++ statistics.php
@@ -6,7 +6,7 @@ require_once( BB_PATH . BB_INC . 'functions.bb-statistics.php' );
 
 $popular = get_popular_topics();
 
-$bb->static_title = __('Statistics') . ' &laquo;';
+$bb->static_title = __('Statistics');
 
 bb_load_template( 'stats.php', array('popular') );
 
