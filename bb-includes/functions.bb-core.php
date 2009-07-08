6bcf8839bbc9a80341403f786075eb7cc2cef99c
Sam Bauers <sambauers@git.wordpress.org>
2009-07-08 04:08:29 +0000

    Better localisation on bb_since()
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@2301 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/functions.bb-core.php
+++ bb-includes/functions.bb-core.php
@@ -363,34 +363,40 @@ function bb_since( $original, $do_more = 0 ) {
 		
 	// array of time period chunks
 	$chunks = array(
-		array(60 * 60 * 24 * 365 , __('year') , __('years')),
-		array(60 * 60 * 24 * 30 , __('month') , __('months')),
-		array(60 * 60 * 24 * 7, __('week') , __('weeks')),
-		array(60 * 60 * 24 , __('day') , __('days')),
-		array(60 * 60 , __('hour') , __('hours')),
-		array(60 , __('minute') , __('minutes')),
-		array(1 , __('second') , __('seconds')),
+		( 60 * 60 * 24 * 365 ), // years
+		( 60 * 60 * 24 * 30 ),  // months
+		( 60 * 60 * 24 * 7 ),   // weeks
+		( 60 * 60 * 24 ),       // days
+		( 60 * 60 ),            // hours
+		( 60 ),                 // minutes
+		( 1 )                   // seconds
 	);
 
 	$since = $today - $original;
 
 	for ($i = 0, $j = count($chunks); $i < $j; $i++) {
-		$seconds = $chunks[$i][0];
-		$name = $chunks[$i][1];
-		$names = $chunks[$i][2];
+		$seconds = $chunks[$i];
 
 		if ( 0 != $count = floor($since / $seconds) )
 			break;
 	}
 
-	$print = sprintf(__('%1$d %2$s'), $count, (1 == $count) ? $name : $names);
+	$trans = array(
+		_n( '%d year', '%d years', $count ),
+		_n( '%d month', '%d months', $count ),
+		_n( '%d week', '%d weeks', $count ),
+		_n( '%d day', '%d days', $count ),
+		_n( '%d hour', '%d hours', $count ),
+		_n( '%d minute', '%d minutes', $count ),
+		_n( '%d second', '%d seconds', $count )
+	);
+
+	$print = sprintf( $trans[$i], $count );
 
 	if ( $do_more && $i + 1 < $j) {
-		$seconds2 = $chunks[$i + 1][0];
-		$name2 = $chunks[$i + 1][1];
-		$names2 = $chunks[$i + 1][2];
+		$seconds2 = $chunks[$i + 1];
 		if ( 0 != $count2 = floor( ($since - $seconds * $count) / $seconds2) )
-			$print .= sprintf(__(', %1$d %2$s'), $count2, (1 == $count2) ? $name2 : $names2);
+			$print .= sprintf( $trans[$i + 1], $count2 );
 	}
 	return $print;
 }
