1bac5fbde40c881acd5d6d323c8dbbdbc88cf59b
Sam Bauers <sambauers@git.wordpress.org>
2009-07-28 16:29:17 +0000

    Number format on total topics and don't apply formatting to get_XXX() functions, fixes #1145, props simonwheatley, props _ck_
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@2331 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/defaults.bb-filters.php
+++ bb-includes/defaults.bb-filters.php
@@ -36,13 +36,13 @@ add_filter( 'bb_pre_forum_name', 'bb_trim_for_db_150', 9999 );
 add_filter( 'pre_term_name', 'bb_trim_for_db_55', 9999 );
 
 // Format Strings for Display
-$filters = array( 'get_forum_name', 'topic_title', 'bb_title', 'bb_option_name' );
+$filters = array( 'forum_name', 'topic_title', 'bb_title', 'bb_option_name' );
 foreach ( $filters as $filter ) {
 	add_filter( $filter, 'esc_html' );
 }
 
 // Numbers
-$filters = array( 'get_forum_topics', 'get_forum_posts', 'total_posts', 'total_users' );
+$filters = array( 'forum_topics', 'forum_posts', 'total_posts', 'total_users', 'total_topics' );
 foreach ( $filters as $filter ) {
 	add_filter( $filter, 'bb_number_format_i18n' );
 }
