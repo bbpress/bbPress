fe7bdf9bff8f37afc483a6dc30b3f9aadbf24805
Sam Bauers <sambauers@git.wordpress.org>
2009-08-12 06:19:11 +0000

    First pass at faster recounting - no improved tag recounting yet (that will be harder). See #1146. Props _ck_.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@2353 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ bb-admin/includes/functions.bb-recount.php
@@ -0,0 +1,253 @@
+<?php
+
+function bb_recount_topic_posts()
+{
+	global $bbdb;
+
+	$statement = __( 'Counting the number of posts in each topic&hellip; %s' );
+	$result = __( 'Failed!' );
+
+	$sql = "INSERT INTO `$bbdb->topics` (`topic_id`, `topic_posts`) (SELECT `topic_id`, COUNT(`post_status`) as `topic_posts` FROM `$bbdb->posts` WHERE `post_status` = '0' GROUP BY `topic_id`) ON DUPLICATE KEY UPDATE `topic_posts` = VALUES(`topic_posts`);";
+	if ( is_wp_error( $bbdb->query( $sql ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$result = __( 'Complete!' );
+	return sprintf( $statement, $result );
+}
+
+function bb_recount_topic_voices()
+{
+	global $bbdb;
+
+	$statement = __( 'Counting the number of voices in each topic&hellip; %s' );
+	$result = __( 'Failed!' );
+
+	$sql_delete = "DELETE FROM `$bbdb->meta` WHERE `object_type` = 'bb_topic' AND `meta_key` = 'voices_count';";
+	if ( is_wp_error( $bbdb->query( $sql_delete ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$sql = "INSERT INTO `$bbdb->meta` (`object_type`, `object_id`, `meta_key`, `meta_value`) (SELECT 'bb_topic', `topic_id`, 'voices_count', COUNT(DISTINCT `poster_id`) as `meta_value` FROM `$bbdb->posts` WHERE `post_status` = '0' GROUP BY `topic_id`);";
+	if ( is_wp_error( $bbdb->query( $sql ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$result = __( 'Complete!' );
+	return sprintf( $statement, $result );
+}
+
+function bb_recount_topic_deleted_posts()
+{
+	global $bbdb;
+
+	$statement = __( 'Counting the number of deleted posts in each topic&hellip; %s' );
+	$result = __( 'Failed!' );
+
+	$sql_delete = "DELETE FROM `$bbdb->meta` WHERE `object_type` = 'bb_topic' AND `meta_key` = 'deleted_posts';";
+	if ( is_wp_error( $bbdb->query( $sql_delete ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$sql = "INSERT INTO `$bbdb->meta` (`object_type`, `object_id`, `meta_key`, `meta_value`) (SELECT 'bb_topic', `topic_id`, 'deleted_posts', COUNT(`post_status`) as `meta_value` FROM `$bbdb->posts` WHERE `post_status` != '0' GROUP BY `topic_id`);";
+	if ( is_wp_error( $bbdb->query( $sql ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$result = __( 'Complete!' );
+	return sprintf( $statement, $result );
+}
+
+function bb_recount_forum_topics()
+{
+	global $bbdb;
+
+	$statement = __( 'Counting the number of topics in each forum&hellip; %s' );
+	$result = __( 'Failed!' );
+
+	$sql = "INSERT INTO `$bbdb->forums` (`forum_id`, `topics`) (SELECT `forum_id`, COUNT(`topic_status`) as `topics` FROM `$bbdb->topics` WHERE `topic_status` = '0' GROUP BY `forum_id`) ON DUPLICATE KEY UPDATE `topics` = VALUES(`topics`);";
+	if ( is_wp_error( $bbdb->query( $sql ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$result = __( 'Complete!' );
+	return sprintf( $statement, $result );
+}
+
+function bb_recount_forum_posts()
+{
+	global $bbdb;
+
+	$statement = __( 'Counting the number of posts in each forum&hellip; %s' );
+	$result = __( 'Failed!' );
+
+	$sql = "INSERT INTO `$bbdb->forums` (`forum_id`, `posts`) (SELECT `forum_id`, COUNT(`post_status`) as `posts` FROM `$bbdb->posts` WHERE `post_status` = '0' GROUP BY `forum_id`) ON DUPLICATE KEY UPDATE `posts` = VALUES(`posts`);";
+	if ( is_wp_error( $bbdb->query( $sql ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$result = __( 'Complete!' );
+	return sprintf( $statement, $result );
+}
+
+function bb_recount_user_topics_replied()
+{
+	global $bbdb;
+
+	$statement = __( 'Counting the number of topics to which each user has replied&hellip; %s' );
+	$result = __( 'Failed!' );
+
+	$sql_select = "SELECT `poster_id`, COUNT(DISTINCT `topic_id`) as `_count` FROM `$bbdb->posts` WHERE `post_status` = '0' GROUP BY `poster_id`;";
+	$insert_rows = $bbdb->get_results( $sql_select );
+
+	if ( is_wp_error( $insert_rows ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$meta_key = $bbdb->prefix . 'topics_replied';
+
+	$insert_values = array();
+	foreach ( $insert_rows as $insert_row ) {
+		$insert_values[] = "('$insert_row->poster_id', '$meta_key', '$insert_row->_count')";
+	}
+
+	if ( !count( $insert_values ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$sql_delete = "DELETE FROM `$bbdb->usermeta` WHERE `meta_key` = '$meta_key';";
+	if ( is_wp_error( $bbdb->query( $sql_delete ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$insert_values = "\n" . join( ",\n", $insert_values );
+	$sql_insert = "INSERT INTO `$bbdb->usermeta` (`user_id`, `meta_key`, `meta_value`) VALUES $insert_values;";
+
+	if ( is_wp_error( $bbdb->query( $sql_insert ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$result = __( 'Complete!' );
+	return sprintf( $statement, $result );
+}
+
+function bb_recount_topic_tags()
+{
+	// Reset tag count to zero
+	$bbdb->query( "UPDATE $bbdb->topics SET tag_count = 0" );
+
+	// Get all tags
+	$terms = $wp_taxonomy_object->get_terms( 'bb_topic_tag' );
+
+	if ( !is_wp_error( $terms ) && is_array( $terms ) ) {
+		$message = __('Counted topic tags');
+		foreach ( $terms as $term ) {
+			$topic_ids = bb_get_tagged_topic_ids( $term->term_id );
+			if ( !is_wp_error( $topic_ids ) && is_array( $topic_ids ) ) {
+				$bbdb->query(
+					"UPDATE $bbdb->topics SET tag_count = tag_count + 1 WHERE topic_id IN (" . join( ',', $topic_ids ) . ")"
+				);
+			}
+			unset( $topic_ids );
+		}
+	}
+	unset( $terms, $term );
+
+	return $message;
+}
+
+function bb_recount_tag_topics()
+{
+	// Get all tags
+	$terms = $wp_taxonomy_object->get_terms( 'bb_topic_tag', array( 'hide_empty' => false ) );
+
+	if ( !is_wp_error( $terms ) && is_array( $terms ) ) {
+		$message = __('Counted tagged topics');
+		$_terms = array();
+		foreach ( $terms as $term ) {
+			$_terms[] = $term->term_id;
+		}
+		if ( count( $_terms ) ) {
+			$wp_taxonomy_object->update_term_count( $_terms, 'bb_topic_tag' );
+		}
+	}
+	unset( $term, $_terms );
+
+	return $message;
+}
+
+function bb_recount_tag_delete_empty()
+{
+	// Get all tags
+	if ( !isset( $terms ) ) {
+		$terms = $wp_taxonomy_object->get_terms( 'bb_topic_tag', array( 'hide_empty' => false ) );
+	}
+
+	if ( !is_wp_error( $terms ) && is_array( $terms ) ) {
+		$message = __('Deleted tags with no topics');
+		foreach ( $terms as $term ) {
+			$topic_ids = bb_get_tagged_topic_ids( $term->term_id );
+			if ( !is_wp_error( $topic_ids ) && is_array( $topic_ids ) ) {
+				if ( false === $topic_ids || ( is_array( $topic_ids ) && !count( $topic_ids ) ) ) {
+					bb_destroy_tag( $term->term_taxonomy_id );
+				}
+			}
+			unset( $topic_ids );
+		}
+	}
+	unset( $terms, $term );
+
+	return $message;
+}
+
+function bb_recount_clean_favorites()
+{
+	global $bbdb;
+
+	$statement = __( 'Removing deleted topics from user favorites&hellip; %s' );
+	$result = __( 'Failed!' );
+
+	$meta_key = $bbdb->prefix . 'favorites';
+
+	$users = $bbdb->get_results( "SELECT `user_id`, `meta_value` AS `favorites` FROM `$bbdb->usermeta` WHERE `meta_key` = '$meta_key';" );
+	if ( is_wp_error( $users ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$topics = $bbdb->get_col( "SELECT `topic_id` FROM `$bbdb->topics` WHERE `topic_status` = '0';" );
+
+	if ( is_wp_error( $topics ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$values = array();
+	foreach ( $users as $user ) {
+		if ( empty( $user->favorites ) || !is_string( $user->favorites ) ) {
+			continue;
+		}
+		$favorites = explode( ',', $user->favorites );
+		if ( empty( $favorites ) || !is_array( $favorites ) ) {
+			continue;
+		}
+		$favorites = join( ',', array_intersect( $topics, $favorites ) );
+		$values[] = "('$user->user_id', '$meta_key', '$favorites')";
+	}
+
+	if ( !count( $values ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$sql_delete = "DELETE FROM `$bbdb->usermeta` WHERE `meta_key` = '$meta_key';";
+	if ( is_wp_error( $bbdb->query( $sql_delete ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$values = "\n" . join( ",\n", $values );
+	$sql_insert = "INSERT INTO `$bbdb->usermeta` (`user_id`, `meta_key`, `meta_value`) VALUES $values;";
+	if ( is_wp_error( $bbdb->query( $sql_insert ) ) ) {
+		return sprintf( $statement, $result );
+	}
+
+	$result = __( 'Complete!' );
+	return sprintf( $statement, $result );
+}
--- bb-admin/tools-recount.php
+++ bb-admin/tools-recount.php
@@ -1,164 +1,59 @@
 <?php
-require_once('admin.php');
+require_once( 'admin.php' );
+require_once( 'includes/functions.bb-recount.php' );
 
 if ( 'post' == strtolower( $_SERVER['REQUEST_METHOD'] ) ) {
 	bb_check_admin_referer( 'do-counts' );
 
+	// Stores messages
 	$messages = array();
-	if ( isset($_POST['topic-posts']) && 1 == $_POST['topic-posts'] ) {
-		if ( $topics = (array) $bbdb->get_results("SELECT topic_id, COUNT(post_id) AS count FROM $bbdb->posts WHERE post_status = '0' GROUP BY topic_id") ) {
-			$messages[] = __('Counted posts');
-			foreach ($topics as $topic) {
-				$topic_id = (int) $topic->topic_id;
-				$bbdb->query( $bbdb->prepare( "UPDATE $bbdb->topics SET topic_posts = %s WHERE topic_id = %s" ), $topic->count, $topic_id );
-			}
-			unset($topics, $topic, $topic_id);
-		}
+
+	if ( !empty( $_POST['topic-posts'] ) ) {
+		$messages[] = bb_recount_topic_posts();
 	}
 
-	if ( isset($_POST['topic-voices']) && 1 == $_POST['topic-voices'] ) {
-		if ( $topics = (array) $bbdb->get_results("SELECT topic_id FROM $bbdb->topics ORDER BY topic_id") ) {
-			$messages[] = __('Counted voices');
-			foreach ($topics as $topic) {
-				$topic_id = (int) $topic->topic_id;
-				if ( $voices = $bbdb->get_col( $bbdb->prepare( "SELECT DISTINCT poster_id FROM $bbdb->posts WHERE topic_id = %s AND post_status = '0';", $topic_id ) ) ) {
-					$voices = count( $voices );
-					bb_update_topicmeta( $topic_id, 'voices_count', $voices );
-				}
-			}
-			unset($topics, $topic, $topic_id);
-		}
+	if ( !empty( $_POST['topic-voices'] ) ) {
+		$messages[] = bb_recount_topic_voices();
 	}
 
-	if ( isset($_POST['topic-deleted-posts']) && 1 == $_POST['topic-deleted-posts'] ) {
-		$old = (array) $bbdb->get_col("SELECT object_id FROM $bbdb->meta WHERE object_type = 'bb_topics' AND meta_key = 'deleted_posts'");
-		$old = array_flip($old);
-		if ( $topics = (array) $bbdb->get_results("SELECT topic_id, COUNT(post_id) AS count FROM $bbdb->posts WHERE post_status != '0' GROUP BY topic_id") ) {
-			$messages[] = __('Counting deleted posts&#8230;');
-			foreach ( $topics as $topic ) {
-				bb_update_topicmeta( $topic->topic_id, 'deleted_posts', $topic->count );
-				unset($old[$topic->topic_id]);
-			}
-			unset($topics, $topic);
-		}
-		if ( $old ) {
-			$old = join(',', array_flip($old));
-			$bbdb->query("DELETE FROM $bbdb->meta WHERE object_type = 'bb_topic' AND object_id IN ($old) AND meta_key = 'deleted_posts'");
-			$messages[] = __('&#8230;counted deleted posts');
-		} else {
-			$messages[] = __('&#8230;no deleted posts to count');
-		}
+	if ( !empty( $_POST['topic-deleted-posts'] ) ) {
+		$messages[] = bb_recount_topic_deleted_posts();
 	}
 
-	if ( isset($_POST['forums']) && 1 == $_POST['forums'] ) {
-		if ( $all_forums = (array) $bbdb->get_col("SELECT forum_id FROM $bbdb->forums") ) {
-			$messages[] = __('Counted forum topics and posts');
-			$all_forums = array_flip( $all_forums );
-			$forums = $bbdb->get_results("SELECT forum_id, COUNT(topic_id) AS topic_count, SUM(topic_posts) AS post_count FROM $bbdb->topics WHERE topic_status = 0 GROUP BY forum_id");
-			foreach ( (array) $forums as $forum ) {
-				$bbdb->query("UPDATE $bbdb->forums SET topics = '$forum->topic_count', posts = '$forum->post_count' WHERE forum_id = '$forum->forum_id'");
-				unset($all_forums[$forum->forum_id]);
-			}
-			if ( $all_forums ) {
-				$all_forums = implode(',', array_flip( $all_forums ) );
-				$bbdb->query("UPDATE $bbdb->forums SET topics = 0, posts = 0 WHERE forum_id IN ($all_forums)");
-			}
-			unset($all_forums, $forums, $forum);
-		}
+	if ( !empty( $_POST['forums'] ) ) {
+		$messages[] = bb_recount_forum_topics();
+		$messages[] = bb_recount_forum_posts();
 	}
 
-	if ( isset($_POST['topics-replied']) && 1 == $_POST['topics-replied'] ) {
-		if ( $users = (array) $bbdb->get_col("SELECT ID FROM $bbdb->users") ) {
-			$messages[] = __('Counted topics to which each user has replied');
-			foreach ( $users as $user )
-				bb_update_topics_replied( $user );
-			unset($users, $user);
-		}
+	if ( !empty( $_POST['topics-replied'] ) ) {
+		$messages[] = bb_recount_user_topics_replied();
 	}
 
-	if ( isset($_POST['topic-tag-count']) && 1 == $_POST['topic-tag-count'] ) {
-		// Reset tag count to zero
-		$bbdb->query( "UPDATE $bbdb->topics SET tag_count = 0" );
-
-		// Get all tags
-		$terms = $wp_taxonomy_object->get_terms( 'bb_topic_tag' );
-
-		if ( !is_wp_error( $terms ) && is_array( $terms ) ) {
-			$messages[] = __('Counted topic tags');
-			foreach ( $terms as $term ) {
-				$topic_ids = bb_get_tagged_topic_ids( $term->term_id );
-				if ( !is_wp_error( $topic_ids ) && is_array( $topic_ids ) ) {
-					$bbdb->query(
-						"UPDATE $bbdb->topics SET tag_count = tag_count + 1 WHERE topic_id IN (" . join( ',', $topic_ids ) . ")"
-					);
-				}
-				unset( $topic_ids );
-			}
-		}
-		unset( $terms, $term );
+	if ( !empty( $_POST['topic-tag-count'] ) ) {
+		$messages[] = bb_recount_topic_tags();
 	}
 
-	if ( isset($_POST['tags-tag-count']) && 1 == $_POST['tags-tag-count'] ) {
-		// Get all tags
-		$terms = $wp_taxonomy_object->get_terms( 'bb_topic_tag', array( 'hide_empty' => false ) );
-
-		if ( !is_wp_error( $terms ) && is_array( $terms ) ) {
-			$messages[] = __('Counted tagged topics');
-			$_terms = array();
-			foreach ( $terms as $term ) {
-				$_terms[] = $term->term_id;
-			}
-			if ( count( $_terms ) ) {
-				$wp_taxonomy_object->update_term_count( $_terms, 'bb_topic_tag' );
-			}
-		}
-		unset( $term, $_terms );
+	if ( !empty( $_POST['tags-tag-count'] ) ) {
+		$messages[] = bb_recount_tag_topics();
 	}
 
-	if ( isset($_POST['tags-delete-empty']) && 1 == $_POST['tags-delete-empty'] ) {
-		// Get all tags
-		if ( !isset( $terms ) ) {
-			$terms = $wp_taxonomy_object->get_terms( 'bb_topic_tag', array( 'hide_empty' => false ) );
-		}
-
-		if ( !is_wp_error( $terms ) && is_array( $terms ) ) {
-			$messages[] = __('Deleted tags with no topics');
-			foreach ( $terms as $term ) {
-				$topic_ids = bb_get_tagged_topic_ids( $term->term_id );
-				if ( !is_wp_error( $topic_ids ) && is_array( $topic_ids ) ) {
-					if ( false === $topic_ids || ( is_array( $topic_ids ) && !count( $topic_ids ) ) ) {
-						bb_destroy_tag( $term->term_taxonomy_id );
-					}
-				}
-				unset( $topic_ids );
-			}
-		}
-		unset( $terms, $term );
+	if ( !empty( $_POST['tags-delete-empty'] ) ) {
+		$messages[] = bb_recount_tag_delete_empty();
 	}
 
-	if ( isset($_POST['clean-favorites']) && 1 == $_POST['clean-favorites'] ) {
-		$favorites_key = $bbdb->prefix . 'favorites';
-		if ( $users = $bbdb->get_results("SELECT user_id AS id, meta_value AS favorites FROM $bbdb->usermeta WHERE meta_key = '" . $favorites_key . "'") ) {
-			$messages[] = __('Removed deleted topics from users\' favorites');
-			$topics = $bbdb->get_col("SELECT topic_id FROM $bbdb->topics WHERE topic_status = '0'");
-			foreach ( $users as $user ) {
-				foreach ( explode(',', $user->favorites) as $favorite ) {
-					if ( !in_array($favorite, $topics) ) {
-						bb_remove_user_favorite( $user->id, $favorite );
-					}
-				}
-			}
-			unset($topics, $users, $user, $favorite);
-		}
+	if ( !empty( $_POST['clean-favorites'] ) ) {
+		$messages[] = bb_recount_clean_favorites();
 	}
 
 	bb_recount_list();
 	foreach ( (array) $recount_list as $item ) {
 		if ( isset($item[2]) && isset($_POST[$item[0]]) && 1 == $_POST[$item[0]] && is_callable($item[2]) ) {
-			call_user_func( $item[2] );
+			$messages[] = call_user_func( $item[2] );
 		}
 	}
-	
+
+	wp_cache_flush();
+
 	if ( count( $messages ) ) {
 		$messages = join( '</p>' . "\n" . '<p>', $messages );
 		bb_admin_notice( $messages );
@@ -195,6 +90,14 @@ if ( $recount_list ) {
 			</div>
 		</div>
 <?php
+	bb_option_form_element( 'row_limit', array(
+		'title' => __( 'Maximum rows per query' ),
+		'class' => array( 'short' ),
+		'note' => array(
+			__( 'The maximum number of rows to retrieve in any one database query, leave blank for no limit.' ),
+			__( 'Using this will caues bbPress to make smaller queries in larger numbers, which is usually slower, but useful if recounting is causing bbPress to run out of memory.' )
+		)
+	) );
 } else {
 ?>
 		<p><?php _e( 'There are no re-count tools available.' ) ?></p>
