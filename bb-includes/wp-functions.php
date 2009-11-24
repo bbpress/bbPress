98960798948f2d943df33b0a61b88d42b3295c31
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2009-11-24 23:43:07 +0000

    better make_clickable() from WP for 0.9
    git-svn-id: https://bbpress.svn.wordpress.org/branches/0.9@2380 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/wp-functions.php
+++ bb-includes/wp-functions.php
@@ -467,23 +467,54 @@ function force_balance_tags( $text ) {
 }
 endif;
 
-if ( !function_exists('make_clickable') ) : // [WP4387]
+if ( !function_exists('make_clickable') ) : // [WP12250]
 function make_clickable($ret) {
 	$ret = ' ' . $ret;
 	// in testing, using arrays here was found to be faster
-	$ret = preg_replace(
-		array(
-			'#([\s>])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is',
-			'#([\s>])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#is',
-			'#([\s>])([a-z0-9\-_.]+)@([^,< \n\r]+)#i'),
-		array(
-			'$1<a href="$2" rel="nofollow">$2</a>',
-			'$1<a href="http://$2" rel="nofollow">$2</a>',
-			'$1<a href="mailto:$2@$3">$2@$3</a>'),$ret);
+	$ret = preg_replace_callback('#(?<=[\s>])(\()?([\w]+?://(?:[\w\\x80-\\xff\#$%&~/=?@\[\](+-]|[.,;:](?![\s<]|(\))?([\s]|$))|(?(1)\)(?![\s<.,;:]|$)|\)))+)#is', '_make_url_clickable_cb', $ret);
+	$ret = preg_replace_callback('#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]+)#is', '_make_web_ftp_clickable_cb', $ret);
+	$ret = preg_replace_callback('#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', '_make_email_clickable_cb', $ret);
 	// this one is not in an array because we need it to run last, for cleanup of accidental links within links
 	$ret = preg_replace("#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $ret);
 	$ret = trim($ret);
 	return $ret;
+}
+endif;
+
+if ( !function_exists( '_make_url_clickable_cb' ) ) : // [WP12088]
+function _make_url_clickable_cb($matches) {
+	$url = $matches[2];
+
+	$url = esc_url($url);
+	if ( empty($url) )
+        	return $matches[0];
+
+	return $matches[1] . "<a href=\"$url\" rel=\"nofollow\">$url</a>";
+}
+endif;
+
+if ( !function_exists( '_make_web_ftp_clickable_cb' ) ) : // [WP11844]
+function _make_web_ftp_clickable_cb($matches) {
+	$ret = '';
+	$dest = $matches[2];
+	$dest = 'http://' . $dest;
+	$dest = esc_url($dest);                                                       
+	if ( empty($dest) )
+		return $matches[0];
+
+	// removed trailing [.,;:)] from URL
+	if ( in_array( substr($dest, -1), array('.', ',', ';', ':', ')') ) === true ) {
+		$ret = substr($dest, -1);
+		$dest = substr($dest, 0, strlen($dest)-1);
+	}
+	return $matches[1] . "<a href=\"$dest\" rel=\"nofollow\">$dest</a>$ret";
+}
+endif;
+
+if ( !function_exists( '_make_email_clickable_cb' ) ) : // [WP6449]
+function _make_email_clickable_cb($matches) {
+	$email = $matches[2] . '@' . $matches[3];
+	return $matches[1] . "<a href=\"mailto:$email\">$email</a>";
 }
 endif;
 
