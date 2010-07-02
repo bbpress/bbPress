3d8939229fa1698ade44987a1744f8e6115b9c76
chrishajer <chrishajer@git.wordpress.org>
2010-07-02 18:09:16 +0000

    Remove redundant code.  Ref changeset 2453. Props !GautamGupta
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@2454 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/functions.bb-capabilities.php
+++ bb-includes/functions.bb-capabilities.php
@@ -27,8 +27,6 @@ function bb_current_user_can($capability) {
 		$retvalue = false;
 		if ( ( $capability == 'write_topic' || $capability == 'write_topics' ) && !bb_is_login_required() )
 			$retvalue = true;
-		if ( ( $capability == 'write_topic' || $capability == 'write_topics' ) && !bb_is_login_required() )
-			$retvalue = true;
 	} else {
 		$retvalue = call_user_func_array(array(&$bb_current_user, 'has_cap'), $args);
 	}
