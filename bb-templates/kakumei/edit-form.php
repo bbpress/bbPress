cf2d29f2a821e8f90664a151f70e411714741dfc
chrishajer <chrishajer@git.wordpress.org>
2010-07-07 18:09:51 +0000

    Add 'for' attribute to labels in edit-form.php. Fixes #1307.  Props mr_pelle
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@2457 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-templates/kakumei/edit-form.php
+++ bb-templates/kakumei/edit-form.php
@@ -1,13 +1,13 @@
 
 <?php if ( $topic_title ) : ?>
 <p role="main">
-  <label><?php _e('Topic:'); ?><br />
+  <label for="topic"><?php _e('Topic:'); ?><br />
 
   <input name="topic" type="text" id="topic" size="50" maxlength="80"  value="<?php echo esc_attr( get_topic_title() ); ?>" />
 </label>
 </p>
 <?php endif; do_action( 'edit_form_pre_post' ); ?>
-<p><label><?php _e('Post:'); ?><br />
+<p><label for="post_content"><?php _e('Post:'); ?><br />
   <textarea name="post_content" cols="50" rows="8" id="post_content"><?php echo apply_filters('edit_text', get_post_text() ); ?></textarea>
   </label>
 </p>
