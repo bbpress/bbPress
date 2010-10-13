ea4423646a5d31aaf0e311b2ee871b5be664b80f
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2010-10-13 06:32:59 +0000

    Use post_modified_time for now. Needs API.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@2543 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-templatetags.php
+++ bbp-templatetags.php
@@ -228,7 +228,7 @@ function bbp_forum_last_active ( $forum_id = 0 ) {
 		if ( !$forum_id )
 			$forum_id = bbp_get_forum_id();
 
-		return apply_filters( 'bbp_get_forum_last_active', get_post_meta( $forum_id, 'bbp_forum_last_active', true ) );
+		return apply_filters( 'bbp_get_forum_last_active', get_post_modified_time( 'U', false, $forum_id ) );
 	}
 
 /**
@@ -684,7 +684,7 @@ function bbp_topic_last_active ( $topic_id = '' ) {
 		if ( !$topic_id )
 			$topic_id = bbp_get_topic_id();
 
-		return apply_filters( 'bbp_get_topic_last_active', get_post_meta( $topic_id, 'bbp_topic_last_active', true ) );
+		return apply_filters( 'bbp_get_topic_last_active', get_post_modified_time( 'U', false, $topic_id ) );
 	}
 
 /**
