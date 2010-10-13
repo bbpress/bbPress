6e0aeb572a5e5c9e7509761bf2ed21ccb1bca75e
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2010-10-13 17:35:56 +0000

    Introduce bbp_replies template part. Fill in bbp_topic template emptiness.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@2552 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-themes/bbp-twentyten/single-bbp_topic.php
+++ bbp-themes/bbp-twentyten/single-bbp_topic.php
@@ -1,3 +1,29 @@
 <?php
-
+/**
+ * bbPress Single Topic
+ *
+ * @package bbPress
+ * @subpackage Template
+ */
 ?>
+
+<?php get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+				<div id="topic-<?php bbp_topic_id(); ?>" class="bbp-topic-info">
+					<h1 class="entry-title"><?php bbp_topic_title(); ?></h1>
+					<div class="entry-content">
+
+						<?php the_content(); ?>
+
+					</div>
+				</div><!-- #topic-<?php bbp_topic_id(); ?> -->
+
+				<?php get_template_part( 'loop', 'bbp_replies' ); ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_footer(); ?>
