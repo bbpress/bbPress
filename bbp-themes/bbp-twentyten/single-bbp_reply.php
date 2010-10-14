263370ff5a7658acd9597afbdc96545f0b738734
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2010-10-14 04:36:37 +0000

    Fill in single-bbp_reply template file
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@2560 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-themes/bbp-twentyten/single-bbp_reply.php
+++ bbp-themes/bbp-twentyten/single-bbp_reply.php
@@ -1,3 +1,21 @@
 <?php
-
+/**
+ * bbPress Single Reply
+ *
+ * @package bbPress
+ * @subpackage Template
+ */
 ?>
+
+<?php get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+				<?php get_template_part( 'loop', 'bbp_replies' ); ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
