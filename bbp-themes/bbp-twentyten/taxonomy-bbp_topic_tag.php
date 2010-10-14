5fc95840e938184346a19ab5d108fe119a40974c
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2010-10-14 04:35:39 +0000

    Fill in some blanks on taxonomy-bbp_topic_tag template file
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@2559 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-themes/bbp-twentyten/taxonomy-bbp_topic_tag.php
+++ bbp-themes/bbp-twentyten/taxonomy-bbp_topic_tag.php
@@ -1,3 +1,30 @@
 <?php
-
+/**
+ * bbPress Topic Tag
+ *
+ * @package bbPress
+ * @subpackage Template
+ */
 ?>
+
+<?php get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+				<div id="topic-tag-<?php //bbp_topic_tag_id(); ?>" class="bbp-topic-tag-info">
+					<h1 class="entry-title"><?php //bbp_topic_tag_title(); ?></h1>
+					<div class="entry-content">
+
+						<?php //bbp_topic_tag_description(); ?>
+
+					</div>
+				</div><!-- #topic-tag-<?php //bbp_topic_tag_id(); ?> -->
+
+				<?php get_template_part( 'loop', 'bbp_topics' ); ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
