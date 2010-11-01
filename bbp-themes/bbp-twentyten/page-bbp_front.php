97690c553ae917c93257e1484eab66cb1707a93e
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2010-11-01 06:17:15 +0000

    Improve page-bbp_front.php
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@2587 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-themes/bbp-twentyten/page-bbp_front.php
+++ bbp-themes/bbp-twentyten/page-bbp_front.php
@@ -12,7 +12,20 @@
 		<div id="container">
 			<div id="content" role="main">
 
-				<?php get_template_part( 'loop', 'bbp_forums' ); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<div id="forum-front" class="bbp-forum-front">
+						<h1 class="entry-title"><?php bbp_title_breadcrumb(); ?></h1>
+						<div class="entry-content">
+
+							<?php the_content(); ?>
+
+							<?php get_template_part( 'loop', 'bbp_forums' ); ?>
+
+						</div>
+					</div><!-- #forum-front -->
+
+				<?php endwhile; ?>
 
 			</div><!-- #content -->
 		</div><!-- #container -->
