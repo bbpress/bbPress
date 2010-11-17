e217450e6a5722e1b82b0466044e45caa97d06ea
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2010-11-17 13:39:57 +0000

    Check caps on new topic and new reply forms
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@2622 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-themes/bbp-twentyten/form-bbp_reply.php
+++ bbp-themes/bbp-twentyten/form-bbp_reply.php
@@ -1,33 +1,44 @@
 
-<div id="new-reply-<?php bbp_topic_id(); ?>" class="bbp-reply-form">
-	<form id="new_post" name="new_post" method="post" action="">
-		<fieldset>
-			<legend>
-				<?php printf( __( 'Reply to: &ldquo;%s&rdquo;', 'bbpress' ), bbp_get_topic_title() ); ?>
-			</legend>
+<?php if ( current_user_can( 'publish_replies' ) || bbp_allow_anonymous() ) : ?>
 
-			<div class="alignleft">
-				<?php bbp_current_user_avatar( 80 ); ?>
-			</div>
+	<div id="new-reply-<?php bbp_topic_id(); ?>" class="bbp-reply-form">
+		<form id="new_post" name="new_post" method="post" action="">
+			<fieldset>
+				<legend>
+					<?php printf( __( 'Reply to: &ldquo;%s&rdquo;', 'bbpress' ), bbp_get_topic_title() ); ?>
+				</legend>
 
-			<div class="alignleft">
-				<p>
-					<label for="bbp_reply_content"><?php _e( 'Reply:', 'bbpress' ); ?></label><br />
-					<textarea id="bbp_reply_content" tabindex="3" name="bbp_reply_content" cols="52" rows="6"></textarea>
-				</p>
+				<div class="alignleft">
+					<?php bbp_current_user_avatar( 80 ); ?>
+				</div>
 
-				<p>
-					<label for="bbp_topic_tags"><?php _e( 'Tags:', 'bbpress' ); ?></label><br />
-					<input id="bbp_topic_tags" type="text" value="" tabindex="5" size="40" name="bbp_topic_tags" id="post_tags" />
-				</p>
+				<div class="alignleft">
+					<p>
+						<label for="bbp_reply_content"><?php _e( 'Reply:', 'bbpress' ); ?></label><br />
+						<textarea id="bbp_reply_content" tabindex="3" name="bbp_reply_content" cols="52" rows="6"></textarea>
+					</p>
 
-				<p align="right">
-					<button type="submit" tabindex="6" id="bbp_reply_submit" name="bbp_reply_submit"><?php _e( 'Submit', 'bbpress' ); ?></button>
-				</p>
-			</div>
+					<p>
+						<label for="bbp_topic_tags"><?php _e( 'Tags:', 'bbpress' ); ?></label><br />
+						<input id="bbp_topic_tags" type="text" value="" tabindex="5" size="40" name="bbp_topic_tags" id="post_tags" />
+					</p>
 
-			<?php bbp_new_reply_form_fields(); ?>
+					<p align="right">
+						<button type="submit" tabindex="6" id="bbp_reply_submit" name="bbp_reply_submit"><?php _e( 'Submit', 'bbpress' ); ?></button>
+					</p>
+				</div>
 
-		</fieldset>
-	</form>
-</div>
+				<?php bbp_new_reply_form_fields(); ?>
+
+			</fieldset>
+		</form>
+	</div>
+
+<?php else : ?>
+
+	<div id="no-reply-<?php bbp_topic_id(); ?>" class="bbp-no-reply">
+		<h2 class="entry-title"><?php _e( 'Sorry!', 'bbpress' ); ?></h2>
+		<div class="entry-content"><?php _e( 'You cannot reply to this topic.', 'bbpress' ); ?></div>
+	</div>
+
+<?php endif; ?>
--- bbp-themes/bbp-twentyten/form-bbp_topic.php
+++ bbp-themes/bbp-twentyten/form-bbp_topic.php
@@ -1,47 +1,59 @@
 
-<div id="new-topic-<?php bbp_topic_id(); ?>" class="bbp-topic-form">
-	<form id="new_post" name="new_post" method="post" action="">
-		<fieldset>
-			<legend><?php printf( __( 'New Topic in: &ldquo;%s&rdquo;', 'bbpress' ), bbp_get_forum_title() ); ?></legend>
+<?php if ( current_user_can( 'publish_topics' ) || bbp_allow_anonymous() ) : ?>
 
-			<div class="alignleft">
+	<div id="new-topic-<?php bbp_topic_id(); ?>" class="bbp-topic-form">
+		<form id="new_post" name="new_post" method="post" action="">
+			<fieldset>
+				<legend><?php printf( __( 'New Topic in: &ldquo;%s&rdquo;', 'bbpress' ), bbp_get_forum_title() ); ?></legend>
 
-				<?php bbp_current_user_avatar( 80 ); ?>
+				<div class="alignleft">
 
-			</div>
+					<?php bbp_current_user_avatar( 80 ); ?>
 
-			<div class="alignleft">
-				<p>
-					<label for="bbp_topic_title"><?php _e( 'Title:', 'bbpress' ); ?></label><br />
-					<input type="text" id="bbp_topic_title" value="" tabindex="1" size="40" name="bbp_topic_title" />
-				</p>
+				</div>
 
-				<p>
-					<label for="bbp_topic_content"><?php _e( 'Topic:', 'bbpress' ); ?></label><br />
-					<textarea id="bbp_topic_content" tabindex="3" name="bbp_topic_content" cols="52" rows="6"></textarea>
-				</p>
-
-				<p>
-					<label for="bbp_topic_tags"><?php _e( 'Tags:', 'bbpress' ); ?></label><br />
-					<input type="text" value="" tabindex="5" size="40" name="bbp_topic_tags" id="post_tags" />
-				</p>
-
-				<?php if ( !bbp_is_forum() ) : ?>
-
+				<div class="alignleft">
 					<p>
-						<label for="bbp_forum_id"><?php _e( 'Forum:', 'bbpress' ); ?></label><br />
-						<?php bbp_forum_dropdown(); ?>
+						<label for="bbp_topic_title"><?php _e( 'Title:', 'bbpress' ); ?></label><br />
+						<input type="text" id="bbp_topic_title" value="" tabindex="1" size="40" name="bbp_topic_title" />
 					</p>
 
-				<?php endif; ?>
+					<p>
+						<label for="bbp_topic_content"><?php _e( 'Topic:', 'bbpress' ); ?></label><br />
+						<textarea id="bbp_topic_content" tabindex="3" name="bbp_topic_content" cols="52" rows="6"></textarea>
+					</p>
 
-				<p align="right">
-					<button type="submit" tabindex="7" id="bbp_topic_submit" name="bbp_topic_submit"><?php _e( 'Submit', 'bbpress' ); ?></button>
-				</p>
-			</div>
+					<p>
+						<label for="bbp_topic_tags"><?php _e( 'Tags:', 'bbpress' ); ?></label><br />
+						<input type="text" value="" tabindex="5" size="40" name="bbp_topic_tags" id="post_tags" />
+					</p>
 
-			<?php bbp_new_topic_form_fields(); ?>
+					<?php if ( !bbp_is_forum() ) : ?>
 
-		</fieldset>
-	</form>
-</div>
+						<p>
+							<label for="bbp_forum_id"><?php _e( 'Forum:', 'bbpress' ); ?></label><br />
+							<?php bbp_forum_dropdown(); ?>
+						</p>
+
+					<?php endif; ?>
+
+					<p align="right">
+						<button type="submit" tabindex="7" id="bbp_topic_submit" name="bbp_topic_submit"><?php _e( 'Submit', 'bbpress' ); ?></button>
+					</p>
+				</div>
+
+				<?php bbp_new_topic_form_fields(); ?>
+
+			</fieldset>
+		</form>
+	</div>
+
+<?php else : ?>
+
+	<div id="no-topic-<?php bbp_topic_id(); ?>" class="bbp-no-topic">
+		<h2 class="entry-title"><?php _e( 'Sorry!', 'bbpress' ); ?></h2>
+		<div class="entry-content"><?php _e( 'You cannot create new topics at this time.', 'bbpress' ); ?></div>
+	</div>
+
+
+<?php endif; ?>
