f808833964af679183c5b6781e730331e1ff9f4e
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2010-12-03 11:20:55 +0000

    Add missing subscribed forum topics template part code.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@2673 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-themes/bbp-twentyten/profile-bbp_subscriptions.php
+++ bbp-themes/bbp-twentyten/profile-bbp_subscriptions.php
@@ -1,7 +1,22 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-?>
+				<?php if ( bbp_is_user_home() ) : ?>
+
+					<div id="bbp-author-subscriptions" class="bbp-author-subscriptions">
+						<hr />
+						<h2 class="entry-title"><?php _e( 'Subscribed Forum Topics', 'bbpress' ); ?></h2>
+						<div class="entry-content">
+
+							<?php if ( bbp_get_user_subscriptions() ) : ?>
+
+								<?php get_template_part( 'loop', 'bbp_topics' ); ?>
+
+							<?php else : ?>
+
+								<p><?php _e( 'You are not currently subscribed to any topics.', 'bbpress' ); ?></p>
+
+							<?php endif; ?>
+
+						</div>
+					</div><!-- #bbp-author-favorites -->
+
+				<?php endif; ?>
