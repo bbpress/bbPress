d24f32b7185e223500ac54d5c7fef49f38ebf554
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2011-01-07 23:20:43 +0000

    Better user edit and registration file inclusion checks.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@2776 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-includes/bbp-functions.php
+++ bbp-includes/bbp-functions.php
@@ -1815,10 +1815,11 @@ function bbp_pre_get_posts( $wp_query ) {
 			$wp_query->bbp_is_user_profile_edit = true;
 
 			// Load the required user editing functions
-			if ( version_compare( $wp_version, '3.1', '<=' ) )
+			if ( !function_exists( '_wp_get_user_contactmethods' ) )
 				include_once( ABSPATH . 'wp-includes/registration.php' );
 
-			require_once( ABSPATH . 'wp-admin/includes/user.php' );
+			if ( !function_exists( 'edit_user' ) )
+				require_once( ABSPATH . 'wp-admin/includes/user.php' );
 
 		} else {
 			$wp_query->bbp_is_user_profile_page = true;
