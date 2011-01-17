b6d5536f417bf60ab435753b5a2201ef3b5c5b1c
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2011-01-17 22:34:31 +0000

    Adjust bbp_register template file to use template part in preparation for shortcode usage.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@2813 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-themes/bbp-twentyten/page-bbp_register.php
+++ bbp-themes/bbp-twentyten/page-bbp_register.php
@@ -26,88 +26,16 @@
 
 							<?php if ( !is_user_logged_in() ) : ?>
 
-								<form method="post" action="">
-
-									<fieldset>
-										<legend><?php _e( 'Profile Information', 'bbpress' ); ?></legend>
-
-										<p><?php _e( 'Your password will be emailed to the address you provide.', 'bbpress' ); ?></p>
-
-										<?php $user_login_error = $bb_register_error->get_error_message( 'user_login' ); ?>
-
-										<table width="100%">
-											<tr class="form-field form-required required<?php if ( $user_login_error ) echo ' form-invalid error'; ?>">
-												<th scope="row">
-													<label for="user_login"><?php _e('Username'); ?></label>
-
-													<?php
-														if ( $user_login_error )
-															echo "<em>$user_login_error</em>";
-													?>
-
-												</th>
-
-												<td>
-													<input name="user_login" type="text" id="user_login" size="30" maxlength="30" value="<?php echo $user_login; ?>" />
-												</td>
-											</tr>
-
-											<?php
-
-											if ( is_array( $profile_info_keys ) ) :
-												foreach ( $profile_info_keys as $key => $label ) :
-													$class = 'form-field';
-
-													if ( $label[0] )
-														$class .= ' form-required required';
-
-													if ( $profile_info_key_error = $bb_register_error->get_error_message( $key ) )
-														$class .= ' form-invalid error'; ?>
-
-													<tr class="<?php echo $class; ?>">
-														<th scope="row">
-															<label for="<?php echo $key; ?>"><?php echo $label[1]; ?></label>
-
-															<?php
-																if ( $profile_info_key_error )
-																	echo "<em>$profile_info_key_error</em>";
-															?>
-
-														</th>
-														<td>
-															<input name="<?php echo $key; ?>" type="text" id="<?php echo $key; ?>" size="30" maxlength="140" value="<?php echo $$key; ?>" />
-														</td>
-													</tr>
-
-												<?php
-
-												endforeach; // profile_info_keys
-											endif; // profile_info_keys
-
-											?>
-
-										</table>
-
-										<p class="required-message"><?php _e('These items are <span class="required">required</span>.') ?></p>
-
-										<?php do_action( 'bbp_extra_profile_info', $user ); ?>
-
-										<p class="submit">
-											<input type="submit" name="Submit" value="<?php echo esc_attr__( 'Register &raquo;' ); ?>" />
-										</p>
-
-									</fieldset>
-
-								</form>
+								<?php get_template_part( 'form', 'bbp_user_register' ); ?>
 
 							<?php else : ?>
 
-								<p><?php _e('You&#8217;re already logged in, why do you need to register?'); ?></p>
+								<p><?php _e( 'You&#8217;re already logged in, why do you need to register?', 'bbpress' ); ?></p>
 
 							<?php endif; ?>
 
 						</div>
-					</div><!-- #bbp-login -->
+					</div><!-- #bbp-register -->
 
 				<?php endwhile; ?>
 
