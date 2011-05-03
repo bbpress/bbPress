29e6739f2859fec2653d53a719a1e0c81503cd94
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2011-05-03 06:20:19 +0000

    Enqueue the default style.css for twentyten parent template now that bbp-twentyten is a child theme again.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3084 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-themes/bbp-twentyten/functions.php
+++ bbp-themes/bbp-twentyten/functions.php
@@ -32,8 +32,11 @@ function bbp_twentyten_enqueue_styles () {
 	if ( is_admin() )
 		return false;
 
+	// TwentyTen
+	wp_enqueue_style( 'twentyten', get_template_directory_uri() . '/style.css', 'bbp-twentyten-default', 20100503, 'screen' );
+
 	// bbPress specific
-	wp_enqueue_style( 'bbp-twentyten-bbpress', get_stylesheet_directory_uri() . '/css/bbpress.css', 'bbp-twentyten-default', 20100312, 'screen' );
+	wp_enqueue_style( 'bbp-twentyten-bbpress', get_stylesheet_directory_uri() . '/css/bbpress.css', 'twentyten', 20100503, 'screen' );
 }
 add_action( 'init', 'bbp_twentyten_enqueue_styles' );
 endif;
