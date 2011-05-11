624a40fd255690f2f898d185fdc4934c0e90aa2f
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2011-05-11 23:02:25 +0000

    In the single topics loop, wrap topic meta items in spans for easier styling for theme compat.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3137 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-themes/bbp-twentyten/bbpress/loop-topics.php
+++ bbp-themes/bbp-twentyten/bbpress/loop-topics.php
@@ -37,12 +37,15 @@
 
 						<p class="bbp-topic-meta">
 
-							<?php printf( __( 'Started by: %1$s', 'bbpress' ), bbp_get_topic_author_link( array( 'size' => '14' ) ) ); ?>
+							<span class="bbp-topic-started-by"><?php printf( __( 'Started by: %1$s', 'bbpress' ), bbp_get_topic_author_link( array( 'size' => '14' ) ) ); ?></span>
 
-							<?php if ( !bbp_is_forum() || ( bbp_get_topic_forum_id() != bbp_get_forum_id() ) ) printf( __( 'in: <a href="%1$s">%2$s</a>', 'bbpress' ), bbp_get_forum_permalink( bbp_get_topic_forum_id() ), bbp_get_forum_title( bbp_get_topic_forum_id() ) ); ?>
+							<?php if ( !bbp_is_forum() || ( bbp_get_topic_forum_id() != bbp_get_forum_id() ) ) : ?>
+
+								<span class="bbp-topic-started-in"><?php printf( __( 'in: <a href="%1$s">%2$s</a>', 'bbpress' ), bbp_get_forum_permalink( bbp_get_topic_forum_id() ), bbp_get_forum_title( bbp_get_topic_forum_id() ) ); ?></span>
+
+							<?php endif; ?>
 
 						</p>
-
 					</td>
 
 					<td class="bbp-topic-voice-count"><?php bbp_topic_voice_count(); ?></td>
