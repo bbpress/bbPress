42001fab74a8a4358261e63bc3b62a21f960b23e
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2011-05-21 07:49:46 +0000

    Add archive pages to bbp-twentyten theme
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3182 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ bbp-themes/bbp-twentyten/archive-forum.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * bbPress - Forum Archive
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+?>
+
+<?php get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+				<?php do_action( 'bbp_template_notices' ); ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<div id="forum-front" class="bbp-forum-front">
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<div class="entry-content">
+
+							<?php do_action( 'bbp_template_before_forums_index' ); ?>
+
+							<?php if ( bbp_has_forums() ) : ?>
+
+								<?php bbp_get_template_part( 'bbpress/loop', 'forums' ); ?>
+
+							<?php else : ?>
+
+								<?php bbp_get_template_part( 'bbpress/no',   'forums' ); ?>
+
+							<?php endif; ?>
+
+							<?php do_action( 'bbp_template_after_forums_index' ); ?>
+
+						</div>
+					</div><!-- #forum-front -->
+
+				<?php endwhile; ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ bbp-themes/bbp-twentyten/archive-topic.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * bbPress - Topic Archive
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+?>
+
+<?php get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+				<?php do_action( 'bbp_template_notices' ); ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<div id="topics-front" class="bbp-topics-front">
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<div class="entry-content">
+
+							<?php bbp_get_template_part( 'bbpress/nav', 'breadcrumb' ); ?>
+
+							<?php do_action( 'bbp_template_before_topics_index' ); ?>
+
+							<?php if ( bbp_has_topics() ) : ?>
+
+								<?php bbp_get_template_part( 'bbpress/pagination', 'topics' ); ?>
+
+								<?php bbp_get_template_part( 'bbpress/loop',       'topics' ); ?>
+
+								<?php bbp_get_template_part( 'bbpress/pagination', 'topics' ); ?>
+
+							<?php else : ?>
+
+								<?php bbp_get_template_part( 'bbpress/no', 'topics' ); ?>
+
+							<?php endif; ?>
+
+							<?php do_action( 'bbp_template_after_topics_index' ); ?>
+
+						</div>
+					</div><!-- #topics-front -->
+
+				<?php endwhile; ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
