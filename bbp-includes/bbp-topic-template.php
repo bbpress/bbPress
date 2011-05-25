f9ee05235f98febfdb951310081a3390b79b6ac4
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2011-05-25 09:51:08 +0000

    Add topic pagination support for pages or any single post, for improved shortcode support. Fixes #1533.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3226 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-includes/bbp-topic-template.php
+++ bbp-includes/bbp-topic-template.php
@@ -244,6 +244,10 @@ function bbp_has_topics( $args = '' ) {
 			elseif ( is_post_type_archive( bbp_get_topic_post_type() ) )
 				$base = user_trailingslashit( trailingslashit( home_url( $bbp->topic_archive_slug ) ) . 'page/%#%/' );
 
+			// Page or single post
+			elseif ( is_page() || is_single() )
+				$base = user_trailingslashit( trailingslashit( get_permalink() ) . 'page/%#%/' );
+
 			// Default
 			else
 				$base = user_trailingslashit( trailingslashit( get_permalink( $post_parent ) ) . 'page/%#%/' );
