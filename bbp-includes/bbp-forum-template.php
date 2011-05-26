ca5ba517284fee69de207e7bd47472b9e42640d9
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2011-05-26 08:51:26 +0000

    Fix subforum sort ordering. Props anointed.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3228 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-includes/bbp-forum-template.php
+++ bbp-includes/bbp-forum-template.php
@@ -550,7 +550,8 @@ function bbp_forum_get_subforums( $args = '' ) {
 		'post_type'      => bbp_get_forum_post_type(),
 		'post_status'    => implode( ',', $post_stati ),
 		'posts_per_page' => get_option( '_bbp_forums_per_page', 15 ),
-		'sort_column'    => 'menu_order, post_title'
+		'sort_column'    => 'menu_order, post_title',
+		'order'          => 'ASC'
 	);
 
 	$r = wp_parse_args( $args, $default );
