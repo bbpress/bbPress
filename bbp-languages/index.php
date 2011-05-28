05ad4c4bbc5cc853b2d76b9ccf6a4c72bea30e75
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2011-05-28 05:25:25 +0000

    Add an index.php file to every folder, with a note not to modify core files. In the languages and themes folders, include additional reminder that custom files will be lost during bbPress updates and where custom files should be located.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3245 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ bbp-languages/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Do not put custom translations here. They will be deleted on bbPress updates.
+ * 
+ * Keep custom bbPress themes in /wp-content/languages/
+ */
+
+?>
---
+++ bbp-themes/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Do not put custom themes here. They will be deleted on bbPress updates.
+ * 
+ * Keep custom bbPress themes in /wp-content/themes/
+ */
+
+?>
