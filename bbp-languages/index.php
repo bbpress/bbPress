fd3c9e75fb625100a7b50ddf0ee993a9910afe17
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2011-08-28 22:44:54 +0000

    Fix verbiage in comment in bbp-languages/index.php.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3466 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-languages/index.php
+++ bbp-languages/index.php
@@ -3,7 +3,7 @@
 /**
  * Do not put custom translations here. They will be deleted on bbPress updates.
  *
- * Keep custom bbPress themes in /wp-content/languages/
+ * Keep custom bbPress translations in /wp-content/languages/
  */
 
 ?>
