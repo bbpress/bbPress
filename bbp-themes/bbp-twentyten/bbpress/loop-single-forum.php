0003dd73d2571216443a9a90d615dd4c92ab96d3
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2012-04-14 21:16:00 +0000

    Tweak bbp-forum-content and bbp-forums-list styling. Fixes #1800.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@3835 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-themes/bbp-twentyten/bbpress/loop-single-forum.php
+++ bbp-themes/bbp-twentyten/bbpress/loop-single-forum.php
@@ -27,7 +27,7 @@
 
 			<?php do_action( 'bbp_theme_before_forum_description' ); ?>
 
-			<div class="bbp-forum-description"><?php the_content(); ?></div>
+			<div class="bbp-forum-content"><?php the_content(); ?></div>
 
 			<?php do_action( 'bbp_theme_after_forum_description' ); ?>
 
