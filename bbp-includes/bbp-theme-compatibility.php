cfa034771b5edea7358be9c1d41fc37d4e0954d3
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2012-08-17 21:56:02 +0000

    Theme Compat:
    * Defaults and dummy data for entire single-post schema.
    
    git-svn-id: https://bbpress.svn.wordpress.org/branches/plugin@4155 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbp-includes/bbp-theme-compatibility.php
+++ bbp-includes/bbp-theme-compatibility.php
@@ -299,15 +299,31 @@ function bbp_theme_compat_reset_post( $args = array() ) {
 
 	// Default arguments
 	$defaults = array(
-		'ID'              => -9999,
-		'post_title'      => '',
-		'post_author'     => 0,
-		'post_date'       => 0,
-		'post_content'    => '',
-		'post_type'       => 'page',
-		'post_status'     => bbp_get_public_status_id(),
-		'post_name'       => '',
-		'comment_status'  => 'closed',
+		'ID'                    => -9999,
+		'post_status'           => bbp_get_public_status_id(),
+		'post_author'           => 0,
+		'post_parent'           => 0,
+		'post_type'             => 'page',
+		'post_date'             => 0,
+		'post_date_gmt'         => 0,
+		'post_modified'         => 0,
+		'post_modified_gmt'     => 0,
+		'post_content'          => '',
+		'post_title'            => '',
+		'post_category'         => 0,
+		'post_excerpt'          => '',
+		'post_content_filtered' => '',
+		'post_mime_type'        => '',
+		'post_password'         => '',
+		'post_name'             => '',
+		'guid'                  => '',
+		'menu_order'            => 0,
+		'pinged'                => '',
+		'to_ping'               => '',
+		'ping_status'           => '',
+		'comment_status'        => 'closed',
+		'comment_count'         => 0,
+
 		'is_404'          => false,
 		'is_page'         => false,
 		'is_single'       => false,
@@ -316,17 +332,33 @@ function bbp_theme_compat_reset_post( $args = array() ) {
 	);
 
 	// Switch defaults if post is set
-	if ( isset( $wp_query->post ) ) {
+	if ( isset( $wp_query->post ) ) {		  
 		$defaults = array(
-			'ID'              => get_the_ID(),
-			'post_title'      => get_the_title(),
-			'post_author'     => get_the_author_meta('ID'),
-			'post_date'       => get_the_date(),
-			'post_content'    => get_the_content(),
-			'post_type'       => get_post_type(),
-			'post_status'     => get_post_status(),
-			'post_name'       => !empty( $wp_query->post->post_name ) ? $wp_query->post->post_name : '',
-			'comment_status'  => comments_open(),
+			'ID'                    => $wp_query->post->ID,
+			'post_status'           => $wp_query->post->post_status,
+			'post_author'           => $wp_query->post->post_author,
+			'post_parent'           => $wp_query->post->post_parent,
+			'post_type'             => $wp_query->post->post_type,
+			'post_date'             => $wp_query->post->post_date,
+			'post_date_gmt'         => $wp_query->post->post_date_gmt,
+			'post_modified'         => $wp_query->post->post_modified,
+			'post_modified_gmt'     => $wp_query->post->post_modified_gmt,
+			'post_content'          => $wp_query->post->post_content,
+			'post_title'            => $wp_query->post->post_title,
+			'post_category'         => $wp_query->post->post_category,
+			'post_excerpt'          => $wp_query->post->post_excerpt,
+			'post_content_filtered' => $wp_query->post->post_content_filtered,
+			'post_mime_type'        => $wp_query->post->post_mime_type,
+			'post_password'         => $wp_query->post->post_password,
+			'post_name'             => $wp_query->post->post_name,
+			'guid'                  => $wp_query->post->guid,
+			'menu_order'            => $wp_query->post->menu_order,
+			'pinged'                => $wp_query->post->pinged,
+			'to_ping'               => $wp_query->post->to_ping,
+			'ping_status'           => $wp_query->post->ping_status,
+			'comment_status'        => $wp_query->post->comment_status,
+			'comment_count'         => $wp_query->post->comment_count,
+
 			'is_404'          => false,
 			'is_page'         => false,
 			'is_single'       => false,
@@ -342,16 +374,31 @@ function bbp_theme_compat_reset_post( $args = array() ) {
 	unset( $post            );
 
 	// Setup the dummy post object
-	$wp_query->post                 = new stdClass; 
-	$wp_query->post->ID             = $dummy['ID'];
-	$wp_query->post->post_title     = $dummy['post_title'];
-	$wp_query->post->post_author    = $dummy['post_author'];
-	$wp_query->post->post_date      = $dummy['post_date'];
-	$wp_query->post->post_content   = $dummy['post_content'];
-	$wp_query->post->post_type      = $dummy['post_type'];
-	$wp_query->post->post_status    = $dummy['post_status'];
-	$wp_query->post->post_name      = $dummy['post_name'];
-	$wp_query->post->comment_status = $dummy['comment_status'];
+	$wp_query->post                        = new stdClass; 
+	$wp_query->post->ID                    = $dummy['ID'];
+	$wp_query->post->post_status           = $dummy['post_status'];
+	$wp_query->post->post_author           = $dummy['post_author'];
+	$wp_query->post->post_parent           = $dummy['post_parent'];
+	$wp_query->post->post_type             = $dummy['post_type'];
+	$wp_query->post->post_date             = $dummy['post_date'];
+	$wp_query->post->post_date_gmt         = $dummy['post_date_gmt'];
+	$wp_query->post->post_modified         = $dummy['post_modified'];
+	$wp_query->post->post_modified_gmt     = $dummy['post_modified_gmt'];
+	$wp_query->post->post_content          = $dummy['post_content'];
+	$wp_query->post->post_title            = $dummy['post_title'];
+	$wp_query->post->post_category         = $dummy['post_category'];
+	$wp_query->post->post_excerpt          = $dummy['post_content_filtered'];
+	$wp_query->post->post_content_filtered = $dummy['post_content_filtered'];
+	$wp_query->post->post_mime_type        = $dummy['post_mime_type'];
+	$wp_query->post->post_password         = $dummy['post_password'];
+	$wp_query->post->post_name             = $dummy['post_name'];
+	$wp_query->post->guid                  = $dummy['guid'];
+	$wp_query->post->menu_order            = $dummy['menu_order'];
+	$wp_query->post->pinged                = $dummy['pinged'];
+	$wp_query->post->to_ping               = $dummy['to_ping'];
+	$wp_query->post->ping_status           = $dummy['ping_status'];
+	$wp_query->post->comment_status        = $dummy['comment_status'];
+	$wp_query->post->comment_count         = $dummy['comment_count'];
 
 	// Set the $post global
 	$post = $wp_query->post;
