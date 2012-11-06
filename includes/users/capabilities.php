1dbed18d7d2ce136a9aea6c540228ba3440b446b
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2012-11-06 01:21:55 +0000

    Capabilities:
    * Move status and role user functions out of users/functions.php and into users/capabilities.php.
    * See #1939.
    
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@4346 9866e705-20ec-0310-96e7-cbb4277adcfb

--- includes/users/capabilities.php
+++ includes/users/capabilities.php
@@ -45,6 +45,84 @@ function bbp_map_primary_meta_caps( $caps, $cap, $user_id, $args ) {
 }
 
 /**
+ * Return a user's main role
+ *
+ * @since bbPress (r3860)
+ *
+ * @param int $user_id
+ * @uses bbp_get_user_id() To get the user id
+ * @uses get_userdata() To get the user data
+ * @uses apply_filters() Calls 'bbp_set_user_role' with the role and user id
+ * @return string
+ */
+function bbp_set_user_role( $user_id = 0, $new_role = '' ) {
+
+	// Validate user id
+	$user_id = bbp_get_user_id( $user_id, false, false );
+	$user    = get_userdata( $user_id );
+
+	// User exists
+	if ( !empty( $user ) ) {
+
+		// Get users forum role
+		$role = bbp_get_user_role( $user_id );
+
+		// User already has this role so no new role is set
+		if ( $new_role == $role ) {
+			$new_role = false;
+
+		// Users role is different than the new role
+		} else {
+
+			// Remove the old role
+			if ( ! empty( $role ) ) {
+				$user->remove_role( $role );
+			}
+
+			// Add the new role
+			$user->add_role( $new_role );
+		}
+
+	// User does don exist so return false
+	} else {
+		$new_role = false;
+	}
+
+	return apply_filters( 'bbp_set_user_role', $new_role, $user_id, $user );
+}
+
+/**
+ * Return a user's main role
+ *
+ * @since bbPress (r3860)
+ *
+ * @param int $user_id
+ * @uses bbp_get_user_id() To get the user id
+ * @uses get_userdata() To get the user data
+ * @uses apply_filters() Calls 'bbp_get_user_role' with the role and user id
+ * @return string
+ */
+function bbp_get_user_role( $user_id = 0 ) {
+
+	// Validate user id
+	$user_id = bbp_get_user_id( $user_id, false, false );
+	$user    = get_userdata( $user_id );
+	$role    = false;
+
+	// User has roles so lets
+	if ( ! empty( $user->roles ) ) {
+		$roles = array_intersect( array_values( $user->roles ), array_keys( bbp_get_editable_roles() ) );
+
+		// If there's a role in the array, use the first one
+		if ( !empty( $roles ) ) {
+			$role = array_shift( array_values( $roles ) );
+		}
+	}
+
+	return apply_filters( 'bbp_get_user_role', $role, $user_id, $user );
+}
+
+/**
  * Helper function hooked to 'bbp_edit_user_profile_update' action to save or
  * update user roles and capabilities.
  *
@@ -149,4 +227,323 @@ function bbp_get_user_role_map() {
 		'subscriber'    => $default_role,
 		''              => $default_role
 	) );
+}
+
+/** User Status ***************************************************************/
+
+/**
+ * Checks if the user has been marked as a spammer.
+ *
+ * @since bbPress (r3355)
+ *
+ * @param int $user_id int The ID for the user.
+ * @return bool True if spammer, False if not.
+ */
+function bbp_is_user_spammer( $user_id = 0 ) {
+
+	// Default to current user
+	if ( empty( $user_id ) && is_user_logged_in() )
+		$user_id = bbp_get_current_user_id();
+
+	// No user to check
+	if ( empty( $user_id ) )
+		return false;
+
+	// Assume user is not spam
+	$is_spammer = false;
+
+	// Get user data
+	$user = get_userdata( $user_id );
+
+	// No user found
+	if ( empty( $user ) ) {
+		$is_spammer = false;
+
+	// User found
+	} else {
+
+		// Check if spam
+		if ( !empty( $user->spam ) )
+			$is_spammer = true;
+
+		if ( 1 == $user->user_status )
+			$is_spammer = true;
+	}
+
+	return apply_filters( 'bp_core_is_user_spammer', (bool) $is_spammer );
+}
+
+/**
+ * Mark a users topics and replies as spam when the user is marked as spam
+ *
+ * @since bbPress (r3405)
+ *
+ * @global WPDB $wpdb
+ * @param int $user_id Optional. User ID to spam. Defaults to displayed user.
+
+ * @uses bbp_is_single_user()
+ * @uses bbp_is_user_home()
+ * @uses bbp_get_displayed_user_field()
+ * @uses is_super_admin()
+ * @uses get_blogs_of_user()
+ * @uses get_current_blog_id()
+ * @uses bbp_get_topic_post_type()
+ * @uses bbp_get_reply_post_type()
+ * @uses switch_to_blog()
+ * @uses get_post_type()
+ * @uses bbp_spam_topic()
+ * @uses bbp_spam_reply()
+ * @uses restore_current_blog()
+ *
+ * @return If no user ID passed
+ */
+function bbp_make_spam_user( $user_id = 0 ) {
+
+	// Use displayed user if it's not yourself
+	if ( empty( $user_id ) && bbp_is_single_user() && !bbp_is_user_home() )
+		$user_id = bbp_get_displayed_user_id();
+
+	// Bail if no user ID
+	if ( empty( $user_id ) )
+		return;
+
+	// Bail if user ID is super admin
+	if ( is_super_admin( $user_id ) )
+		return;
+
+	// Arm the torpedos
+	global $wpdb;
+
+	// Get the blog IDs of the user to mark as spam
+	$blogs = get_blogs_of_user( $user_id, true );
+
+	// If user has no blogs, they are a guest on this site
+	if ( empty( $blogs ) )
+		$blogs[$wpdb->blogid] = array();
+
+	// Make array of post types to mark as spam
+	$post_types  = array( bbp_get_topic_post_type(), bbp_get_reply_post_type() );
+	$post_types  = "'" . implode( "', '", $post_types ) . "'";
+	$status      = bbp_get_public_status_id();
+
+	// Loop through blogs and remove their posts
+	foreach ( (array) array_keys( $blogs ) as $blog_id ) {
+
+		// Switch to the blog ID
+		switch_to_blog( $blog_id );
+
+		// Get topics and replies
+		$posts = $wpdb->get_col( "SELECT ID FROM {$wpdb->posts} WHERE post_author = {$user_id} AND post_status = '{$status}' AND post_type IN ({$post_types})" );
+
+		// Loop through posts and spam them
+		if ( !empty( $posts ) ) {
+			foreach ( $posts as $post_id ) {
+
+				// The routines for topics ang replies are different, so use the
+				// correct one based on the post type
+				switch ( get_post_type( $post_id ) ) {
+
+					case bbp_get_topic_post_type() :
+						bbp_spam_topic( $post_id );
+						break;
+
+					case bbp_get_reply_post_type() :
+						bbp_spam_reply( $post_id );
+						break;
+				}
+			}
+		}
+
+		// Switch back to current blog
+		restore_current_blog();
+	}
+}
+
+/**
+ * Mark a users topics and replies as spam when the user is marked as spam
+ *
+ * @since bbPress (r3405)
+ *
+ * @global WPDB $wpdb
+ * @param int $user_id Optional. User ID to unspam. Defaults to displayed user.
+ *
+ * @uses bbp_is_single_user()
+ * @uses bbp_is_user_home()
+ * @uses bbp_get_displayed_user_field()
+ * @uses is_super_admin()
+ * @uses get_blogs_of_user()
+ * @uses bbp_get_topic_post_type()
+ * @uses bbp_get_reply_post_type()
+ * @uses switch_to_blog()
+ * @uses get_post_type()
+ * @uses bbp_unspam_topic()
+ * @uses bbp_unspam_reply()
+ * @uses restore_current_blog()
+ *
+ * @return If no user ID passed
+ */
+function bbp_make_ham_user( $user_id = 0 ) {
+
+	// Use displayed user if it's not yourself
+	if ( empty( $user_id ) && bbp_is_single_user() && !bbp_is_user_home() )
+		$user_id = bbp_get_displayed_user_field();
+
+	// Bail if no user ID
+	if ( empty( $user_id ) )
+		return;
+
+	// Bail if user ID is super admin
+	if ( is_super_admin( $user_id ) )
+		return;
+
+	// Arm the torpedos
+	global $wpdb;
+
+	// Get the blog IDs of the user to mark as spam
+	$blogs = get_blogs_of_user( $user_id, true );
+
+	// If user has no blogs, they are a guest on this site
+	if ( empty( $blogs ) )
+		$blogs[$wpdb->blogid] = array();
+
+	// Make array of post types to mark as spam
+	$post_types = array( bbp_get_topic_post_type(), bbp_get_reply_post_type() );
+	$post_types = "'" . implode( "', '", $post_types ) . "'";
+	$status     = bbp_get_spam_status_id();
+
+	// Loop through blogs and remove their posts
+	foreach ( (array) array_keys( $blogs ) as $blog_id ) {
+
+		// Switch to the blog ID
+		switch_to_blog( $blog_id );
+
+		// Get topics and replies
+		$posts = $wpdb->get_col( "SELECT ID FROM {$wpdb->posts} WHERE post_author = {$user_id} AND post_status = '{$status}' AND post_type IN ({$post_types})" );
+
+		// Loop through posts and spam them
+		if ( !empty( $posts ) ) {
+			foreach ( $posts as $post_id ) {
+
+				// The routines for topics ang replies are different, so use the
+				// correct one based on the post type
+				switch ( get_post_type( $post_id ) ) {
+
+					case bbp_get_topic_post_type() :
+						bbp_unspam_topic( $post_id );
+						break;
+
+					case bbp_get_reply_post_type() :
+						bbp_unspam_reply( $post_id );
+						break;
+				}
+			}
+		}
+
+		// Switch back to current blog
+		restore_current_blog();
+	}
+}
+
+/**
+ * Checks if the user has been marked as deleted.
+ *
+ * @since bbPress (r3355)
+ *
+ * @param int $user_id int The ID for the user.
+ * @return bool True if deleted, False if not.
+ */
+function bbp_is_user_deleted( $user_id = 0 ) {
+
+	// Default to current user
+	if ( empty( $user_id ) && is_user_logged_in() )
+		$user_id = bbp_get_current_user_id();
+
+	// No user to check
+	if ( empty( $user_id ) )
+		return false;
+
+	// Assume user is not deleted
+	$is_deleted = false;
+
+	// Get user data
+	$user = get_userdata( $user_id );
+
+	// No user found
+	if ( empty( $user ) ) {
+		$is_deleted = true;
+
+	// User found
+	} else {
+
+		// Check if deleted
+		if ( !empty( $user->deleted ) )
+			$is_deleted = true;
+
+		if ( 2 == $user->user_status )
+			$is_deleted = true;
+
+	}
+
+	return apply_filters( 'bp_core_is_user_deleted', (bool) $is_deleted );
+}
+
+/**
+ * Checks if user is active
+ * 
+ * @since bbPress (r3502)
+ * 
+ * @uses is_user_logged_in() To check if user is logged in
+ * @uses bbp_get_displayed_user_id() To get current user ID
+ * @uses bbp_is_user_spammer() To check if user is spammer
+ * @uses bbp_is_user_deleted() To check if user is deleted
+ *
+ * @param int $user_id The user ID to check
+ * @return bool True if public, false if not
+ */
+function bbp_is_user_active( $user_id = 0 ) {
+
+	// Default to current user
+	if ( empty( $user_id ) && is_user_logged_in() )
+		$user_id = bbp_get_current_user_id();
+
+	// No user to check
+	if ( empty( $user_id ) )
+		return false;
+
+	// Check spam
+	if ( bbp_is_user_spammer( $user_id ) )
+		return false;
+
+	// Check deleted
+	if ( bbp_is_user_deleted( $user_id ) )
+		return false;
+
+	// Assume true if not spam or deleted
+	return true;
+}
+
+/**
+ * Checks if user is not active.
+ * 
+ * @since bbPress (r3502)
+ * 
+ * @uses is_user_logged_in() To check if user is logged in
+ * @uses bbp_get_displayed_user_id() To get current user ID
+ * @uses bbp_is_user_active() To check if user is active
+ *
+ * @param int $user_id The user ID to check. Defaults to current user ID
+ * @return bool True if inactive, false if active
+ */
+function bbp_is_user_inactive( $user_id = 0 ) {
+
+	// Default to current user
+	if ( empty( $user_id ) && is_user_logged_in() )
+		$user_id = bbp_get_current_user_id();
+
+	// No user to check
+	if ( empty( $user_id ) )
+		return false;
+
+	// Return the inverse of active
+	return !bbp_is_user_active( $user_id );
 }
