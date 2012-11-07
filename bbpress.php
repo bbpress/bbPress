daf30861f24f031f58b6d70e2786b8bcd9911005
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2012-11-07 09:36:03 +0000

    Bump.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@4356 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbpress.php
+++ bbpress.php
@@ -17,7 +17,7 @@
  * Description: bbPress is forum software with a twist from the creators of WordPress.
  * Author:      The bbPress Community
  * Author URI:  http://bbpress.org
- * Version:     2.2-beta
+ * Version:     2.2-beta1
  * Text Domain: bbpress
  * Domain Path: /languages/
  */
@@ -180,7 +180,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.2-beta-4318';
+		$this->version    = '2.2-beta1-4355';
 		$this->db_version = '216';
 
 		/** Paths *************************************************************/
