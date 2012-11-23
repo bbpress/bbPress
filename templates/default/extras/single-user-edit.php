01f239e8585703c83b352e90817c30b277c29a39
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2012-11-23 10:53:25 +0000

    Users:
    * Reference correct template part in single-user-edit.php.
    * Fixes #2045 (trunk)
    
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@4466 9866e705-20ec-0310-96e7-cbb4277adcfb

--- templates/default/extras/single-user-edit.php
+++ templates/default/extras/single-user-edit.php
@@ -14,7 +14,7 @@ get_header(); ?>
 	<div id="bbp-user-<?php bbp_current_user_id(); ?>" class="bbp-single-user">
 		<div class="entry-content">
 
-			<?php bbp_get_template_part( 'content', 'single-user-edit' ); ?>
+			<?php bbp_get_template_part( 'content', 'single-user' ); ?>
 
 		</div><!-- .entry-content -->
 	</div><!-- #bbp-user-<?php bbp_current_user_id(); ?> -->
