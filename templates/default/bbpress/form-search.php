430778a29848afa602c8f8a4c617b5ddc561d925
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2012-12-23 06:57:01 +0000

    Tweak search form to include classes and tab indexes.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@4645 9866e705-20ec-0310-96e7-cbb4277adcfb

--- templates/default/bbpress/form-search.php
+++ templates/default/bbpress/form-search.php
@@ -9,10 +9,10 @@
 
 ?>
 
-<form role="search" method="get" id="bbp-search" action="<?php bbp_search_url(); ?>">
+<form role="search" method="get" id="bbp-search-form" action="<?php bbp_search_url(); ?>">
 	<div>
-		<label class="screen-reader-text" for="bbp_search"><?php _e( 'Search for:', 'bbpress' ); ?></label>
-		<input type="text" value="<?php echo esc_attr( bbp_get_search_terms() ); ?>" name="bbp_search" id="bbp_search" />
-		<input type="submit" id="bbp_search_submit" value="<?php _e( 'Search Forums', 'bbpress' ); ?>" />
+		<label class="screen-reader-text hidden" for="bbp_search"><?php _e( 'Search for:', 'bbpress' ); ?></label>
+		<input tabindex="<?php bbp_tab_index(); ?>" type="text" value="<?php echo esc_attr( bbp_get_search_terms() ); ?>" name="bbp_search" id="bbp_search" />
+		<input tabindex="<?php bbp_tab_index(); ?>" class="button" type="submit" id="bbp_search_submit" value="<?php _e( 'Search', 'bbpress' ); ?>" />
 	</div>
 </form>
