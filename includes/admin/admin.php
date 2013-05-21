c41d6538133a4e049fb44e42b116c88f6cd2088f
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2013-05-21 15:43:09 +0000

    Add bbp_settings_user_slugs section to mapped capabilities method. See #1500.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@4939 9866e705-20ec-0310-96e7-cbb4277adcfb

--- includes/admin/admin.php
+++ includes/admin/admin.php
@@ -410,6 +410,7 @@ class BBP_Admin {
 			case 'bbp_settings_theme_compat' : // Settings - Theme compat
 			case 'bbp_settings_root_slugs'   : // Settings - Root slugs
 			case 'bbp_settings_single_slugs' : // Settings - Single slugs
+			case 'bbp_settings_user_slugs'   : // Settings - User slugs
 			case 'bbp_settings_per_page'     : // Settings - Per page
 			case 'bbp_settings_per_rss_page' : // Settings - Per RSS page
 				$caps = array( bbpress()->admin->minimum_capability );
