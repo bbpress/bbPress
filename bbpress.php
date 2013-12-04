2670bfbfd0b637efddb112cbf56e4358b0c722f6
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2013-12-04 19:56:50 +0000

    Bump trunk to 2.6-alpha.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5214 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbpress.php
+++ bbpress.php
@@ -17,7 +17,7 @@
  * Description: bbPress is forum software with a twist from the creators of WordPress.
  * Author:      The bbPress Community
  * Author URI:  http://bbpress.org
- * Version:     2.5
+ * Version:     2.6-alpha
  * Text Domain: bbpress
  * Domain Path: /languages/
  */
@@ -190,7 +190,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.5-5199';
+		$this->version    = '2.6-alpha-5214';
 		$this->db_version = '250';
 
 		/** Paths *************************************************************/
