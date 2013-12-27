6b21d446b0213ff78f8542e5c4bcd190c65c6bcd
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2013-12-27 22:14:18 +0000

    Bump 2.5 branch to 2.5.2.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.5@5234 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbpress.php
+++ bbpress.php
@@ -17,7 +17,7 @@
  * Description: bbPress is forum software with a twist from the creators of WordPress.
  * Author:      The bbPress Community
  * Author URI:  http://bbpress.org
- * Version:     2.5.1
+ * Version:     2.5.2
  * Text Domain: bbpress
  * Domain Path: /languages/
  */
@@ -190,7 +190,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.5.1-5217';
+		$this->version    = '2.5.2-5234';
 		$this->db_version = '250';
 
 		/** Paths *************************************************************/
