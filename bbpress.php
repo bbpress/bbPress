6a1be7a6c7e77d5ea76d416ee3365ad0579b0edf
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2014-01-11 18:20:32 +0000

    Bump 2.5 branch to 2.5.3, and update `readme.txt`.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.5@5249 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbpress.php
+++ bbpress.php
@@ -17,7 +17,7 @@
  * Description: bbPress is forum software with a twist from the creators of WordPress.
  * Author:      The bbPress Community
  * Author URI:  http://bbpress.org
- * Version:     2.5.2
+ * Version:     2.5.3
  * Text Domain: bbpress
  * Domain Path: /languages/
  */
@@ -190,7 +190,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.5.2-5234';
+		$this->version    = '2.5.3-5249';
 		$this->db_version = '250';
 
 		/** Paths *************************************************************/
