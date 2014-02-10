c1c3ceaa3767c3575898b3045ca4204e21cf4198
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2014-02-10 23:22:21 +0000

    Gruntify bbPress trunk (part 2):
    * Move bbPress core files into `/src`
    * Move tests into `/tests/phpunit/`
    * Move `phpunit.xml` into trunk root
    * Introduce Grunt and Travis configuration files
    * Introduce stub `bbpress.php` to help activate bbPress for existing trunk checkouts
    * See #2542.
    
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5271 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ Gruntfile.js
@@ -0,0 +1,255 @@
+/* jshint node:true */
+module.exports = function( grunt ) {
+	var path = require( 'path' ),
+	SOURCE_DIR = 'src/',
+	BUILD_DIR = 'build/',
+
+	BBP_RTL_CSS = [
+		'admin/css/*-rtl.css',
+		'templates/default/css/*-rtl.css'
+	],
+
+	BBP_LTR_CSS = [
+		'admin/css/*.css',
+		'templates/default/css/*.css'
+	],
+
+	BBP_JS = [
+		'admin/js/*.js',
+		'templates/default/js/*.js'
+	],
+
+	BBP_EXCLUDED_FILES = [
+		// Ignore these
+		'!tests/**',  // unit tests
+		'!Gruntfile.js',
+		'!package.json',
+		'!.gitignore',
+		'!.jshintrc',
+		'!.travis.yml',
+		'node_modules/**',
+
+		// And these from .gitignore
+		'!**/.{svn,git}/**',
+		'!lib-cov/**',
+		'!*.seed',
+		'!*.log',
+		'!*.csv',
+		'!*.dat',
+		'!*.out',
+		'!*.pid',
+		'!*.gz',
+		'!pids/**',
+		'!logs/**',
+		'!results/**',
+		'!.DS_Store',
+		'!node_modules/**',
+		'!npm-debug.log',
+		'!build/**'
+	];
+
+	// Load tasks.
+	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
+
+	// Project configuration.
+	grunt.initConfig({
+		clean: {
+			all: [ BUILD_DIR ],
+			dynamic: {
+				cwd: BUILD_DIR,
+				dot: true,
+				expand: true,
+				src: []
+			}
+		},
+		copy: {
+			files: {
+				files: [
+					{
+						cwd: SOURCE_DIR,
+						dest: BUILD_DIR,
+						dot: true,
+						expand: true,
+						src: [ '!**/.{svn,git}/**', '**' ]
+					}
+				]
+			},
+			dynamic: {
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				dot: true,
+				expand: true,
+				src: []
+			}
+		},
+		cssmin: {
+			ltr: {
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				expand: true,
+				ext: '.min.css',
+				src: BBP_LTR_CSS,
+				options: { banner: '/*! https://wordpress.org/plugins/bbpress/ */' }
+			},
+			rtl: {
+				cwd: BUILD_DIR,
+				dest: BUILD_DIR,
+				expand: true,
+				ext: '.min.css',
+				src: BBP_RTL_CSS,
+				options: { banner: '/*! https://wordpress.org/plugins/bbpress/ */' }
+			}
+		},
+		cssjanus: {
+			core: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				ext: '-rtl.css',
+				src: BBP_LTR_CSS,
+				options: { generateExactDuplicates: true }
+			},
+			dynamic: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				ext: '-rtl.css',
+				src: []
+			}
+		},
+		jshint: {
+			options: grunt.file.readJSON( '.jshintrc' ),
+			grunt: {
+				src: [ 'Gruntfile.js' ]
+			},
+			core: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				src: BBP_JS,
+
+				/**
+				 * Limit JSHint's run to a single specified file: grunt jshint:core --file=filename.js
+				 *
+				 * @param {String} filepath
+				 * @returns {Bool}
+				 */
+				filter: function( filepath ) {
+					var index, file = grunt.option( 'file' );
+
+					// Don't filter when no target file is specified
+					if ( ! file ) {
+						return true;
+					}
+
+					// Normalise filepath for Windows
+					filepath = filepath.replace( /\\/g, '/' );
+					index = filepath.lastIndexOf( '/' + file );
+
+					// Match only the filename passed from cli
+					if ( filepath === file || ( -1 !== index && index === filepath.length - ( file.length + 1 ) ) ) {
+						return true;
+					}
+
+					return false;
+				}
+			}
+		},
+		uglify: {
+			core: {
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				expand: true,
+				ext: '.min.js',
+				src: BBP_JS
+			},
+			options: {
+				banner: '/*! https://wordpress.org/plugins/bbpress/ */\n'
+			}
+		},
+		phpunit: {
+			'default': {
+				cmd: 'phpunit',
+				args: ['-c', 'phpunit.xml']
+			},
+			multisite: {
+				cmd: 'phpunit',
+				args: ['-c', 'tests/phpunit/multisite.xml']
+			}
+		},
+		jsvalidate:{
+			options:{
+				globals: {},
+				esprimaOptions:{},
+				verbose: false
+			},
+			build: {
+				files: {
+					src: BUILD_DIR + '/**/*.js'
+				}
+			}
+		},
+		watch: {
+			all: {
+				files: [
+					SOURCE_DIR + '**',
+					// Ignore version control directories.
+					'!' + SOURCE_DIR + '**/.{svn,git}/**'
+				],
+				tasks: [ 'clean:dynamic', 'copy:dynamic' ],
+				options: {
+					dot: true,
+					interval: 2000,
+					spawn: false
+				}
+			},
+			rtl: {
+				files: BBP_LTR_CSS.map( function( path ) {
+					return SOURCE_DIR + path;
+				} ),
+				tasks: [ 'cssjanus:dynamic' ],
+				options: {
+					interval: 2000,
+					spawn: false
+				}
+			}
+		}
+	});
+
+
+	// Build tasks.
+	grunt.registerTask( 'build',         [ 'clean:all', 'copy:files', 'cssjanus:core', 'cssmin:ltr', 'cssmin:rtl', 'uglify:core', 'jsvalidate:build' ] );
+	grunt.registerTask( 'build-release', [ 'clean:all', 'copy:files', 'cssjanus:core', 'cssmin:ltr', 'cssmin:rtl', 'uglify:core', 'jsvalidate:build', 'phpunit:all' ] );
+
+	// Testing tasks.
+	grunt.registerMultiTask( 'phpunit', 'Runs PHPUnit tests, including the ajax and multisite tests.', function() {
+		grunt.util.spawn( {
+			cmd:  this.data.cmd,
+			args: this.data.args,
+			opts: { stdio: 'inherit' }
+		}, this.async() );
+	});
+
+	grunt.registerTask( 'test', 'Runs all unit tasks.', [ 'phpunit' ] );
+
+	// Default task.
+	grunt.registerTask( 'default', [ 'build' ] );
+
+
+	// Add a listener to the watch task.
+	//
+	// On `watch:all`, automatically updates the `copy:dynamic` and `clean:dynamic` configurations so that only the changed files are updated.
+	// On `watch:rtl`, automatically updates the `cssjanus:dynamic` configuration.
+	grunt.event.on( 'watch', function( action, filepath, target ) {
+		if ( target !== 'all' && target !== 'rtl' ) {
+			return;
+		}
+
+		var relativePath = path.relative( SOURCE_DIR, filepath ),
+		cleanSrc = ( action === 'deleted' ) ? [ relativePath ] : [],
+		copySrc  = ( action === 'deleted' ) ? [] : [ relativePath ];
+
+		grunt.config( [ 'clean', 'dynamic', 'src' ], cleanSrc );
+		grunt.config( [ 'copy', 'dynamic', 'src' ], copySrc );
+		grunt.config( [ 'cssjanus', 'dynamic', 'src' ], copySrc );
+	});
+};
