c60b9f706ec500542bc7d9a961b71f410545cf1c
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2014-02-10 02:46:20 +0000

    Fix relative path to unit test bootstrap. See #2542.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5267 9866e705-20ec-0310-96e7-cbb4277adcfb

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,5 +20,5 @@ $GLOBALS['wp_tests_options'] = array(
 if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
 	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
 } else {
-	require '../../../../tests/phpunit/includes/bootstrap.php';
+	require '../../../../../tests/phpunit/includes/bootstrap.php';
 }
