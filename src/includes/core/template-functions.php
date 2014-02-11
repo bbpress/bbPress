b934db783d9d9f3933e871def2d025add5f3f35d
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2014-02-11 02:20:47 +0000

    In `bbp_locate_template()` check for `WP_USE_THEMES` before calling `load_template()` to avoid warnings when running phpunit tests. See #2542.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5283 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/core/template-functions.php
+++ src/includes/core/template-functions.php
@@ -101,7 +101,7 @@ function bbp_locate_template( $template_names, $load = false, $require_once = tr
 	do_action( 'bbp_locate_template', $located, $template_name, $template_names, $template_locations, $load, $require_once );
 
 	// Maybe load the template if one was located
-	if ( ( true === $load ) && !empty( $located ) ) {
+	if ( ( defined( 'WP_USE_THEMES' ) && WP_USE_THEMES ) && ( true === $load ) && !empty( $located ) ) {
 		load_template( $located, $require_once );
 	}
 
