636a1ee3e75a0cfee332033e585ae3c913c3d9c9
Stephen Edgar <netweb@git.wordpress.org>
2014-02-20 00:51:34 +0000

    Fix incorrect `bbbpress` text domain used `bbp_get_topic_pagination_count`. Props netweb. Fixes #2549.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5291 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/replies/template.php
+++ src/includes/replies/template.php
@@ -2338,7 +2338,7 @@ function bbp_topic_pagination_count() {
 
 			// Adjust for topic
 			$threads--;
-			$retstr  = sprintf( _n( 'Viewing %1$s reply thread', 'Viewing %1$s reply threads', $threads, 'bbbpress' ), bbp_number_format( $threads ) );
+			$retstr  = sprintf( _n( 'Viewing %1$s reply thread', 'Viewing %1$s reply threads', $threads, 'bbpress' ), bbp_number_format( $threads ) );
 
 		// We are not including the lead topic
 		} elseif ( bbp_show_lead_topic() ) {
