f3292b47da7592a17efd09d53682ee27b741ba0a
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2014-03-28 00:39:03 +0000

    Add missing `forum.js`. Props SGr33n. Fixes #2568.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5324 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ src/templates/default/js/forum.js
@@ -0,0 +1,26 @@
+jQuery( document ).ready( function ( $ ) {
+
+	function bbp_ajax_call( action, forum_id, nonce, update_selector ) {
+		var $data = {
+			action : action,
+			id     : forum_id,
+			nonce  : nonce
+		};
+
+		$.post( bbpForumJS.bbp_ajaxurl, $data, function ( response ) {
+			if ( response.success ) {
+				$( update_selector ).html( response.content );
+			} else {
+				if ( !response.content ) {
+					response.content = bbpForumJS.generic_ajax_error;
+				}
+				alert( response.content );
+			}
+		} );
+	}
+
+	$( '#subscription-toggle' ).on( 'click', 'span a.subscription-toggle', function( e ) {
+		e.preventDefault();
+		bbp_ajax_call( 'forum_subscription', $( this ).attr( 'data-forum' ), bbpForumJS.subs_nonce, '#subscription-toggle' );
+	} );
+} );
