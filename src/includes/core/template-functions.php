80f6b5ef0dab82898314853539537122a20924ac
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2014-05-31 16:23:48 +0000

    In template stack functions, wrap `function_exists()` checks with `is_array()` check. Allows class methods to be used as template stack locations. See: https://buddypress.trac.wordpress.org/ticket/5671
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5355 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/core/template-functions.php
+++ src/includes/core/template-functions.php
@@ -255,14 +255,22 @@ function bbp_enqueue_script( $handle = '', $file = '', $dependencies = array(), 
  *
  * @since bbPress (r4323)
  *
- * @param string $location Callback function that returns the
+ * @param string $location_callback Callback function that returns the
  * @param int $priority
  */
 function bbp_register_template_stack( $location_callback = '', $priority = 10 ) {
 
 	// Bail if no location, or function does not exist
-	if ( empty( $location_callback ) || ! function_exists( $location_callback ) )
+	if ( empty( $location_callback ) ) {
 		return false;
+	}
+
+	// Bail if callback function does not exist
+	if ( ! is_array( $location_callback ) ) {
+		if ( ! function_exists( $location_callback ) ) {
+			return false;
+		}
+	}
 
 	// Add location callback to template stack
 	return add_filter( 'bbp_template_stack', $location_callback, (int) $priority );
@@ -273,15 +281,23 @@ function bbp_register_template_stack( $location_callback = '', $priority = 10 ) 
  *
  * @since bbPress (r4652)
  *
- * @param string $location Callback function that returns the
+ * @param string $location_callback Callback function that returns the
  * @param int $priority
  * @see bbp_register_template_stack()
  */
 function bbp_deregister_template_stack( $location_callback = '', $priority = 10 ) {
 
 	// Bail if no location, or function does not exist
-	if ( empty( $location_callback ) || ! function_exists( $location_callback ) )
+	if ( empty( $location_callback ) ) {
 		return false;
+	}
+
+	// Bail if callback function does not exist
+	if ( ! is_array( $location_callback ) ) {
+		if ( ! function_exists( $location_callback ) ) {
+			return false;
+		}
+	}
 
 	// Remove location callback to template stack
 	return remove_filter( 'bbp_template_stack', $location_callback, (int) $priority );
