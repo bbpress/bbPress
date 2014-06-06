aac195e3562a2bea4156eb12d94a954079775875
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2014-06-06 13:58:40 +0000

    Update reply position repair tool to delete invalid `_bbp_reply_to` fields, and tweak name and description. Props netweb. See #2588. (trunk)
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5375 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/admin/tools.php
+++ src/includes/admin/tools.php
@@ -288,7 +288,7 @@ function bbp_admin_repair_list() {
 		10 => array( 'bbp-sync-forum-visibility',    __( 'Recalculate private and hidden forums',             'bbpress' ), 'bbp_admin_repair_forum_visibility'         ),
 		15 => array( 'bbp-sync-all-topics-forums',   __( 'Recalculate last activity in each topic and forum', 'bbpress' ), 'bbp_admin_repair_freshness'                ),
 		20 => array( 'bbp-sync-all-topics-sticky',   __( 'Recalculate sticky relationship of each topic',     'bbpress' ), 'bbp_admin_repair_sticky'                   ),
-		25 => array( 'bbp-sync-all-reply-positions', __( 'Recalculate reply position hierarchy',              'bbpress' ), 'bbp_admin_repair_reply_menu_order'         ),
+		25 => array( 'bbp-sync-all-reply-positions', __( 'Recalculate the position of each reply',            'bbpress' ), 'bbp_admin_repair_reply_menu_order'         ),
 		30 => array( 'bbp-group-forums',             __( 'Repair BuddyPress Group Forum relationships',       'bbpress' ), 'bbp_admin_repair_group_forum_relationship' ),
 		35 => array( 'bbp-forum-topics',             __( 'Count topics in each forum',                        'bbpress' ), 'bbp_admin_repair_forum_topic_count'        ),
 		40 => array( 'bbp-forum-replies',            __( 'Count replies in each forum',                       'bbpress' ), 'bbp_admin_repair_forum_reply_count'        ),
@@ -1320,7 +1320,12 @@ function bbp_admin_repair_reply_menu_order() {
 	global $wpdb;
 
 	$statement = __( 'Recalculating reply menu order &hellip; %s', 'bbpress' );
-	$result    = __( 'No reply hierachy to recalculate!',          'bbpress' );
+	$result    = __( 'No reply positions to recalculate!',         'bbpress' );
+
+	// Delete cases where `_bbp_reply_to` was accidentally set to itself
+	if ( is_wp_error( $wpdb->query( "DELETE FROM `{$wpdb->postmeta}` WHERE `meta_key` = '_bbp_reply_to' AND `post_id` = `meta_value`;" ) ) ) {
+		return array( 1, sprintf( $statement, $result ) ); 
+	}
 
 	// Post type
 	$rpt = bbp_get_reply_post_type();
