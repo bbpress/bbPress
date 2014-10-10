03a3bc2e90ff77ada4deddc401a1f55f0597319d
Stephen Edgar <netweb@git.wordpress.org>
2014-10-10 08:01:49 +0000

    Include anonymous topic and reply import support in vBulletin 3 (`vBulletin3.php`) and vBulletin 4 (`vBulletin.php`) importers Props netweb. See #2347
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5546 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/admin/converters/vBulletin.php
+++ src/includes/admin/converters/vBulletin.php
@@ -191,6 +191,23 @@ class vBulletin extends BBP_Converter_Base {
 			'callback_method' => 'callback_userid'
 		);
 
+		// Topic author name (Stored in postmeta as _bbp_anonymous_name)
+		$this->field_map[] = array(
+			'from_tablename'  => 'thread',
+			'from_fieldname'  => 'postusername',
+			'to_type'         => 'topic',
+			'to_fieldname'    => '_bbp_old_topic_author_name_id'
+		);
+
+		// Is the topic anonymous (Stored in postmeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'thread',
+			'from_fieldname'  => 'postuserid',
+			'to_type'         => 'topic',
+			'to_fieldname'    => '_bbp_old_is_topic_anonymous_id',
+			'callback_method' => 'callback_check_anonymous'
+		);
+
 		// Topic Author ip (Stored in postmeta)
 		// Note: We join the 'post' table because 'thread' table does not include topic content.
 		$this->field_map[] = array(
@@ -383,6 +400,23 @@ class vBulletin extends BBP_Converter_Base {
 			'to_type'         => 'reply',
 			'to_fieldname'    => 'post_author',
 			'callback_method' => 'callback_userid'
+		);
+
+		// Reply author name (Stored in postmeta as _bbp_anonymous_name)
+		$this->field_map[] = array(
+			'from_tablename'  => 'post',
+			'from_fieldname'  => 'username',
+			'to_type'         => 'reply',
+			'to_fieldname'    => '_bbp_old_reply_author_name_id'
+		);
+
+		// Is the reply anonymous (Stored in postmeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'post',
+			'from_fieldname'  => 'userid',
+			'to_type'         => 'reply',
+			'to_fieldname'    => '_bbp_old_is_reply_anonymous_id',
+			'callback_method' => 'callback_check_anonymous'
 		);
 
 		// Reply content.
