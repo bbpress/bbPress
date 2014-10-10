2a0f27ec2960dd3ab32cda21d422d830baebf053
Stephen Edgar <netweb@git.wordpress.org>
2014-10-10 06:34:27 +0000

    DB schema change for `wp_bbconverter_translator` converter sync table * Increase `meta_value` maximum characters from 25 o 255 to allow for longer meta values during import See #2650
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5543 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/admin/converter.php
+++ src/includes/admin/converter.php
@@ -603,7 +603,7 @@ class BBP_Converter {
 					value_type varchar(25) null,
 					value_id bigint(20) unsigned not null default '0',
 					meta_key varchar(255) null,
-					meta_value varchar(25) null,
+					meta_value varchar(255) null,
 				PRIMARY KEY  (meta_id),
 					KEY value_id (value_id),
 					KEY meta_join (meta_key, meta_value) ) {$charset_collate};";
