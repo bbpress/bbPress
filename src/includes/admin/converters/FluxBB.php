816efa0d6077b39a27bab3433ac2a8b3f69a3a21
Stephen Edgar <netweb@git.wordpress.org>
2014-11-22 12:42:39 +0000

    FluxBB importer: Include Subscribed Forums and Subscribed Topics Props netweb. See #2668
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5573 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/admin/converters/FluxBB.php
+++ src/includes/admin/converters/FluxBB.php
@@ -135,6 +135,25 @@ class FluxBB extends BBP_Converter_Base {
 			'default'      => date('Y-m-d H:i:s')
 		);
 
+		/** Forum Subscriptions Section ***************************************/
+
+		// Subscribed forum ID (Stored in usermeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'forum_subscriptions',
+			'from_fieldname'  => 'forum_id',
+			'to_type'         => 'forum_subscriptions',
+			'to_fieldname'    => '_bbp_forum_subscriptions'
+		);
+
+		// Subscribed user ID (Stored in usermeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'forum_subscriptions',
+			'from_fieldname'  => 'user_id',
+			'to_type'         => 'forum_subscriptions',
+			'to_fieldname'    => 'user_id',
+			'callback_method' => 'callback_userid'
+		);
+
 		/** Topic Section *****************************************************/
 
 		// Old topic id (Stored in postmeta)
@@ -296,6 +315,25 @@ class FluxBB extends BBP_Converter_Base {
 		/**
 		 * FluxBB v1.5.3 Forums do not support topic tags out of the box
 		 */
+
+		/** Topic Subscriptions Section ***************************************/
+
+		// Subscribed topic ID (Stored in usermeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'topic_subscriptions',
+			'from_fieldname'  => 'topic_id',
+			'to_type'         => 'topic_subscriptions',
+			'to_fieldname'    => '_bbp_subscriptions'
+		);
+
+		// Subscribed user ID (Stored in usermeta)
+		$this->field_map[] = array(
+			'from_tablename'  => 'topic_subscriptions',
+			'from_fieldname'  => 'user_id',
+			'to_type'         => 'topic_subscriptions',
+			'to_fieldname'    => 'user_id',
+			'callback_method' => 'callback_userid'
+		);
 
 		/** Reply Section *****************************************************/
 
