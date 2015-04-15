43849ae57c8a694c9fa5fbdae68409a3400926a8
Stephen Edgar <netweb@git.wordpress.org>
2015-04-15 04:31:15 +0000

    Tests: Add PHPUnit factory's for topics and replies: * New class `BBP_UnitTest_Factory_For_Topic` for creating topics in PHPUnit tests * New class `BBP_UnitTest_Factory_For_Reply` for creating replies in PHPUnit tests
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5677 9866e705-20ec-0310-96e7-cbb4277adcfb

--- tests/phpunit/includes/factory.php
+++ tests/phpunit/includes/factory.php
@@ -3,11 +3,15 @@
 class BBP_UnitTest_Factory extends WP_UnitTest_Factory {
 
 	public $forum = null;
+	public $topic = null;
+	public $reply = null;
 
 	public function __construct() {
 		parent::__construct();
 
 		$this->forum = new BBP_UnitTest_Factory_For_Forum( $this );
+		$this->topic = new BBP_UnitTest_Factory_For_Topic( $this );
+		$this->reply = new BBP_UnitTest_Factory_For_Reply( $this );
 	}
 }
 
@@ -33,5 +37,54 @@ class BBP_UnitTest_Factory_For_Forum extends WP_UnitTest_Factory_For_Thing {
 
 	public function get_object_by_id( $forum_id ) {
 		return bbp_get_forum( $forum_id );
+	}
+}
+
+class BBP_UnitTest_Factory_For_Topic extends WP_UnitTest_Factory_For_Thing {
+
+	public function __construct( $factory = null ) {
+		parent::__construct( $factory );
+
+		$this->default_generation_definitions = array(
+			'post_title'   => new WP_UnitTest_Generator_Sequence( 'Topic %s' ),
+			'post_content' => new WP_UnitTest_Generator_Sequence( 'Content of Topic %s' ),
+		);
+	}
+
+	public function create_object( $args ) {
+		return bbp_insert_topic( $args );
+	}
+
+	public function update_object( $topic_id, $fields ) {
+		$fields['topic_id'] = $topic_id;
+		return bbp_update_topic( $fields );
+	}
+
+	public function get_object_by_id( $topic_id ) {
+		return bbp_get_topic( $topic_id );
+	}
+}
+
+class BBP_UnitTest_Factory_For_Reply extends WP_UnitTest_Factory_For_Thing {
+
+	public function __construct( $factory = null ) {
+		parent::__construct( $factory );
+
+		$this->default_generation_definitions = array(
+			'post_content' => new WP_UnitTest_Generator_Sequence( 'Content of Reply %s' ),
+		);
+	}
+
+	public function create_object( $args ) {
+		return bbp_insert_reply( $args );
+	}
+
+	public function update_object( $reply_id, $fields ) {
+		$fields['reply_id'] = $reply_id;
+		return bbp_update_reply( $fields );
+	}
+
+	public function get_object_by_id( $reply_id ) {
+		return bbp_get_reply( $reply_id );
 	}
 }
