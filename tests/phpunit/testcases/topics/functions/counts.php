7d9a128162f0542a25a34fe7a2306d32db488d28
Stephen Edgar <netweb@git.wordpress.org>
2015-05-01 05:55:45 +0000

    Tests: Tests for the topic component count functions `/includes/topics/template.php`
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5716 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/topics/functions/counts.php
@@ -0,0 +1,164 @@
+<?php
+
+/**
+ * Tests for the topic component count functions.
+ *
+ * @group topics
+ * @group functions
+ * @group counts
+ */
+class BBP_Tests_Topics_Functions_Counts extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_bump_topic_reply_count
+	 */
+	public function test_bbp_bump_topic_reply_count() {
+		$t = $this->factory->topic->create();
+
+		$count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '0', $count );
+
+		bbp_bump_topic_reply_count( $t );
+
+		$count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '1', $count );
+	}
+
+	/**
+	 * @covers ::bbp_bump_topic_reply_count_hidden
+	 */
+	public function test_bbp_bump_topic_reply_count_hidden() {
+		$t = $this->factory->topic->create();
+
+		$count = bbp_get_topic_reply_count_hidden( $t );
+		$this->assertSame( '0', $count );
+
+		bbp_bump_topic_reply_count_hidden( $t );
+
+		$count = bbp_get_topic_reply_count_hidden( $t );
+		$this->assertSame( '1', $count );
+	}
+
+	/**
+	 * @covers ::bbp_update_topic_reply_count
+	 */
+	public function test_bbp_update_topic_reply_count() {
+		// Create a forum
+		$f = $this->factory->forum->create();
+
+		// Create a topic
+		$t = $this->factory->topic->create( array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			)
+		) );
+
+		$count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '0', $count );
+
+		// Create 3 replies
+		$r1 = $this->factory->reply->create_many( 3, array(
+			'post_parent' => $t,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			)
+		) );
+
+		bbp_update_topic_reply_count( $t );
+
+		$count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '3', $count );
+
+		// Create another reply
+		$r2 = $this->factory->reply->create( array(
+			'post_parent' => $t,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			)
+		) );
+
+		// Test update using reply id
+		bbp_update_topic_reply_count( $r2 );
+
+		$count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '4', $count );
+	}
+
+	/**
+	 * @covers ::bbp_update_topic_reply_count_hidden
+	 */
+	public function test_bbp_update_topic_reply_count_hidden() {
+		$t = $this->factory->topic->create();
+
+		$count = bbp_get_topic_reply_count_hidden( $t );
+		$this->assertSame( '0', $count );
+
+		$r = $this->factory->reply->create_many( 15, array(
+			'post_parent' => $t,
+		) );
+
+		bbp_update_topic_reply_count_hidden( $t );
+
+		$count = bbp_get_topic_reply_count_hidden( $t );
+		$this->assertSame( '0', $count );
+
+		bbp_spam_reply( $r[11] );
+
+		bbp_update_topic_reply_count_hidden( $t );
+
+		$count = bbp_get_topic_reply_count_hidden( $t );
+		$this->assertSame( '1', $count );
+
+		bbp_unapprove_reply( $r[7] );
+
+		bbp_update_topic_reply_count_hidden( $t );
+
+		$count = bbp_get_topic_reply_count_hidden( $t );
+		$this->assertSame( '2', $count );
+	}
+
+	/**
+	 * @covers ::bbp_update_topic_voice_count
+	 */
+	public function test_bbp_update_topic_voice_count() {
+		$u = $this->factory->user->create_many( 2 );
+		$t = $this->factory->topic->create();
+
+		$count = bbp_get_topic_voice_count( $t );
+		$this->assertSame( '1', $count );
+
+		$r = $this->factory->reply->create( array(
+			'post_author' => $u[0],
+			'post_parent' => $t,
+		) );
+
+		bbp_update_topic_voice_count( $t );
+
+		$count = bbp_get_topic_voice_count( $t );
+		$this->assertSame( '2', $count );
+
+		$r = $this->factory->reply->create( array(
+			'post_author' => $u[1],
+			'post_parent' => $t,
+		) );
+
+		bbp_update_topic_voice_count( $t );
+
+		$count = bbp_get_topic_voice_count( $t );
+		$this->assertSame( '3', $count );
+	}
+
+	/**
+	 * @covers ::bbp_update_topic_anonymous_reply_count
+	 * @todo   Implement test_bbp_update_topic_anonymous_reply_count().
+	 */
+	public function test_bbp_update_topic_anonymous_reply_count() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
