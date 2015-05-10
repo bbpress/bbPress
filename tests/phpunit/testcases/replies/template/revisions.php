1bc31bb0185287b12b096a13a2ef6f6549940085
Stephen Edgar <netweb@git.wordpress.org>
2015-05-10 06:25:37 +0000

    Tests: Topic and Reply revisions template function unit tests
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5726 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/replies/template/revisions.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Tests for the reply revision template functions.
+ *
+ * @group replies
+ * @group template
+ * @group revisions
+ */
+class BBP_Tests_Replies_Template_Revisions extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_reply_content_append_revisions
+	 * @todo   Implement test_bbp_reply_content_append_revisions().
+	 */
+	public function test_bbp_reply_content_append_revisions() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_revision_log
+	 * @covers ::bbp_get_reply_revision_log
+	 * @todo   Implement test_bbp_get_reply_revision_log().
+	 */
+	public function test_bbp_get_reply_revision_log() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_reply_raw_revision_log
+	 * @todo   Implement test_bbp_get_reply_raw_revision_log().
+	 */
+	public function test_bbp_get_reply_raw_revision_log() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_reply_revisions
+	 * @todo   Implement test_bbp_get_reply_revisions().
+	 */
+	public function test_bbp_get_reply_revisions() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_reply_revision_count
+	 * @todo   Implement test_bbp_get_reply_revision_count().
+	 */
+	public function test_bbp_get_reply_revision_count() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
---
+++ tests/phpunit/testcases/topics/template/revisions.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Tests for the topic revision template functions.
+ *
+ * @group topics
+ * @group template
+ * @group revisions
+ */
+class BBP_Tests_Topics_Template_Revisions extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_topic_content_append_revisions
+	 * @todo   Implement test_bbp_topic_content_append_revisions().
+	 */
+	public function test_bbp_topic_content_append_revisions() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_revision_log
+	 * @covers ::bbp_get_topic_revision_log
+	 * @todo   Implement test_bbp_get_topic_revision_log().
+	 */
+	public function test_bbp_get_topic_revision_log() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_topic_raw_revision_log
+	 * @todo   Implement test_bbp_get_topic_raw_revision_log().
+	 */
+	public function test_bbp_get_topic_raw_revision_log() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_topic_revisions
+	 * @todo   Implement test_bbp_get_topic_revisions().
+	 */
+	public function test_bbp_get_topic_revisions() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_topic_revision_count
+	 * @todo   Implement test_bbp_get_topic_revision_count().
+	 */
+	public function test_bbp_get_topic_revision_count() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
