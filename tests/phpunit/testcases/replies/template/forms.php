0ecf628a8e72588d016e030959e2d980bc8618c8
Stephen Edgar <netweb@git.wordpress.org>
2015-05-10 05:33:44 +0000

    Tests: Topic and Reply forms template function unit tests
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5724 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/replies/template/forms.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Tests for the `bbp_*_form_reply_*_()` functions.
+ *
+ * @group replies
+ * @group template
+ * @group forms
+ */
+class BBP_Tests_Replies_Template_Forms extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_form_reply_content
+	 * @covers ::bbp_get_form_reply_content
+	 * @todo   Implement test_bbp_get_form_reply_content().
+	 */
+	public function test_bbp_get_form_reply_content() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_reply_to
+	 * @covers ::bbp_get_form_reply_to
+	 * @todo   Implement test_bbp_get_form_reply_to().
+	 */
+	public function test_bbp_get_form_reply_to() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_to_dropdown
+	 * @covers ::bbp_get_reply_to_dropdown
+	 * @todo   Implement test_bbp_get_reply_to_dropdown().
+	 */
+	public function test_bbp_get_reply_to_dropdown() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_reply_log_edit
+	 * @covers ::bbp_get_form_reply_log_edit
+	 * @todo   Implement test_bbp_get_form_reply_log_edit().
+	 */
+	public function test_bbp_get_form_reply_log_edit() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_reply_edit_reason
+	 * @covers ::bbp_get_form_reply_edit_reason
+	 * @todo   Implement test_bbp_get_form_reply_edit_reason().
+	 */
+	public function test_bbp_get_form_reply_edit_reason() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_reply_status_dropdown
+	 * @covers ::bbp_get_form_reply_status_dropdown
+	 * @todo   Implement test_bbp_get_form_reply_status_dropdown().
+	 */
+	public function test_bbp_get_form_reply_status_dropdown() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_reply_form_post_request
+	 * @todo   Implement test_bbp_is_reply_form_post_request().
+	 */
+	public function test_bbp_is_reply_form_post_request() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
---
+++ tests/phpunit/testcases/topics/template/forms.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Tests for the `bbp_*_form_topic_*_()` functions.
+ *
+ * @group topics
+ * @group template
+ * @group forms
+ */
+class BBP_Tests_Topics_Template_Forms extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_form_topic_type_dropdown
+	 * @covers ::bbp_get_form_topic_type_dropdown
+	 * @todo   Implement test_bbp_get_form_topic_type_dropdown().
+	 */
+	public function test_bbp_get_form_topic_type_dropdown() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_topic_status_dropdown
+	 * @covers ::bbp_get_form_topic_status_dropdown
+	 * @todo   Implement test_bbp_get_form_topic_status_dropdown().
+	 */
+	public function test_bbp_get_form_topic_status_dropdown() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_topic_title
+	 * @covers ::bbp_get_form_topic_title
+	 * @todo   Implement test_bbp_get_form_topic_title().
+	 */
+	public function test_bbp_get_form_topic_title() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_topic_content
+	 * @covers ::bbp_get_form_topic_content
+	 * @todo   Implement test_bbp_get_form_topic_content().
+	 */
+	public function test_bbp_get_form_topic_content() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_topic_tags
+	 * @covers ::bbp_get_form_topic_tags
+	 * @todo   Implement test_bbp_get_form_topic_tags().
+	 */
+	public function test_bbp_get_form_topic_tags() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_topic_forum
+	 * @covers ::bbp_get_form_topic_forum
+	 * @todo   Implement test_bbp_get_form_topic_forum().
+	 */
+	public function test_bbp_get_form_topic_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_topic_subscribed
+	 * @covers ::bbp_get_form_topic_subscribed
+	 * @todo   Implement test_bbp_get_form_topic_subscribed().
+	 */
+	public function test_bbp_get_form_topic_subscribed() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_topic_log_edit
+	 * @covers ::bbp_get_form_topic_log_edit
+	 * @todo   Implement test_bbp_get_form_topic_log_edit().
+	 */
+	public function test_bbp_get_form_topic_log_edit() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_form_topic_edit_reason
+	 * @covers ::bbp_get_form_topic_edit_reason
+	 * @todo   Implement test_bbp_get_form_topic_edit_reason().
+	 */
+	public function test_bbp_get_form_topic_edit_reason() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_topic_form_post_request
+	 * @todo   Implement test_bbp_is_topic_form_post_request().
+	 */
+	public function test_bbp_is_topic_form_post_request() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
