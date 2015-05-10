c8a414af2fdab328f7852fab5cc52c08b094b921
Stephen Edgar <netweb@git.wordpress.org>
2015-05-10 05:46:36 +0000

    Tests: Topic and Reply loop template function unit tests
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5725 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/replies/template/loop.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Tests for the reply loop functions.
+ *
+ * @group replies
+ * @group template
+ * @group loop
+ */
+class BBP_Tests_Replies_Template_Reply_Loop extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_has_replies
+	 * @todo   Implement test_bbp_has_replies().
+	 */
+	public function test_bbp_has_replies() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_replies
+	 * @todo   Implement test_bbp_replies().
+	 */
+	public function test_bbp_replies() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_the_reply
+	 * @todo   Implement test_bbp_the_reply().
+	 */
+	public function test_bbp_the_reply() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
---
+++ tests/phpunit/testcases/topics/template/loop.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Tests for the topic loop functions.
+ *
+ * @group topics
+ * @group template
+ * @group loop
+ */
+class BBP_Tests_Topics_Template_Topic_Loop extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_has_topics
+	 * @todo   Implement test_bbp_has_topics().
+	 */
+	public function test_bbp_has_topics() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topics
+	 * @todo   Implement test_bbp_topics().
+	 */
+	public function test_bbp_topics() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_the_topic
+	 * @todo   Implement test_bbp_the_topic().
+	 */
+	public function test_bbp_the_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
