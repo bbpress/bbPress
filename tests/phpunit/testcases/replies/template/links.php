2d429faa3b18cb908efaf6bcbe7afa72e1969d1d
netweb <netweb@9866e705-20ec-0310-96e7-cbb4277adcfb>
2015-05-13 08:42:01 +0000

    Tests: Skeleton test placeholders for reply link template functions
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5758 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/replies/template/links.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * Tests for the `bbp_*_reply_*()` template functions.
+ *
+ * @group replies
+ * @group template
+ * @group links
+ */
+class BBP_Tests_Replies_Template_Links extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_reply_to_link
+	 * @covers ::bbp_get_reply_to_link
+	 * @todo   Implement test_bbp_get_reply_to_link().
+	 */
+	public function test_bbp_get_reply_to_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_cancel_reply_to_link
+	 * @covers ::bbp_get_cancel_reply_to_link
+	 * @todo   Implement test_bbp_get_cancel_reply_to_link().
+	 */
+	public function test_bbp_get_cancel_reply_to_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_admin_links
+	 * @covers ::bbp_get_reply_admin_links
+	 * @todo   Implement test_bbp_get_reply_admin_links().
+	 */
+	public function test_bbp_get_reply_admin_links() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_edit_link
+	 * @covers ::bbp_get_reply_edit_link
+	 * @todo   Implement test_bbp_get_reply_edit_link().
+	 */
+	public function test_bbp_get_reply_edit_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_edit_url
+	 * @covers ::bbp_get_reply_edit_url
+	 * @todo   Implement test_bbp_get_reply_edit_url().
+	 */
+	public function test_bbp_get_reply_edit_url() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_trash_link
+	 * @covers ::bbp_get_reply_trash_link
+	 * @todo   Implement test_bbp_get_reply_trash_link().
+	 */
+	public function test_bbp_get_reply_trash_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_spam_link
+	 * @covers ::bbp_get_reply_spam_link
+	 * @todo   Implement test_bbp_get_reply_spam_link().
+	 */
+	public function test_bbp_get_reply_spam_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_move_link
+	 * @covers ::bbp_get_reply_move_link
+	 * @todo   Implement test_bbp_get_reply_move_link().
+	 */
+	public function test_bbp_get_reply_move_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_split_link
+	 * @covers ::bbp_get_topic_split_link
+	 * @todo   Implement test_bbp_get_topic_split_link().
+	 */
+	public function test_bbp_get_topic_split_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_approve_link
+	 * @covers ::bbp_get_reply_approve_link
+	 * @todo   Implement test_bbp_get_reply_approve_link().
+	 */
+	public function test_bbp_get_reply_approve_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_pagination_links
+	 * @covers ::bbp_get_topic_pagination_links
+	 * @todo   Implement test_bbp_get_topic_pagination_links().
+	 */
+	public function test_bbp_get_topic_pagination_links() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
