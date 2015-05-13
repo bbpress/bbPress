2abd237a23ff5af2c3ea57092c2bc6a8b27ce7a1
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 09:25:33 +0000

    Tests: Skeleton test placeholders for reply status functions.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5762 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/replies/functions/status.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Tests for the reply component functions.
+ *
+ * @group replies
+ * @group functions
+ * @group status
+ */
+class BBP_Tests_Replies_Functions_Status extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_get_reply_statuses
+	 * @todo   Implement test_bbp_get_reply_statuses().
+	 */
+	public function test_bbp_get_reply_statuses() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_spam_reply
+	 * @todo   Implement test_bbp_spam_reply().
+	 */
+	public function test_bbp_spam_reply() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_unspam_reply
+	 * @todo   Implement test_bbp_unspam_reply().
+	 */
+	public function test_bbp_unspam_reply() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_approve_reply
+	 * @todo   Implement test_bbp_approve_reply().
+	 */
+	public function test_bbp_approve_reply() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_unapprove_reply
+	 * @todo   Implement test_bbp_unapprove_reply().
+	 */
+	public function test_bbp_unapprove_reply() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
