31f58832dffa170b628d6c5e602aac5d8e6fe6ee
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 07:06:50 +0000

    Tests: Skeleton test placeholders for search functions and template functions
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5754 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/search/functions/search.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Tests for the search component search functions.
+ *
+ * @group search
+ * @group functions
+ */
+class BBP_Tests_Search_Functions_Search extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_search_query
+	 * @todo   Implement test_bbp_search_query().
+	 */
+	public function test_bbp_search_query() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_search_query_args
+	 * @todo   Implement test_bbp_get_search_query_args().
+	 */
+	public function test_bbp_get_search_query_args() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_search_results_redirect
+	 * @todo   Implement test_bbp_search_results_redirect().
+	 */
+	public function test_bbp_search_results_redirect() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
---
+++ tests/phpunit/testcases/search/template/search.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * Tests for the search component search template functions.
+ *
+ * @group search
+ * @group template
+ */
+class BBP_Tests_Search_Template_Search extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_has_search_results
+	 * @todo   Implement test_bbp_has_search_results().
+	 */
+	public function test_bbp_has_search_results() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_search_results
+	 * @todo   Implement test_bbp_search_results().
+	 */
+	public function test_bbp_search_results() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_the_search_result
+	 * @todo   Implement test_bbp_the_search_result().
+	 */
+	public function test_bbp_the_search_result() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_search_title
+	 * @covers ::bbp_get_search_title
+	 * @todo   Implement test_bbp_get_search_title().
+	 */
+	public function test_bbp_get_search_title() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_search_url
+	 * @covers ::bbp_get_search_url
+	 * @todo   Implement test_bbp_get_search_url().
+	 */
+	public function test_bbp_get_search_url() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_search_results_url
+	 * @covers ::bbp_get_search_results_url
+	 * @todo   Implement test_bbp_get_search_results_url().
+	 */
+	public function test_bbp_get_search_results_url() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_search_terms
+	 * @covers ::bbp_get_search_terms
+	 * @todo   Implement test_bbp_get_search_terms().
+	 */
+	public function test_bbp_get_search_terms() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_search_pagination_count
+	 * @covers ::bbp_get_search_pagination_count
+	 * @todo   Implement test_bbp_get_search_pagination_count().
+	 */
+	public function test_bbp_get_search_pagination_count() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_search_pagination_links
+	 * @covers ::bbp_get_search_pagination_links
+	 * @todo   Implement test_bbp_get_search_pagination_links().
+	 */
+	public function test_bbp_get_search_pagination_links() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
