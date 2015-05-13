1d3193324b8c94cc804ed4c2c0fa3c213c5c6d85
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 06:48:49 +0000

    Tests: Skeleton test placeholders for topic template functions
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5753 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/topics/template/topic.php
@@ -0,0 +1,222 @@
+<?php
+
+/**
+ * Tests for the topics component topic template functions.
+ *
+ * @group topics
+ * @group template
+ * @group topic
+ */
+class BBP_Tests_Topics_Template_Topic extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_show_lead_topic
+	 * @todo   Implement test_bbp_show_lead_topic().
+	 */
+	public function test_bbp_show_lead_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_id
+	 * @covers ::bbp_get_topic_id
+	 * @todo   Implement test_bbp_get_topic_id().
+	 */
+	public function test_bbp_get_topic_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_topic
+	 * @todo   Implement test_bbp_get_topic().
+	 */
+	public function test_bbp_get_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_permalink
+	 * @covers ::bbp_get_topic_permalink
+	 * @todo   Implement test_bbp_get_topic_permalink().
+	 */
+	public function test_bbp_get_topic_permalink() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_title
+	 * @covers ::bbp_get_topic_title
+	 * @todo   Implement test_bbp_get_topic_title().
+	 */
+	public function test_bbp_get_topic_title() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_archive_title
+	 * @covers ::bbp_get_topic_archive_title
+	 * @todo   Implement test_bbp_get_topic_archive_title().
+	 */
+	public function test_bbp_get_topic_archive_title() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_content
+	 * @covers ::bbp_get_topic_content
+	 * @todo   Implement test_bbp_get_topic_content().
+	 */
+	public function test_bbp_get_topic_content() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_excerpt
+	 * @covers ::bbp_get_topic_excerpt
+	 * @todo   Implement test_bbp_get_topic_excerpt().
+	 */
+	public function test_bbp_get_topic_excerpt() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_post_date
+	 * @covers ::bbp_get_topic_post_date
+	 * @todo   Implement test_bbp_get_topic_post_date().
+	 */
+	public function test_bbp_get_topic_post_date() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_pagination
+	 * @covers ::bbp_get_topic_pagination
+	 * @todo   Implement test_bbp_get_topic_pagination().
+	 */
+	public function test_bbp_get_topic_pagination() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_forum_title
+	 * @covers ::bbp_get_topic_forum_title
+	 * @todo   Implement test_bbp_get_topic_forum_title().
+	 */
+	public function test_bbp_get_topic_forum_title() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_forum_id
+	 * @covers ::bbp_get_topic_forum_id
+	 * @todo   Implement test_bbp_get_topic_forum_id().
+	 */
+	public function test_bbp_get_topic_forum_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_class
+	 * @covers ::bbp_get_topic_class
+	 * @todo   Implement test_bbp_get_topic_class().
+	 */
+	public function test_bbp_get_topic_class() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_forum_pagination_count
+	 * @covers ::bbp_get_forum_pagination_count
+	 * @todo   Implement test_bbp_get_forum_pagination_count().
+	 */
+	public function test_bbp_get_forum_pagination_count() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_notices
+	 * @todo   Implement test_bbp_topic_notices().
+	 */
+	public function test_bbp_topic_notices() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_type_select
+	 * @todo   Implement test_bbp_topic_type_select().
+	 */
+	public function test_bbp_topic_type_select() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_single_topic_description
+	 * @covers ::bbp_get_single_topic_description
+	 * @todo   Implement test_bbp_get_single_topic_description().
+	 */
+	public function test_bbp_get_single_topic_description() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_row_actions
+	 * @todo   Implement test_bbp_topic_row_actions().
+	 */
+	public function test_bbp_topic_row_actions() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
