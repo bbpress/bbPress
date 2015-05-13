f6339b33d6ea591e61484a6e37dcad41fb61a8d5
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 05:41:49 +0000

    Tests: Skeleton test placeholders for topic status template functions.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5749 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/topics/template/status.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * Tests for the topic status template functions.
+ *
+ * @group topics
+ * @group template
+ * @group status
+ */
+class BBP_Tests_Topics_Template_Status extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_is_topic_sticky
+	 * @todo   Implement test_bbp_is_topic_sticky().
+	 */
+	public function test_bbp_is_topic_sticky() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_topic_super_sticky
+	 * @todo   Implement test_bbp_is_topic_super_sticky().
+	 */
+	public function test_bbp_is_topic_super_sticky() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_status
+	 * @todo   Implement test_bbp_topic_status().
+	 */
+	public function test_bbp_topic_status() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_topic_status
+	 * @todo   Implement test_bbp_get_topic_status().
+	 */
+	public function test_bbp_get_topic_status() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_topic_closed
+	 * @todo   Implement test_bbp_is_topic_closed().
+	 */
+	public function test_bbp_is_topic_closed() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_topic_open
+	 * @todo   Implement test_bbp_is_topic_open().
+	 */
+	public function test_bbp_is_topic_open() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_topic_published
+	 * @todo   Implement test_bbp_is_topic_published().
+	 */
+	public function test_bbp_is_topic_published() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_topic_spam
+	 * @todo   Implement test_bbp_is_topic_spam().
+	 */
+	public function test_bbp_is_topic_spam() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_topic_trash
+	 * @todo   Implement test_bbp_is_topic_trash().
+	 */
+	public function test_bbp_is_topic_trash() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_topic_pending
+	 * @todo   Implement test_bbp_is_topic_pending().
+	 */
+	public function test_bbp_is_topic_pending() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_topic_private
+	 * @todo   Implement test_bbp_is_topic_private().
+	 */
+	public function test_bbp_is_topic_private() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_is_topic_anonymous
+	 * @todo   Implement test_bbp_is_topic_anonymous().
+	 */
+	public function test_bbp_is_topic_anonymous() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
