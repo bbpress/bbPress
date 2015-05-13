e8225eede3d8fd518df17f9e2eafa4b2f2168d01
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 05:43:43 +0000

    Tests: Skeleton test placeholders for topic tag template functions.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5750 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/topics/template/topic-tag.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Tests for the topic tag template functions.
+ *
+ * @group topics
+ * @group topic_tag
+ * @group template
+ * @group topic
+ * @group topic_tag
+ */
+class BBP_Tests_Topic_Tags_Template_Topic_Tag extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_topic_tag_list
+	 * @covers ::bbp_get_topic_tag_list
+	 * @todo   Implement test_bbp_get_topic_tag_list().
+	 */
+	public function test_bbp_get_topic_tag_list() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_tag_tax_id
+	 * @covers ::bbp_get_topic_tag_tax_id
+	 * @todo   Implement test_bbp_get_topic_tag_tax_id().
+	 */
+	public function test_bbp_get_topic_tag_tax_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_topic_tag_tax_labels
+	 * @todo   Implement test_bbp_get_topic_tag_tax_labels().
+	 */
+	public function test_bbp_get_topic_tag_tax_labels() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_topic_tag_tax_rewrite
+	 * @todo   Implement test_bbp_get_topic_tag_tax_rewrite().
+	 */
+	public function test_bbp_get_topic_tag_tax_rewrite() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_tag_id
+	 * @covers ::bbp_get_topic_tag_id
+	 * @todo   Implement test_bbp_get_topic_tag_id().
+	 */
+	public function test_bbp_get_topic_tag_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_tag_name
+	 * @covers ::bbp_get_topic_tag_name
+	 * @todo   Implement test_bbp_get_topic_tag_name().
+	 */
+	public function test_bbp_get_topic_tag_name() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_tag_slug
+	 * @covers ::bbp_get_topic_tag_slug
+	 * @todo   Implement test_bbp_get_topic_tag_slug().
+	 */
+	public function test_bbp_get_topic_tag_slug() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_tag_link
+	 * @covers ::bbp_get_topic_tag_link
+	 * @todo   Implement test_bbp_get_topic_tag_link().
+	 */
+	public function test_bbp_get_topic_tag_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_tag_edit_link
+	 * @covers ::bbp_get_topic_tag_edit_link
+	 * @todo   Implement test_bbp_get_topic_tag_edit_link().
+	 */
+	public function test_bbp_get_topic_tag_edit_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_tag_description
+	 * @covers ::bbp_get_topic_tag_description
+	 * @todo   Implement test_bbp_get_topic_tag_description().
+	 */
+	public function test_bbp_get_topic_tag_description() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
