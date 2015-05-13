39c06ddcffcad3975e0024bdc494d7c53ef01a53
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 04:28:38 +0000

    Tests: Skeleton test placeholders for forum update last functions
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5745 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/forums/functions/update-last-thing.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Tests for the `bbp_update_forum_last_*()` functions.
+ *
+ * @group forums
+ * @group functions
+ * @group update_last_thing
+ */
+class BBP_Tests_Forums_Functions_Update_Forum_Last_Thing extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_update_forum_last_topic_id
+	 * @todo   Implement test_bbp_update_forum_last_topic_id().
+	 */
+	public function test_bbp_update_forum_last_topic_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_update_forum_last_reply_id
+	 * @todo   Implement test_bbp_update_forum_last_reply_id().
+	 */
+	public function test_bbp_update_forum_last_reply_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_update_forum_last_active_id
+	 * @todo   Implement test_bbp_update_forum_last_active_id().
+	 */
+	public function test_bbp_update_forum_last_active_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_update_forum_last_active_time
+	 * @todo   Implement test_bbp_update_forum_last_active_time().
+	 */
+	public function test_bbp_update_forum_last_active_time() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
