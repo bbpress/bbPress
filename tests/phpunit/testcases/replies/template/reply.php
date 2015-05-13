b0deb33130fea883c8ca2e9f0b174dfe457cc749
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 08:48:05 +0000

    Tests: Skeleton test placeholders for reply template functions
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5759 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/replies/template/reply.php
@@ -0,0 +1,213 @@
+<?php
+
+/**
+ * Tests for the `bbp_*_reply_*()` template functions.
+ *
+ * @group replies
+ * @group template
+ * @group reply
+ */
+class BBP_Tests_Replies_Template_Reply extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_reply_id
+	 * @covers ::bbp_get_reply_id
+	 * @todo   Implement test_bbp_get_reply_id().
+	 */
+	public function test_bbp_get_reply_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_reply
+	 * @todo   Implement test_bbp_get_reply().
+	 */
+	public function test_bbp_get_reply() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_permalink
+	 * @covers ::bbp_get_reply_permalink
+	 * @todo   Implement test_bbp_get_reply_permalink().
+	 */
+	public function test_bbp_get_reply_permalink() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_url
+	 * @covers ::bbp_get_reply_url
+	 * @todo   Implement test_bbp_get_reply_url().
+	 */
+	public function test_bbp_get_reply_url() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_title
+	 * @covers ::bbp_get_reply_title
+	 * @todo   Implement test_bbp_get_reply_title().
+	 */
+	public function test_bbp_get_reply_title() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_reply_title_fallback
+	 * @todo   Implement test_bbp_get_reply_title_fallback().
+	 */
+	public function test_bbp_get_reply_title_fallback() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_content
+	 * @covers ::bbp_get_reply_content
+	 * @todo   Implement test_bbp_get_reply_content().
+	 */
+	public function test_bbp_get_reply_content() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_excerpt
+	 * @covers ::bbp_get_reply_excerpt
+	 * @todo   Implement test_bbp_get_reply_excerpt().
+	 */
+	public function test_bbp_get_reply_excerpt() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_post_date
+	 * @covers ::bbp_get_reply_post_date
+	 * @todo   Implement test_bbp_get_reply_post_date().
+	 */
+	public function test_bbp_get_reply_post_date() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_topic_title
+	 * @covers ::bbp_get_reply_topic_title
+	 * @todo   Implement test_bbp_get_reply_topic_title().
+	 */
+	public function test_bbp_get_reply_topic_title() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_topic_id
+	 * @covers ::bbp_get_reply_topic_id
+	 * @todo   Implement test_bbp_get_reply_topic_id().
+	 */
+	public function test_bbp_get_reply_topic_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_forum_id
+	 * @covers ::bbp_get_reply_forum_id
+	 * @todo   Implement test_bbp_get_reply_forum_id().
+	 */
+	public function test_bbp_get_reply_forum_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_ancestor_id
+	 * @covers ::bbp_get_reply_ancestor_id
+	 * @todo   Implement test_bbp_get_reply_ancestor_id().
+	 */
+	public function test_bbp_get_reply_ancestor_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_to
+	 * @covers ::bbp_get_reply_to
+	 * @todo   Implement test_bbp_get_reply_to().
+	 */
+	public function test_bbp_get_reply_to() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_position
+	 * @covers ::bbp_get_reply_position
+	 * @todo   Implement test_bbp_get_reply_position().
+	 */
+	public function test_bbp_get_reply_position() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_reply_class
+	 * @covers ::bbp_get_reply_class
+	 * @todo   Implement test_bbp_get_reply_class().
+	 */
+	public function test_bbp_get_reply_class() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_pagination_count
+	 * @covers ::bbp_get_topic_pagination_count
+	 * @todo   Implement test_bbp_get_topic_pagination_count().
+	 */
+	public function test_bbp_get_topic_pagination_count() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
