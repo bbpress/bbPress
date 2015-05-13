bf12d4586c3100dbede66e511f6e948d80e927ad
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 06:36:14 +0000

    Tests: Skeleton test placeholders for topic get last template functions
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5751 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/topics/template/get-last-thing.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Tests for the `bbp_get_topic__last_*()` template functions.
+ *
+ * @group topics
+ * @group template
+ * @group get_last_thing
+ */
+class BBP_Tests_Topics_Template_Get_Topic_Last_Thing extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_topic_last_active_id
+	 * @covers ::bbp_get_topic_last_active_id
+	 * @todo   Implement test_bbp_get_topic_last_active_id().
+	 */
+	public function test_bbp_get_topic_last_active_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_last_active_time
+	 * @covers ::bbp_get_topic_last_active_time
+	 * @todo   Implement test_bbp_get_topic_last_active_time().
+	 */
+	public function test_bbp_get_topic_last_active_time() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_last_reply_id
+	 * @covers ::bbp_get_topic_last_reply_id
+	 * @todo   Implement test_bbp_get_topic_last_reply_id().
+	 */
+	public function test_bbp_get_topic_last_reply_id() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_last_reply_title
+	 * @covers ::bbp_get_topic_last_reply_title
+	 * @todo   Implement test_bbp_get_topic_last_reply_title().
+	 */
+	public function test_bbp_get_topic_last_reply_title() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_last_reply_permalink
+	 * @covers ::bbp_get_topic_last_reply_permalink
+	 * @todo   Implement test_bbp_get_topic_last_reply_permalink().
+	 */
+	public function test_bbp_get_topic_last_reply_permalink() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_last_reply_url
+	 * @covers ::bbp_get_topic_last_reply_url
+	 * @todo   Implement test_bbp_get_topic_last_reply_url().
+	 */
+	public function test_bbp_get_topic_last_reply_url() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
