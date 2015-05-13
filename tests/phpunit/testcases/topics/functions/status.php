615a778ac27f73a62fdf96306445ae78ee471a26
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 09:20:25 +0000

    Tests: Skeleton test placeholders for topic status functions.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5761 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/topics/functions/status.php
@@ -0,0 +1,165 @@
+<?php
+
+/**
+ * Tests for the topic component statuses and types functions.
+ *
+ * @group topics
+ * @group functions
+ * @group status
+ */
+class BBP_Tests_Topics_Functions_Status extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_get_topic_statuses
+	 * @todo   Implement test_bbp_get_topic_statuses().
+	 */
+	public function test_bbp_get_topic_statuses() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_topic_types
+	 * @todo   Implement test_bbp_get_topic_types().
+	 */
+	public function test_bbp_get_topic_types() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_stickies
+	 * @todo   Implement test_bbp_get_stickies().
+	 */
+	public function test_bbp_get_stickies() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_super_stickies
+	 * @todo   Implement test_bbp_get_super_stickies().
+	 */
+	public function test_bbp_get_super_stickies() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_close_topic
+	 * @todo   Implement test_bbp_close_topic().
+	 */
+	public function test_bbp_close_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_open_topic
+	 * @todo   Implement test_bbp_open_topic().
+	 */
+	public function test_bbp_open_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_spam_topic
+	 * @todo   Implement test_bbp_spam_topic().
+	 */
+	public function test_bbp_spam_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_spam_topic_replies
+	 * @todo   Implement test_bbp_spam_topic_replies().
+	 */
+	public function test_bbp_spam_topic_replies() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_unspam_topic
+	 * @todo   Implement test_bbp_unspam_topic().
+	 */
+	public function test_bbp_unspam_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_unspam_topic_replies
+	 * @todo   Implement test_bbp_unspam_topic_replies().
+	 */
+	public function test_bbp_unspam_topic_replies() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_stick_topic
+	 * @todo   Implement test_bbp_stick_topic().
+	 */
+	public function test_bbp_stick_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_approve_topic
+	 * @todo   Implement test_bbp_approve_topic().
+	 */
+	public function test_bbp_approve_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_unapprove_topic
+	 * @todo   Implement test_bbp_unapprove_topic().
+	 */
+	public function test_bbp_unapprove_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_unstick_topic
+	 * @todo   Implement test_bbp_unstick_topic().
+	 */
+	public function test_bbp_unstick_topic() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
