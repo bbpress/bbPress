91b43c29638409a5970d4a7a9c0d928c895802d6
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 04:17:35 +0000

    Tests: Skeleton test placeholders for forum visibility functions.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5742 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/forums/functions/visibility.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Tests for bbPress forum visibility functions.
+ *
+ * @group forums
+ * @group functions
+ * @group visibility
+ */
+class BBP_Tests_Forums_Functions_Visibility extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_repair_forum_visibility
+	 * @todo   Implement test_bbp_repair_forum_visibility().
+	 */
+	public function test_bbp_repair_forum_visibility() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_forum_visibilities
+	 * @todo   Implement test_bbp_get_forum_visibilities().
+	 */
+	public function test_bbp_get_forum_visibilities() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_hidden_forum_ids
+	 * @todo   Implement test_bbp_get_hidden_forum_ids().
+	 */
+	public function test_bbp_get_hidden_forum_ids() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_private_forum_ids
+	 * @todo   Implement test_bbp_get_private_forum_ids().
+	 */
+	public function test_bbp_get_private_forum_ids() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_pre_get_posts_normalize_forum_visibility
+	 * @todo   Implement test_bbp_pre_get_posts_normalize_forum_visibility().
+	 */
+	public function test_bbp_pre_get_posts_normalize_forum_visibility() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_forum_enforce_hidden
+	 * @todo   Implement test_bbp_forum_enforce_hidden().
+	 */
+	public function test_bbp_forum_enforce_hidden() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_forum_enforce_private
+	 * @todo   Implement test_bbp_forum_enforce_private().
+	 */
+	public function test_bbp_forum_enforce_private() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
