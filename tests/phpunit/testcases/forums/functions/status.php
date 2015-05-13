d83705e99208153ce0a12196bbba118952f6a38d
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 04:19:46 +0000

    Tests: Skeleton test placeholders for forum statuses and types functions
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5743 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/forums/functions/status.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * Tests for bbPress forum statuses and types functions.
+ *
+ * @group forums
+ * @group functions
+ * @group status
+ */
+class BBP_Tests_Forums_Functions_Status extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_get_forum_statuses
+	 * @todo   Implement test_bbp_get_forum_statuses().
+	 */
+	public function test_bbp_get_forum_statuses() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_forum_types
+	 * @todo   Implement test_bbp_get_forum_types().
+	 */
+	public function test_bbp_get_forum_types() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_close_forum
+	 * @todo   Implement test_bbp_close_forum().
+	 */
+	public function test_bbp_close_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_open_forum
+	 * @todo   Implement test_bbp_open_forum().
+	 */
+	public function test_bbp_open_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_categorize_forum
+	 * @todo   Implement test_bbp_categorize_forum().
+	 */
+	public function test_bbp_categorize_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_normalize_forum
+	 * @todo   Implement test_bbp_normalize_forum().
+	 */
+	public function test_bbp_normalize_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_publicize_forum
+	 * @todo   Implement test_bbp_publicize_forum().
+	 */
+	public function test_bbp_publicize_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_privatize_forum
+	 * @todo   Implement test_bbp_privatize_forum().
+	 */
+	public function test_bbp_privatize_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_hide_forum
+	 * @todo   Implement test_bbp_hide_forum().
+	 */
+	public function test_bbp_hide_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
