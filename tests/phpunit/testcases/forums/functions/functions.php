6e6b76d265a9fe3e80ffd702d2ec8fe36907ef0b
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 04:25:28 +0000

    Tests: Tests: Skeleton test placeholders for forums forum functions
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5744 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/forums/functions/functions.php
@@ -0,0 +1,186 @@
+<?php
+
+/**
+ * Tests for bbPress forum functions.
+ *
+ * @group forums
+ * @group functions
+ * @group forum
+ */
+class BBP_Tests_Forums_Functions_Forum extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_insert_forum
+	 * @todo   Implement test_bbp_insert_forum().
+	 */
+	public function test_bbp_insert_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_new_forum_handler
+	 * @todo   Implement test_bbp_new_forum_handler().
+	 */
+	public function test_bbp_new_forum_handler() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_edit_forum_handler
+	 * @todo   Implement test_bbp_edit_forum_handler().
+	 */
+	public function test_bbp_edit_forum_handler() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_save_forum_extras
+	 * @todo   Implement test_bbp_save_forum_extras().
+	 */
+	public function test_bbp_save_forum_extras() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_remove_forum_from_all_subscriptions
+	 * @todo   Implement test_bbp_remove_forum_from_all_subscriptions().
+	 */
+	public function test_bbp_remove_forum_from_all_subscriptions() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_update_forum
+	 * @todo   Implement test_bbp_update_forum().
+	 */
+	public function test_bbp_update_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_check_forum_edit
+	 * @todo   Implement test_bbp_check_forum_edit().
+	 */
+	public function test_bbp_check_forum_edit() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+	/**
+	 * @covers ::bbp_delete_forum_topics
+	 * @todo   Implement test_bbp_delete_forum_topics().
+	 */
+	public function test_bbp_delete_forum_topics() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_trash_forum_topics
+	 * @todo   Implement test_bbp_trash_forum_topics().
+	 */
+	public function test_bbp_trash_forum_topics() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_untrash_forum_topics
+	 * @todo   Implement test_bbp_untrash_forum_topics().
+	 */
+	public function test_bbp_untrash_forum_topics() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_delete_forum
+	 * @todo   Implement test_bbp_delete_forum().
+	 */
+	public function test_bbp_delete_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_trash_forum
+	 * @todo   Implement test_bbp_trash_forum().
+	 */
+	public function test_bbp_trash_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_untrash_forum
+	 * @todo   Implement test_bbp_untrash_forum().
+	 */
+	public function test_bbp_untrash_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_deleted_forum
+	 * @todo   Implement test_bbp_deleted_forum().
+	 */
+	public function test_bbp_deleted_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_trashed_forum
+	 * @todo   Implement test_bbp_trashed_forum().
+	 */
+	public function test_bbp_trashed_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_untrashed_forum
+	 * @todo   Implement test_bbp_untrashed_forum().
+	 */
+	public function test_bbp_untrashed_forum() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
