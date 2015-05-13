7ff6072a34a4eff92b1f8af2aa55f22689e4dd14
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 05:30:38 +0000

    Tests: Skeleton test placeholders for user component functions.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5748 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/users/functions/functions.php
@@ -0,0 +1,175 @@
+<?php
+
+/**
+ * Tests for the user component functions.
+ *
+ * @group users
+ * @group functions
+ */
+ class BBP_Tests_Users_Functions extends BBP_UnitTestCase {
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_redirect_login
+	 * @todo   Implement test_bbp_redirect_login().
+	 */
+	public function test_bbp_redirect_login() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_is_anonymous
+	 * @todo   Implement test_bbp_is_anonymous().
+	 */
+	public function test_bbp_is_anonymous() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_current_anonymous_user_data
+	 * @todo   Implement test_bbp_current_anonymous_user_data().
+	 */
+	public function test_bbp_current_anonymous_user_data() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_get_current_anonymous_user_data
+	 * @todo   Implement test_bbp_get_current_anonymous_user_data().
+	 */
+	public function test_bbp_get_current_anonymous_user_data() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_set_current_anonymous_user_data
+	 * @todo   Implement test_bbp_set_current_anonymous_user_data().
+	 */
+	public function test_bbp_set_current_anonymous_user_data() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_current_author_ip
+	 * @todo   Implement test_bbp_current_author_ip().
+	 */
+	public function test_bbp_current_author_ip() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_current_author_ua
+	 * @todo   Implement test_bbp_current_author_ua().
+	 */
+	public function test_bbp_current_author_ua() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_edit_user_handler
+	 * @todo   Implement test_bbp_edit_user_handler().
+	 */
+	public function test_bbp_edit_user_handler() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_user_email_change_handler
+	 * @todo   Implement test_bbp_user_email_change_handler().
+	 */
+	public function test_bbp_user_email_change_handler() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_edit_user_email_send_notification
+	 * @todo   Implement test_bbp_edit_user_email_send_notification().
+	 */
+	public function test_bbp_edit_user_email_send_notification() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_user_edit_after
+	 * @todo   Implement test_bbp_user_edit_after().
+	 */
+	public function test_bbp_user_edit_after() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_check_user_edit
+	 * @todo   Implement test_bbp_check_user_edit().
+	 */
+	public function test_bbp_check_user_edit() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_forum_enforce_blocked
+	 * @todo   Implement test_bbp_forum_enforce_blocked().
+	 */
+	public function test_bbp_forum_enforce_blocked() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_sanitize_displayed_user_field
+	 * @todo   Implement test_bbp_sanitize_displayed_user_field().
+	 */
+	public function test_bbp_sanitize_displayed_user_field() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers BBP_Users_Functions::bbp_user_maybe_convert_pass
+	 * @todo   Implement test_bbp_user_maybe_convert_pass().
+	 */
+	public function test_bbp_user_maybe_convert_pass() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
