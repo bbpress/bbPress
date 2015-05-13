990f054e464bc4bc549a809864cf2b8add8af5af
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 06:47:53 +0000

    Tests: Skeleton test placeholders for topic link template functions
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5752 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/topics/template/links.php
@@ -0,0 +1,191 @@
+<?php
+
+/**
+ * Tests for the topics component link template functions.
+ *
+ * @group topics
+ * @group template
+ * @group links
+ */
+class BBP_Tests_Topics_Template_Links extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_topic_subscription_link
+	 * @covers ::bbp_get_topic_subscription_link
+	 * @todo   Implement test_bbp_get_topic_subscription_link().
+	 */
+	public function test_bbp_get_topic_subscription_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_favorite_link
+	 * @covers ::bbp_get_topic_favorite_link
+	 * @todo   Implement test_bbp_get_topic_favorite_link().
+	 */
+	public function test_bbp_get_topic_favorite_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_freshness_link
+	 * @covers ::bbp_get_topic_freshness_link
+	 * @todo   Implement test_bbp_get_topic_freshness_link().
+	 */
+	public function test_bbp_get_topic_freshness_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_replies_link
+	 * @covers ::bbp_get_topic_replies_link
+	 * @todo   Implement test_bbp_get_topic_replies_link().
+	 */
+	public function test_bbp_get_topic_replies_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_admin_links
+	 * @covers ::bbp_get_topic_admin_links
+	 * @todo   Implement test_bbp_get_topic_admin_links().
+	 */
+	public function test_bbp_get_topic_admin_links() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_edit_link
+	 * @covers ::bbp_get_topic_edit_link
+	 * @todo   Implement test_bbp_get_topic_edit_link().
+	 */
+	public function test_bbp_get_topic_edit_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_edit_url
+	 * @covers ::bbp_get_topic_edit_url
+	 * @todo   Implement test_bbp_get_topic_edit_url().
+	 */
+	public function test_bbp_get_topic_edit_url() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_trash_link
+	 * @covers ::bbp_get_topic_trash_link
+	 * @todo   Implement test_bbp_get_topic_trash_link().
+	 */
+	public function test_bbp_get_topic_trash_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_close_link
+	 * @covers ::bbp_get_topic_close_link
+	 * @todo   Implement test_bbp_get_topic_close_link().
+	 */
+	public function test_bbp_get_topic_close_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_approve_link
+	 * @covers ::bbp_get_topic_approve_link
+	 * @todo   Implement test_bbp_get_topic_approve_link().
+	 */
+	public function test_bbp_get_topic_approve_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_stick_link
+	 * @covers ::bbp_get_topic_stick_link
+	 * @todo   Implement test_bbp_get_topic_stick_link().
+	 */
+	public function test_bbp_get_topic_stick_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_merge_link
+	 * @covers ::bbp_get_topic_merge_link
+	 * @todo   Implement test_bbp_get_topic_merge_link().
+	 */
+	public function test_bbp_get_topic_merge_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_spam_link
+	 * @covers ::bbp_get_topic_spam_link
+	 * @todo   Implement test_bbp_get_topic_spam_link().
+	 */
+	public function test_bbp_get_topic_spam_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_topic_reply_link
+	 * @covers ::bbp_get_topic_reply_link
+	 * @todo   Implement test_bbp_get_topic_reply_link().
+	 */
+	public function test_bbp_get_topic_reply_link() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_forum_pagination_links
+	 * @covers ::bbp_get_forum_pagination_links
+	 * @todo   Implement test_bbp_get_forum_pagination_links().
+	 */
+	public function test_bbp_get_forum_pagination_links() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
