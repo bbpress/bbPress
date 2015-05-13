7d28eae4958532aa349bacf4022a8345c88e1462
Stephen Edgar <netweb@git.wordpress.org>
2015-05-13 07:50:58 +0000

    Tests: Skeleton test placeholders for topic tag functions.
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5757 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/topics/functions/topic-tag.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Tests for the topic component functions.
+ *
+ * @group topics
+ * @group topic_tags
+ * @group functions
+ * @group topic
+ * @group topic_tag
+ */
+class BBP_Tests_Topics_Functions_Topic_Tag extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_edit_topic_tag_handler
+	 * @todo   Implement test_bbp_edit_topic_tag_handler().
+	 */
+	public function test_bbp_edit_topic_tag_handler() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_spam_topic_tags
+	 * @todo   Implement test_bbp_spam_topic_tags().
+	 */
+	public function test_bbp_spam_topic_tags() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_unspam_topic_tags
+	 * @todo   Implement test_bbp_unspam_topic_tags().
+	 */
+	public function test_bbp_unspam_topic_tags() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_topic_tag_names
+	 * @todo   Implement test_bbp_get_topic_tag_names().
+	 */
+	public function test_bbp_get_topic_tag_names() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_check_topic_tag_edit
+	 * @todo   Implement test_bbp_check_topic_tag_edit().
+	 */
+	public function test_bbp_check_topic_tag_edit() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
