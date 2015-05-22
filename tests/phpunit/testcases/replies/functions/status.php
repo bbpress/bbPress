f1465d849c48ad60297906d2563bf92cf3f4874a
Stephen Edgar <netweb@git.wordpress.org>
2015-05-22 11:34:28 +0000

    Tests: Spam and Unspam reply function tests: * `test_bbp_spam_reply()` for `bbp_spam_reply()` * `test_bbp_unspam_reply()` for `bbp_unspam_reply()`
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5781 9866e705-20ec-0310-96e7-cbb4277adcfb

--- tests/phpunit/testcases/replies/functions/status.php
+++ tests/phpunit/testcases/replies/functions/status.php
@@ -22,24 +22,87 @@ class BBP_Tests_Replies_Functions_Status extends BBP_UnitTestCase {
 
 	/**
 	 * @covers ::bbp_spam_reply
-	 * @todo   Implement test_bbp_spam_reply().
 	 */
 	public function test_bbp_spam_reply() {
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+
+		// Create a forum
+		$f = $this->factory->forum->create();
+
+		// Create a topic
+		$t = $this->factory->topic->create( array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			)
+		) );
+
+		// Create some replies
+		$r = $this->factory->reply->create_many( 3, array(
+			'post_parent' => $t,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			)
+		) );
+
+		bbp_spam_reply( $r[1] );
+
+		$reply_post_status = bbp_get_reply_status( $r[1] );
+		$this->assertSame( 'spam', $reply_post_status );
+
+		$reply_spam_meta_status = get_post_meta( $r[1], '_bbp_spam_meta_status', true );
+		$this->assertSame( 'publish', $reply_spam_meta_status );
+
+		$topic_reply_count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '2', $topic_reply_count );
 	}
 
 	/**
 	 * @covers ::bbp_unspam_reply
-	 * @todo   Implement test_bbp_unspam_reply().
 	 */
 	public function test_bbp_unspam_reply() {
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+
+		// Create a forum
+		$f = $this->factory->forum->create();
+
+		// Create a topic
+		$t = $this->factory->topic->create( array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			)
+		) );
+
+		// Create some replies
+		$r = $this->factory->reply->create_many( 3, array(
+			'post_parent' => $t,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			)
+		) );
+
+		bbp_spam_reply( $r[1] );
+
+		$reply_post_status = bbp_get_reply_status( $r[1] );
+		$this->assertSame( 'spam', $reply_post_status );
+
+		$reply_spam_meta_status = get_post_meta( $r[1], '_bbp_spam_meta_status', true );
+		$this->assertSame( 'publish', $reply_spam_meta_status );
+
+		$topic_reply_count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '2', $topic_reply_count );
+
+		bbp_unspam_reply( $r[1] );
+
+		$reply_post_status = bbp_get_reply_status( $r[1] );
+		$this->assertSame( 'publish', $reply_post_status );
+
+		$reply_spam_meta_status = get_post_meta( $r[1], '_bbp_spam_meta_status', true );
+		$this->assertSame( '', $reply_spam_meta_status );
+
+		$topic_reply_count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '3', $topic_reply_count );
 	}
 
 	/**
