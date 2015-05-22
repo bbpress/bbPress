f36f272bb9096a6142a3fd2a26bae83a33e4eebd
Stephen Edgar <netweb@git.wordpress.org>
2015-05-22 10:37:26 +0000

    Tests: Updated topic component count functions tests Props thebrandonallen, netweb. See #2801
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5780 9866e705-20ec-0310-96e7-cbb4277adcfb

--- tests/phpunit/testcases/topics/functions/counts.php
+++ tests/phpunit/testcases/topics/functions/counts.php
@@ -60,6 +60,7 @@ class BBP_Tests_Topics_Functions_Counts extends BBP_UnitTestCase {
 			)
 		) );
 
+		// Start with zero
 		$count = bbp_get_topic_reply_count( $t );
 		$this->assertSame( '0', $count );
 
@@ -72,13 +73,10 @@ class BBP_Tests_Topics_Functions_Counts extends BBP_UnitTestCase {
 			)
 		) );
 
-		// @todo Investigate caching issues in bbp_get_public_child_count()
-		wp_cache_flush();
+		$count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '3', $count );
 
 		bbp_update_topic_reply_count( $t );
-
-		// @todo Investigate caching issues in bbp_get_public_child_count()
-		wp_cache_flush();
 
 		$count = bbp_get_topic_reply_count( $t );
 		$this->assertSame( '3', $count );
@@ -94,40 +92,62 @@ class BBP_Tests_Topics_Functions_Counts extends BBP_UnitTestCase {
 
 		// Test update using reply id
 		bbp_update_topic_reply_count( $r2 );
-
 		$count = bbp_get_topic_reply_count( $t );
 		$this->assertSame( '4', $count );
+
+		// Spam a reply
+		bbp_spam_reply( $r2 );
+
+		bbp_update_topic_reply_count( $t );
+		$count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '3', $count );
+
+		// Set the reply count manually
+		bbp_update_topic_reply_count( $t, 7 );
+		$count = bbp_get_topic_reply_count( $t );
+		$this->assertSame( '7', $count );
 	}
 
 	/**
 	 * @covers ::bbp_update_topic_reply_count_hidden
 	 */
 	public function test_bbp_update_topic_reply_count_hidden() {
-		$t = $this->factory->topic->create();
+		// Create a forum
+		$f = $this->factory->forum->create();
 
+		// Create a topic
+		$t = $this->factory->topic->create( array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			)
+		) );
+
+		// Start with zero
 		$count = bbp_get_topic_reply_count_hidden( $t );
 		$this->assertSame( '0', $count );
 
-		$r = $this->factory->reply->create_many( 15, array(
+		$r = $this->factory->reply->create_many( 3, array(
 			'post_parent' => $t,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			)
 		) );
 
 		bbp_update_topic_reply_count_hidden( $t );
-
 		$count = bbp_get_topic_reply_count_hidden( $t );
 		$this->assertSame( '0', $count );
 
-		bbp_spam_reply( $r[11] );
+		bbp_spam_reply( $r[2] );
 
 		bbp_update_topic_reply_count_hidden( $t );
-
 		$count = bbp_get_topic_reply_count_hidden( $t );
 		$this->assertSame( '1', $count );
 
-		bbp_unapprove_reply( $r[7] );
+		bbp_unapprove_reply( $r[0] );
 
 		bbp_update_topic_reply_count_hidden( $t );
-
 		$count = bbp_get_topic_reply_count_hidden( $t );
 		$this->assertSame( '2', $count );
 	}
@@ -148,9 +168,11 @@ class BBP_Tests_Topics_Functions_Counts extends BBP_UnitTestCase {
 		) );
 
 		bbp_update_topic_voice_count( $t );
-
 		$count = bbp_get_topic_voice_count( $t );
 		$this->assertSame( '2', $count );
+
+		$count = bbp_update_topic_voice_count( $t );
+		$this->assertSame( 2, $count );
 
 		$r = $this->factory->reply->create( array(
 			'post_author' => $u[1],
@@ -158,7 +180,6 @@ class BBP_Tests_Topics_Functions_Counts extends BBP_UnitTestCase {
 		) );
 
 		bbp_update_topic_voice_count( $t );
-
 		$count = bbp_get_topic_voice_count( $t );
 		$this->assertSame( '3', $count );
 	}
