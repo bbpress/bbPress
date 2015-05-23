1952f3f7e3c24fe937892a1180d9e3ed8aef3e9d
Stephen Edgar <netweb@git.wordpress.org>
2015-05-23 06:50:53 +0000

    Tests: Tests for the common component query functions * `public function test_bbp_get_public_child_ids()` for `bbp_get_public_child_ids()` * `public function test_bbp_get_public_child_count()` for `bbp_get_public_child_count()`
    Props thebrandonallen, netweb. See #2801
    
    
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5791 9866e705-20ec-0310-96e7-cbb4277adcfb

--- tests/phpunit/testcases/common/query.php
+++ tests/phpunit/testcases/common/query.php
@@ -12,18 +12,199 @@ class BBP_Tests_Common_Functions_Query extends BBP_UnitTestCase {
 	/**
 	 * @group  counts
 	 * @covers ::bbp_get_public_child_count
-	 * @todo   Implement test_bbp_get_public_child_ids().
 	 */
 	public function test_bbp_get_public_child_count() {
+		$f = $this->factory->forum->create();
 
+		// Test initial forum public child counts
+		$count = bbp_get_public_child_count( $f, bbp_get_forum_post_type() );
+		$this->assertSame( 0, $count );
+
+		$count = bbp_get_public_child_count( $f, bbp_get_topic_post_type() );
+		$this->assertSame( 0, $count );
+
+		/* Sub-Forums *********************************************************/
+
+		$this->factory->forum->create_many( 3, array(
+			'post_parent' => $f,
+		) );
+
+		$this->factory->forum->create( array(
+			'post_parent' => $f,
+			'post_status' => bbp_get_private_status_id(),
+		) );
+
+		$count = bbp_get_public_child_count( $f, bbp_get_forum_post_type() );
+		$this->assertSame( 3, $count );
+
+		$this->factory->forum->create_many( 2, array(
+			'post_parent' => $f,
+		) );
+
+		$count = bbp_get_public_child_count( $f, bbp_get_forum_post_type() );
+		$this->assertSame( 5, $count );
+
+		/* Topics *************************************************************/
+
+		$t1 = $this->factory->topic->create_many( 3, array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			),
+		) );
+
+		$this->factory->topic->create( array(
+			'post_parent' => $f,
+			'post_status' => bbp_get_spam_status_id(),
+			'topic_meta' => array(
+				'forum_id' => $f,
+			),
+		) );
+
+		$count = bbp_get_public_child_count( $f, bbp_get_topic_post_type() );
+		$this->assertSame( 3, $count );
+
+		$this->factory->topic->create_many( 2, array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			),
+		) );
+
+		$count = bbp_get_public_child_count( $f, bbp_get_topic_post_type() );
+		$this->assertSame( 5, $count );
+
+		/* Replies ************************************************************/
+
+		$this->factory->reply->create_many( 3, array(
+			'post_parent' => $t1[0],
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t1[0],
+			),
+		) );
+
+		$this->factory->reply->create( array(
+			'post_parent' => $t1[0],
+			'post_status' => bbp_get_spam_status_id(),
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t1[0],
+			),
+		) );
+
+		$count = bbp_get_public_child_count( $t1[0], bbp_get_reply_post_type() );
+		$this->assertSame( 3, $count );
+
+		$this->factory->reply->create_many( 2, array(
+			'post_parent' => $t1[0],
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t1[0],
+			),
+		) );
+
+		$count = bbp_get_public_child_count( $t1[0], bbp_get_reply_post_type() );
+		$this->assertSame( 5, $count );
 	}
 
 	/**
 	 * @covers ::bbp_get_public_child_ids
-	 * @todo   Implement test_bbp_get_public_child_ids().
 	 */
 	public function test_bbp_get_public_child_ids() {
+		$f = $this->factory->forum->create();
 
+		// Test initial forum public child counts
+		$count = count( bbp_get_public_child_ids( $f, bbp_get_forum_post_type() ) );
+		$this->assertSame( 0, $count );
+
+		$count = count( bbp_get_public_child_ids( $f, bbp_get_topic_post_type() ) );
+		$this->assertSame( 0, $count );
+
+		/* Sub-Forums *********************************************************/
+
+		$this->factory->forum->create_many( 3, array(
+			'post_parent' => $f,
+		) );
+
+		$this->factory->forum->create( array(
+			'post_parent' => $f,
+			'post_status' => bbp_get_private_status_id(),
+		) );
+
+		bbp_clean_post_cache( $f );
+		$count = count( bbp_get_public_child_ids( $f, bbp_get_forum_post_type() ) );
+		$this->assertSame( 3, $count );
+
+		$this->factory->forum->create_many( 2, array(
+			'post_parent' => $f,
+		) );
+
+		$count = count( bbp_get_public_child_ids( $f, bbp_get_forum_post_type() ) );
+		$this->assertSame( 5, $count );
+
+		/* Topics *************************************************************/
+
+		$t1 = $this->factory->topic->create_many( 3, array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			),
+		) );
+
+		$this->factory->topic->create( array(
+			'post_parent' => $f,
+			'post_status' => bbp_get_spam_status_id(),
+			'topic_meta' => array(
+				'forum_id' => $f,
+			),
+		) );
+
+		$count = count( bbp_get_public_child_ids( $f, bbp_get_topic_post_type() ) );
+		$this->assertSame( 3, $count );
+
+		$this->factory->topic->create_many( 2, array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			),
+		) );
+
+		$count = count( bbp_get_public_child_ids( $f, bbp_get_topic_post_type() ) );
+		$this->assertSame( 5, $count );
+
+		/* Replies ************************************************************/
+
+		$this->factory->reply->create_many( 3, array(
+			'post_parent' => $t1[0],
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t1[0],
+			),
+		) );
+
+		$this->factory->reply->create( array(
+			'post_parent' => $t1[0],
+			'post_status' => bbp_get_spam_status_id(),
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t1[0],
+			),
+		) );
+
+		$count = count( bbp_get_public_child_ids( $t1[0], bbp_get_reply_post_type() ) );
+		$this->assertSame( 3, $count );
+
+		$this->factory->reply->create_many( 2, array(
+			'post_parent' => $t1[0],
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t1[0],
+			),
+		) );
+
+		$count = count( bbp_get_public_child_ids( $t1[0], bbp_get_reply_post_type() ) );
+		$this->assertSame( 5, $count );
 	}
 
 	/**
