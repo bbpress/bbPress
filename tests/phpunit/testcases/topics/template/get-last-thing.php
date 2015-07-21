97b126b599fae3aecdacd139e9cf9105211698b2
Stephen Edgar <netweb@git.wordpress.org>
2015-07-21 10:06:59 +0000

    Tests: Complete topic template tests in `BBP_Tests_Topics_Template_Get_Topic_Last_Thing`
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5873 9866e705-20ec-0310-96e7-cbb4277adcfb

--- tests/phpunit/testcases/topics/template/get-last-thing.php
+++ tests/phpunit/testcases/topics/template/get-last-thing.php
@@ -12,72 +12,195 @@ class BBP_Tests_Topics_Template_Get_Topic_Last_Thing extends BBP_UnitTestCase {
 	/**
 	 * @covers ::bbp_topic_last_active_id
 	 * @covers ::bbp_get_topic_last_active_id
-	 * @todo   Implement test_bbp_get_topic_last_active_id().
 	 */
 	public function test_bbp_get_topic_last_active_id() {
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+		$f = $this->factory->forum->create();
+		$t = $this->factory->topic->create( array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			)
+		) );
+
+		$topic_last_active_id = bbp_get_topic_last_active_id( $t );
+		$this->assertSame( $t, $topic_last_active_id );
+
+		$r = $this->factory->reply->create( array(
+			'post_parent' => $t,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			)
+		) );
+
+		$topic_last_active_id = bbp_get_topic_last_active_id( $t );
+		$this->assertSame( $r, $topic_last_active_id );
 	}
 
 	/**
 	 * @covers ::bbp_topic_last_active_time
 	 * @covers ::bbp_get_topic_last_active_time
-	 * @todo   Implement test_bbp_get_topic_last_active_time().
 	 */
 	public function test_bbp_get_topic_last_active_time() {
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+		$f = $this->factory->forum->create();
+
+		$now = time();
+		$post_date_topic = date( 'Y-m-d H:i:s', $now - 60*60*100 );
+		$post_date_reply = date( 'Y-m-d H:i:s', $now - 60*60*80 );
+
+		$topic_time = '4 days, 4 hours ago';
+		$reply_time = '1 hour, 6 minutes ago';
+
+		$t = $this->factory->topic->create( array(
+			'post_parent' => $f,
+			'post_date' => $post_date_topic,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			),
+		) );
+
+		// Output.
+		$this->expectOutputString( $topic_time );
+		bbp_topic_last_active_time( $t );
+
+		// Topic time.
+		$datetime = bbp_get_topic_last_active_time( $t );
+		$this->assertSame( $topic_time, $datetime );
+
+		$this->factory->reply->create( array(
+			'post_parent' => $t,
+			'post_date' => $post_date_reply,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			),
+		) );
+
+		// Reply time.
+		$datetime = bbp_get_topic_last_active_time( $t );
+		$this->assertSame( '1 hour, 6 minutes ago', $reply_time );
+
 	}
 
 	/**
 	 * @covers ::bbp_topic_last_reply_id
 	 * @covers ::bbp_get_topic_last_reply_id
-	 * @todo   Implement test_bbp_get_topic_last_reply_id().
 	 */
 	public function test_bbp_get_topic_last_reply_id() {
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+		$f = $this->factory->forum->create();
+		$t = $this->factory->topic->create( array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			)
+		) );
+
+		$topic_last_reply_id = bbp_get_topic_last_reply_id( $t );
+		$this->assertSame( 0, $topic_last_reply_id );
+
+		$r = $this->factory->reply->create( array(
+			'post_parent' => $t,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			)
+		) );
+
+		$topic_last_reply_id = bbp_get_topic_last_reply_id( $t );
+		$this->assertSame( $r, $topic_last_reply_id );
 	}
 
 	/**
 	 * @covers ::bbp_topic_last_reply_title
 	 * @covers ::bbp_get_topic_last_reply_title
-	 * @todo   Implement test_bbp_get_topic_last_reply_title().
 	 */
 	public function test_bbp_get_topic_last_reply_title() {
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+		$f = $this->factory->forum->create();
+		$t = $this->factory->topic->create( array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			)
+		) );
+
+		$title = bbp_get_topic_last_reply_title( $t );
+		$this->assertSame( '', $title );
+
+		$this->factory->reply->create( array(
+			'post_parent' => $t,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			)
+		) );
+
+		$title = bbp_get_topic_last_reply_title( $t );
+		$this->assertSame( 'Reply To: ' . bbp_get_topic_title( $t ), $title );
 	}
 
 	/**
 	 * @covers ::bbp_topic_last_reply_permalink
 	 * @covers ::bbp_get_topic_last_reply_permalink
-	 * @todo   Implement test_bbp_get_topic_last_reply_permalink().
 	 */
 	public function test_bbp_get_topic_last_reply_permalink() {
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+		if ( is_multisite() ) {
+			$this->markTestSkipped( 'Skipping URL tests in multiste for now.' );
+		}
+
+		$f = $this->factory->forum->create();
+
+		$t = $this->factory->topic->create( array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			)
+		) );
+
+		$topic_last_reply_permalink = bbp_get_topic_last_reply_permalink( $f );
+		$this->assertSame( bbp_get_topic_permalink( $t ), $topic_last_reply_permalink );
+
+		$r = $this->factory->reply->create( array(
+			'post_parent' => $t,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			)
+		) );
+
+		$topic_last_reply_permalink = bbp_get_topic_last_reply_permalink( $f );
+		$this->assertSame( bbp_get_reply_permalink( $r ), $topic_last_reply_permalink );
 	}
 
 	/**
 	 * @covers ::bbp_topic_last_reply_url
 	 * @covers ::bbp_get_topic_last_reply_url
-	 * @todo   Implement test_bbp_get_topic_last_reply_url().
 	 */
 	public function test_bbp_get_topic_last_reply_url() {
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+		if ( is_multisite() ) {
+			$this->markTestSkipped( 'Skipping URL tests in multiste for now.' );
+		}
+
+		$f = $this->factory->forum->create();
+
+		$t = $this->factory->topic->create( array(
+			'post_parent' => $f,
+			'topic_meta' => array(
+				'forum_id' => $f,
+			)
+		) );
+
+		$topic_last_reply_url = bbp_get_topic_last_reply_url( $t );
+		$this->assertSame( get_permalink( $t ), $topic_last_reply_url );
+
+		$r = $this->factory->reply->create( array(
+			'post_parent' => $t,
+			'reply_meta' => array(
+				'forum_id' => $f,
+				'topic_id' => $t,
+			)
+		) );
+
+		$topic_last_reply_url = bbp_get_topic_last_reply_url( $t );
+		$this->assertSame( bbp_get_reply_url( $r ), $topic_last_reply_url );
 	}
 }
