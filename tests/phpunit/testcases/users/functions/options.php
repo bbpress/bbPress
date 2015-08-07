7fec5979ee4bc2df042f72a838e99b5523ab5189
Stephen Edgar <netweb@git.wordpress.org>
2015-08-07 07:46:18 +0000

    Tests: Skeleton test placeholders for user options functions
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5887 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/users/functions/options.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Tests for the user component count functions.
+ *
+ * @group users
+ * @group functions
+ * @group options
+ */
+class BBP_Tests_Users_Functions_Options extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_get_default_user_options
+	 * @todo   Implement test_bbp_get_default_user_options().
+	 */
+	public function test_bbp_get_default_user_options() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_add_user_options
+	 * @todo   Implement test_bbp_add_user_options().
+	 */
+	public function test_bbp_add_user_options() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_delete_user_options
+	 * @todo   Implement test_bbp_delete_user_options().
+	 */
+	public function test_bbp_delete_user_options() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_setup_user_option_filters
+	 * @todo   Implement test_bbp_setup_user_option_filters().
+	 */
+	public function test_bbp_setup_user_option_filters() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_filter_get_user_option
+	 * @todo   Implement test_bbp_filter_get_user_option().
+	 */
+	public function test_bbp_filter_get_user_option() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_update_user_last_posted
+	 * @todo   Implement test_bbp_update_user_last_posted().
+	 */
+	public function test_bbp_update_user_last_posted() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_user_last_posted
+	 * @covers ::bbp_get_user_last_posted
+	 * @todo   Implement test_bbp_get_user_last_posted().
+	 */
+	public function test_bbp_get_user_last_posted() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
