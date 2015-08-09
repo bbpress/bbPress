9e42f5d486ac1755a98ffcaf6cf0666936d847b0
Stephen Edgar <netweb@git.wordpress.org>
2015-08-09 08:08:33 +0000

    Tests: Skeleton test placeholders for core abstraction function wrappers
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@5898 9866e705-20ec-0310-96e7-cbb4277adcfb

---
+++ tests/phpunit/testcases/core/abstraction.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Tests for core abstraction function wrappers
+ *
+ * @group core
+ * @group abstraction
+ */
+
+class BBP_Tests_Core_Abstraction extends BBP_UnitTestCase {
+
+	/**
+	 * @covers ::bbp_get_global_object
+	 * @todo   Implement test_bbp_get_global_object().
+	 */
+	public function test_bbp_get_global_object() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_db
+	 * @todo   Implement test_bbp_db().
+	 */
+	public function test_bbp_db() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_rewrite
+	 * @todo   Implement test_bbp_rewrite().
+	 */
+	public function test_bbp_rewrite() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_root_url
+	 * @todo   Implement test_bbp_get_root_url().
+	 */
+	public function test_bbp_get_root_url() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_get_paged_slug
+	 * @todo   Implement test_bbp_get_paged_slug().
+	 */
+	public function test_bbp_get_paged_slug() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * @covers ::bbp_use_pretty_urls
+	 * @todo   Implement test_bbp_use_pretty_urls().
+	 */
+	public function test_bbp_use_pretty_urls() {
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
