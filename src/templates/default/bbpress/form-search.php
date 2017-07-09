4e65388a9b8a2c06efbb3cb033171f29cde687bb
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2017-07-09 17:16:57 +0000

    Templates: Add form template parts for searching specifically for topics & replies.
    These have been in-use on BuddyPress.org/bbPress.org for years, and will be useful in user-profiles soon.
    
    Also use `bbp_search_terms()` in `form-search.php` now that it's properly escaping output.
    
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@6622 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/templates/default/bbpress/form-search.php
+++ src/templates/default/bbpress/form-search.php
@@ -16,7 +16,7 @@ defined( 'ABSPATH' ) || exit;
 	<div>
 		<label class="screen-reader-text hidden" for="bbp_search"><?php esc_html_e( 'Search for:', 'bbpress' ); ?></label>
 		<input type="hidden" name="action" value="bbp-search-request" />
-		<input type="text" value="<?php echo esc_attr( bbp_get_search_terms() ); ?>" name="bbp_search" id="bbp_search" />
+		<input type="text" value="<?php bbp_search_terms(); ?>" name="bbp_search" id="bbp_search" />
 		<input class="button" type="submit" id="bbp_search_submit" value="<?php esc_attr_e( 'Search', 'bbpress' ); ?>" />
 	</div>
 </form>
---
+++ src/templates/default/bbpress/form-topic-search.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Search 
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+?>
+
+<form role="search" method="get" id="bbp-search-form">
+	<div>
+		<label class="screen-reader-text hidden" for="ts"><?php esc_html_e( 'Search topics:', 'bbpress' ); ?></label>
+		<input type="text" value="<?php bbp_search_terms(); ?>" name="ts" id="ts" />
+		<input class="button" type="submit" id="bbp_search_submit" value="<?php esc_attr_e( 'Search', 'bbpress' ); ?>" />
+	</div>
+</form>
