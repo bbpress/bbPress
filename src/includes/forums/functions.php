32dd95aab334a4a4601aa6a8e6bb4f0eb6b5a5c7
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2017-08-28 02:14:53 +0000

    Common: cast return value to `array` in `bbp_pre_get_posts_normalize_forum_visibility()`.
    Trunk, for 2.6. See #3031.
    
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@6673 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/forums/functions.php
+++ src/includes/forums/functions.php
@@ -2059,7 +2059,7 @@ function bbp_pre_get_posts_normalize_forum_visibility( $posts_query = null ) {
 		}
 
 		// Get any existing meta queries
-		$meta_query   = $posts_query->get( 'meta_query', array() );
+		$meta_query   = (array) $posts_query->get( 'meta_query', array() );
 
 		// Add our meta query to existing
 		$meta_query[] = $forum_ids;
