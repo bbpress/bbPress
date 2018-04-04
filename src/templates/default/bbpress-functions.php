4b94377830724bc9bfb8674578b171a1f4334f3a
JJJ <johnjamesjacoby@git.wordpress.org>
2018-04-04 17:30:44 +0000

    Templates: revert r6641 due to reports of styling breakage.
    This reverts back to the 2.5-type behavior of always making sure bbPress styling & scripts are available on all theme-side pages.
    
    We will revisit this enhancement again in a future release.
    
    See #3138. Props robin-w.
    
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@6795 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/templates/default/bbpress-functions.php
+++ src/templates/default/bbpress-functions.php
@@ -155,7 +155,7 @@ class BBP_Default extends BBP_Theme_Compat {
 
 		// Editor scripts
 		// @see https://bbpress.trac.wordpress.org/ticket/2930
-		if ( bbp_use_wp_editor() ) {
+		if ( bbp_use_wp_editor() && is_bbpress() ) {
 			$scripts['bbpress-editor'] = array(
 				'file'         => 'js/editor' . $suffix . '.js',
 				'dependencies' => array( 'jquery' )
