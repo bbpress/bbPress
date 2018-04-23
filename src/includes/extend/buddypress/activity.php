5fb69141ed2b180da38c66d60172f2f5a9d900e6
JJJ <johnjamesjacoby@git.wordpress.org>
2018-04-23 04:05:08 +0000

    BuddyPress: prefer `empty()` check on `disable_blogforum_replies` setting.
    This fixes a bug causing some activity to not appear in relative activity streams.
    
    Fixes #3180.
    
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@6810 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/extend/buddypress/activity.php
+++ src/includes/extend/buddypress/activity.php
@@ -335,7 +335,7 @@ class BBP_BuddyPress_Activity {
 		}
 
 		// Check if blog & forum activity stream commenting is off
-		if ( ( false === $activities_template->disable_blogforum_replies ) || (int) $activities_template->disable_blogforum_replies ) {
+		if ( ! empty( $activities_template->disable_blogforum_replies ) ) {
 
 			// Get the current action name
 			$action_name = bp_get_activity_action_name();
