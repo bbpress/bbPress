ab491eff74a4e364fbfb7e0c2106df3a5d818758
JJJ <johnjamesjacoby@git.wordpress.org>
2019-06-10 23:13:57 +0000

    Tools/Repair: add missing apostrophe in `bbp_admin_repair_user_favorites()`.
    This commit allows the favorites repair tool to correctly query for chunked results.
    
    Fixes #3259.
    Props GDragoN.
    For 2.5.x.
    
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.5@6906 9866e705-20ec-0310-96e7-cbb4277adcfb

--- includes/admin/tools.php
+++ includes/admin/tools.php
@@ -637,7 +637,7 @@ function bbp_admin_repair_user_favorites() {
 			continue;
 
 		$favorites_joined = implode( ',', $favorites );
-		$values[]         = "('{$user->user_id}', '{$key}, '{$favorites_joined}')";
+		$values[]         = "('{$user->user_id}', '{$key}', '{$favorites_joined}')";
 
 		// Cleanup
 		unset( $favorites, $favorites_joined );
