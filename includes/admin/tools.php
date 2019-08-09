4a5593d99ff94f6af0ce3994257424bf3a63e461
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2019-08-09 20:52:13 +0000

    Tools/Repair: add missing `MAX()` in `bbp_admin_repair_freshness()`.
    This commit allows the freshness repair tool to correctly query for the maximum topic ID.
    
    Fixes #3261.
    Props GDragoN.
    For 2.5.x.
    
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.5@6909 9866e705-20ec-0310-96e7-cbb4277adcfb

--- includes/admin/tools.php
+++ includes/admin/tools.php
@@ -878,7 +878,7 @@ function bbp_admin_repair_freshness() {
 
 	// Now we give all the forums with topics the ID their last topic.
 	if ( is_wp_error( $wpdb->query( "INSERT INTO `$wpdb->postmeta` (`post_id`, `meta_key`, `meta_value`)
-			( SELECT `forum`.`ID`, '_bbp_last_topic_id', `topic`.`ID`
+			( SELECT `forum`.`ID`, '_bbp_last_topic_id', MAX( `topic`.`ID` )
 			FROM `$wpdb->posts` AS `forum` INNER JOIN `$wpdb->posts` AS `topic` ON `forum`.`ID` = `topic`.`post_parent`
 			WHERE `topic`.`post_status` IN ( '" . bbp_get_public_status_id() . "' ) AND `forum`.`post_type` = 'forum' AND `topic`.`post_type` = 'topic'
 			GROUP BY `forum`.`ID` );" ) ) )
