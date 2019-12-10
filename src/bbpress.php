9ea83eae05831abd96379fcc2710eeca7e11ad0c
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2019-12-10 20:13:58 +0000

    Remove alpha designation from 2.6 branch, for 2.6.3.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.6@7018 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/bbpress.php
+++ src/bbpress.php
@@ -17,7 +17,7 @@
  * Description: bbPress is forum software with a twist from the creators of WordPress.
  * Author:      The bbPress Contributors
  * Author URI:  https://bbpress.org
- * Version:     2.6.3-alpha-2
+ * Version:     2.6.3
  * Text Domain: bbpress
  * Domain Path: /languages/
  * License:     GPLv2 or later (license.txt)
@@ -203,7 +203,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.6.3-alpha-3';
+		$this->version    = '2.6.3';
 		$this->db_version = '263';
 
 		/** Paths *************************************************************/
