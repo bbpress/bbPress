ffe64b1fe179570246cef57145f5ec09fc28bdb4
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2019-12-10 15:16:51 +0000

    Engagements: stop shouting.
    This commit removes the last remaining exclamation mark from some engagements feedback.
    
    Props casiepa. Fixes #3293. For 2.6.3, branches/2.6.
    
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.6@7012 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/users/engagements.php
+++ src/includes/users/engagements.php
@@ -613,7 +613,7 @@ function bbp_favorites_handler( $action = '' ) {
 
 	// Check current user's ability to edit the user
 	} elseif ( ! current_user_can( 'edit_user', $user_id ) ) {
-		bbp_add_error( 'bbp_favorite_permission', __( '<strong>ERROR</strong>: You do not have permission to edit favorites for that user!.', 'bbpress' ) );
+		bbp_add_error( 'bbp_favorite_permission', __( '<strong>ERROR</strong>: You do not have permission to edit favorites for that user.', 'bbpress' ) );
 	}
 
 	// Bail if errors
