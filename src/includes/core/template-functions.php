2f3366fbcdd2aece64a9c4f03e5776e75292dd38
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2020-04-05 19:21:41 +0000

    Search/Rewrites: unslash search terms inside `bbp_get_search_terms()`.
    This commit ensures that search terms appear correctly inside of template output, without additional slashes.
    
    It also eliminates a few repeated calls to `bbp_get_search_rewrite_id()`, and more strictly compares rewrite rule query vars to null results for improved code clarity & consistency.
    
    Props dd32, johnjamesjacoby.
    
    See #3357. Trunk, for 2.7.0.
    
    
    
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@7076 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/core/template-functions.php
+++ src/includes/core/template-functions.php
@@ -535,13 +535,14 @@ function bbp_parse_query( $posts_query ) {
 		return;
 	}
 
-	// Get query variables
-	$bbp_view = $posts_query->get( bbp_get_view_rewrite_id() );
-	$bbp_user = $posts_query->get( bbp_get_user_rewrite_id() );
-	$is_edit  = $posts_query->get( bbp_get_edit_rewrite_id() );
+	// Get query variables (default to null if not set)
+	$bbp_view  = $posts_query->get( bbp_get_view_rewrite_id(),   null );
+	$bbp_user  = $posts_query->get( bbp_get_user_rewrite_id(),   null );
+	$is_edit   = $posts_query->get( bbp_get_edit_rewrite_id(),   null );
+	$is_search = $posts_query->get( bbp_get_search_rewrite_id(), null );
 
 	// It is a user page - We'll also check if it is user edit
-	if ( ! empty( $bbp_user ) ) {
+	if ( ! is_null( $bbp_user ) ) {
 
 		/** Find User *********************************************************/
 
@@ -572,7 +573,7 @@ function bbp_parse_query( $posts_query ) {
 		$is_engagements = $posts_query->get( bbp_get_user_engagements_rewrite_id()   );
 
 		// View or edit?
-		if ( ! empty( $is_edit ) ) {
+		if ( ! is_null( $is_edit ) ) {
 
 			// We are editing a profile
 			$posts_query->bbp_is_single_user_edit = true;
@@ -644,7 +645,7 @@ function bbp_parse_query( $posts_query ) {
 		bbpress()->displayed_user = $the_user;
 
 	// View Page
-	} elseif ( ! empty( $bbp_view ) ) {
+	} elseif ( ! is_null( $bbp_view ) ) {
 
 		// Check if the view exists by checking if there are query args are set
 		$view_args = bbp_get_view_query_args( $bbp_view );
@@ -665,7 +666,7 @@ function bbp_parse_query( $posts_query ) {
 		$posts_query->bbp_is_404 = false;
 
 	// Search Page
-	} elseif ( isset( $posts_query->query_vars[ bbp_get_search_rewrite_id() ] ) ) {
+	} elseif ( ! is_null( $is_search ) ) {
 
 		// Check if there are search query args set
 		$search_terms = bbp_get_search_terms();
@@ -683,7 +684,7 @@ function bbp_parse_query( $posts_query ) {
 		$posts_query->bbp_is_404 = false;
 
 	// Forum/Topic/Reply Edit Page
-	} elseif ( ! empty( $is_edit ) ) {
+	} elseif ( ! is_null( $is_edit ) ) {
 
 		// Get the post type from the main query loop
 		$post_type = $posts_query->get( 'post_type' );
