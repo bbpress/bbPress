c3d2203df292b30a5c95119d22275f8a746ebbc9
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2020-05-28 20:53:26 +0000

    Bump 2.6 branch to 2.6.6-alpha-1.
    Includes readme.txt updates merged from trunk in r7093.
    
    
    
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.6@7094 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/bbpress.php
+++ src/bbpress.php
@@ -17,7 +17,7 @@
  * Description:       bbPress is forum software with a twist from the creators of WordPress.
  * Author:            The bbPress Contributors
  * Author URI:        https://bbpress.org
- * Version:           2.6.5
+ * Version:           2.6.6-alpha-1
  * Text Domain:       bbpress
  * Domain Path:       /languages/
  * License:           GPLv2 or later (license.txt)
@@ -205,7 +205,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.6.5';
+		$this->version    = '2.6.6-alpha-1';
 		$this->db_version = '263';
 
 		/** Paths *************************************************************/
