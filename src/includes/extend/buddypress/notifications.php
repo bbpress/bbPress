bbpress/bbPress

commit da33e35d9a9650cfcd5238171445592600c1e660

BuddyPress: use `$reply_id` instead of `$topic_id` so `bbp_get_reply_url()` works correctly.
This change ensures that notification links go directly to the reply, not the topic, and not a 404.

Fixes #3213. See #3214.

git-svn-id: https://bbpress.svn.wordpress.org/trunk@6845 9866e705-20ec-0310-96e7-cbb4277adcfb

src/includes/extend/buddypress/notifications.php
@@ -134,7 +134,7 @@ function bbp_buddypress_add_notification( $reply_id = 0, $topic_id = 0, $forum_i
 		return;
 	}
 
-	// Get autohr information
+	// Get author information
 	$topic_author_id   = bbp_get_topic_author_id( $topic_id );
 	$secondary_item_id = $author_id;
 
@@ -146,7 +146,7 @@ function bbp_buddypress_add_notification( $reply_id = 0, $topic_id = 0, $forum_i
 	// Get some reply information
 	$args = array(
 		'user_id'          => $topic_author_id,
-		'item_id'          => $topic_id,
+		'item_id'          => $reply_id,
 		'component_name'   => bbp_get_component_name(),
 		'component_action' => 'bbp_new_reply',
 		'date_notified'    => get_post( $reply_id )->post_date,

commit 43ba7e585d74875cc5f316a2ab9dcbf144589421

BuddyPress: Mark all replies when marking topic notifications as read.
This commit fixes a regression - introduced in r6845 - that was causing marking topic notifications as read to fail. It fixes it by looping through all replies to a topic and attempting to mark them all individually. It is not a particularly optimized approach, but it does resolve the regression in such a way that accounts for both topic IDs and reply IDs.

In trunk, for 2.7.0.

Fixes #3378.

git-svn-id: https://bbpress.svn.wordpress.org/trunk@7108 9866e705-20ec-0310-96e7-cbb4277adcfb

src/includes/extend/buddypress/notifications.php
@@ -189,7 +189,10 @@ function bbp_buddypress_mark_notifications( $action = '' ) {
 
 	// Get required data
 	$user_id  = bp_loggedin_user_id();
-	$topic_id = intval( $_GET['topic_id'] );
+	$topic_id = absint( $_GET['topic_id'] );
+
+	// By default, Redirect to this topic ID
+	$redirect_id = $topic_id;
 
 	// Check nonce
 	if ( ! bbp_verify_nonce_request( 'bbp_mark_topic_' . $topic_id ) ) {
@@ -203,15 +206,38 @@ function bbp_buddypress_mark_notifications( $action = '' ) {
 	// Bail if we have errors
 	if ( ! bbp_has_errors() ) {
 
-		// Attempt to clear notifications for the current user from this topic
-		$success = bp_notifications_mark_notifications_by_item_id( $user_id, $topic_id, bbp_get_component_name(), 'bbp_new_reply' );
+		// Get these once
+		$post_type = bbp_get_reply_post_type();
+		$component = bbp_get_component_name();
+
+		// Attempt to clear notifications for this topic
+		$marked    = bp_notifications_mark_notifications_by_item_id( $user_id, $topic_id, $component, 'bbp_new_reply' );
+
+		// Get all reply IDs for the topic
+		$replies   = bbp_get_all_child_ids( $topic_id, $post_type );
+
+		// If topic has replies
+		if ( ! empty( $replies ) ) {
+
+			// Loop through each reply and attempt to mark it
+			foreach ( $replies as $reply_id ) {
+
+				// Attempt to mark notification for this reply ID
+				$marked = bp_notifications_mark_notifications_by_item_id( $user_id, $reply_id, $component, 'bbp_new_reply' );
+
+				// If marked, redirect to this reply ID
+				if ( ! empty( $marked ) ) {
+					$redirect_id = $reply_id;
+				}
+			}
+		}
 
 		// Do additional subscriptions actions
-		do_action( 'bbp_notifications_handler', $success, $user_id, $topic_id, $action );
+		do_action( 'bbp_notifications_handler', $marked, $user_id, $topic_id, $action );
 	}
 
 	// Redirect to the topic
-	$redirect = bbp_get_reply_url( $topic_id );
+	$redirect = bbp_get_reply_url( $redirect_id );
 
 	// Redirect
 	bbp_redirect( $redirect );