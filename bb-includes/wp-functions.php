e73c03bf5fa9a18a51fc51d797cb206db1496b3a
Alex Concha <xknown@git.wordpress.org>
2020-08-11 05:48:29 +0000

    Sync the _wp_filter_build_unique_id function to rWP38282
    git-svn-id: https://bbpress.svn.wordpress.org/branches/0.9@7119 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bb-includes/wp-functions.php
+++ bb-includes/wp-functions.php
@@ -883,37 +883,38 @@ function remove_action($tag, $function_to_remove, $priority = 10, $accepted_args
 }
 endif;
 
-if ( !function_exists('_wp_filter_build_unique_id') ) : // [WP6025]
+if ( !function_exists('_wp_filter_build_unique_id') ) : // [WP38282]
 function _wp_filter_build_unique_id($tag, $function, $priority = 10)
 {
 	global $wp_filter;
+	static $filter_id_count = 0;
 
-	// If function then just skip all of the tests and not overwrite the following.
-	// Static Calling
 	if( is_string($function) )
 		return $function;
 
-    if ( is_object( $function ) ) {
+	if ( is_object( $function ) ) {
         // Closures are currently implemented as objects.
         $function = array( $function, '' );
-    } else {
+	} else {
         $function = (array) $function;
     }
 
-	// Object Class Calling
-	if(is_object($function[0]) )
+	if ( is_object($function[0]) )
 	{
+		// Object Class Calling
 		if ( function_exists('spl_object_hash') ) {
 			return spl_object_hash($function[0]) . $function[1];
 		} else {
 			$obj_idx = get_class($function[0]).$function[1];
-			if( is_null($function[0]->wp_filter_id) ) {
-				$count = count((array)$wp_filter[$tag][$priority]);
-				$function[0]->wp_filter_id = $count;
-				$obj_idx .= $count;
-				unset($count);
-			} else
+			if ( !isset($function[0]->wp_filter_id) ) {
+				if ( false === $priority )
+					return false;
+				$obj_idx .= isset($wp_filter[$tag][$priority]) ? count((array)$wp_filter[$tag][$priority]) : $filter_id_count;
+				$function[0]->wp_filter_id = $filter_id_count;
+				++$filter_id_count;
+			} else {
 				$obj_idx .= $function[0]->wp_filter_id;
+			}
 			return $obj_idx;
 		}
 	}
