bbpress/bbPress

commit a32959eb34804c4da40fc4cf46b753d154bf91c1

Users: Improve UX of form-user-passwords.php template part.
This change makes sure that the "Generate Password" UI is hidden by default if the browser has JavaScript enabled, and that the traditional UI is visible if JavaScript is not enabled.

It also includes the following related improvements:

* Introduce `bbp_doing_script_debug()` and `bbp_doing_autosave()` helpers, and use them where needed
* Introduce `bbp_asset_version()` and `bbp_get_asset_version()` to bust asset cache when debugging
* Update `forum-user-passwords.php` template part to swap out body class and add missing element ID
* Update password form styling in `bbpress.css` default theme compat CSS

In trunk for 2.7.0. Fixes #3421.

git-svn-id: https://bbpress.svn.wordpress.org/trunk@7190 9866e705-20ec-0310-96e7-cbb4277adcfb

src/includes/common/template.php
@@ -967,7 +967,8 @@ function bbp_is_edit() {
  */
 function bbp_body_class( $wp_classes, $custom_classes = false ) {
 
-	$bbp_classes = array();
+	// Default classes
+	$bbp_classes = array( 'no-js' );
 
 	/** Archives **************************************************************/
 

src/includes/admin/classes/class-bbp-admin.php
@@ -815,10 +815,10 @@ class BBP_Admin {
 
 		// RTL and/or minified
 		$suffix  = is_rtl() ? '-rtl' : '';
-		$suffix .= defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+		$suffix .= bbp_doing_script_debug() ? '' : '.min';
 
 		// Get the version to use for JS
-		$version = bbp_get_version();
+		$version = bbp_get_asset_version();
 
 		// Register admin CSS with dashicons dependency
 		wp_register_style( 'bbp-admin-css', $this->css_url . 'admin' . $suffix . '.css', array( 'dashicons' ), $version );
@@ -859,10 +859,10 @@ class BBP_Admin {
 	public function register_admin_scripts() {
 
 		// Minified
-		$suffix  = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+		$suffix  = bbp_doing_script_debug() ? '' : '.min';
 
 		// Get the version to use for JS
-		$version = bbp_get_version();
+		$version = bbp_get_asset_version();
 
 		// Header JS
 		wp_register_script( 'bbp-admin-common-js',  $this->js_url . 'common'    . $suffix . '.js', array( 'jquery', 'suggest'              ), $version );

src/includes/admin/forums.php
@@ -295,7 +295,7 @@ class BBP_Forums_Admin {
 	public function save_meta_boxes( $forum_id ) {
 
 		// Bail if doing an autosave
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		if ( bbp_doing_autosave() ) {
 			return $forum_id;
 		}
 

src/includes/admin/replies.php
@@ -389,7 +389,7 @@ class BBP_Replies_Admin {
 	public function save_meta_boxes( $reply_id ) {
 
 		// Bail if doing an autosave
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		if ( bbp_doing_autosave() ) {
 			return $reply_id;
 		}
 

src/includes/admin/topics.php
@@ -508,7 +508,7 @@ class BBP_Topics_Admin {
 	public function save_meta_boxes( $topic_id ) {
 
 		// Bail if doing an autosave
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		if ( bbp_doing_autosave() ) {
 			return $topic_id;
 		}
 

src/includes/core/functions.php
@@ -32,6 +32,27 @@ function bbp_version() {
 	}
 
 /**
+ * Output the bbPress asset version
+ *
+ * @since 2.6.7 bbPress (r7188)
+ */
+function bbp_asset_version() {
+	echo bbp_get_asset_version();
+}
+	/**
+	 * Return the bbPress asset version
+	 *
+	 * @since 2.6.7 bbPress (r7188)
+	 *
+	 * @retrun string The bbPress asset version
+	 */
+	function bbp_get_asset_version() {
+		return bbp_doing_script_debug()
+			? (string) time()
+			: bbp_get_version();
+	}
+
+/**
  * Output the bbPress database version
  *
  * @since 2.0.0 bbPress (r3468)
@@ -654,4 +675,28 @@ function bbp_redirect( $location = '', $status = 302 ) {
 
 	// Exit so the redirect takes place immediately
 	exit();
+}
+
+/** Global Helpers ************************************************************/
+
+/**
+ * Return if debugging scripts or not
+ *
+ * @since 2.6.7 (r7188)
+ *
+ * @return bool True if debugging scripts. False if not debugging scripts.
+ */
+function bbp_doing_script_debug() {
+	return defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG;
+}
+
+/**
+ * Return if auto-saving or not
+ *
+ * @since 2.6.7 (r7188)
+ *
+ * @return bool True if mid auto-save. False if not mid auto-save.
+ */
+function bbp_doing_autosave() {
+	return defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE;
 }

src/includes/core/template-functions.php
@@ -140,7 +140,7 @@ function bbp_locate_enqueueable( $file = '' ) {
 		: str_replace( '.min', '', $file );
 
 	// Are we debugging?
-	$script_debug = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG;
+	$script_debug = bbp_doing_script_debug();
 
 	// Debugging, so prefer unminified files
 	if ( true === $script_debug ) {
@@ -217,7 +217,7 @@ function bbp_enqueue_style( $handle = '', $file = '', $deps = array(), $ver = fa
 
 		// Make sure there is always a version
 		if ( empty( $ver ) ) {
-			$ver = bbp_get_version();
+			$ver = bbp_get_asset_version();
 		}
 
 		// Make path to file relative to site URL
@@ -261,7 +261,7 @@ function bbp_enqueue_script( $handle = '', $file = '', $deps = array(), $ver = f
 
 		// Make sure there is always a version
 		if ( empty( $ver ) ) {
-			$ver = bbp_get_version();
+			$ver = bbp_get_asset_version();
 		}
 
 		// Make path to file relative to site URL

src/templates/default/bbpress-functions.php
@@ -46,7 +46,7 @@ class BBP_Default extends BBP_Theme_Compat {
 		parent::__construct( bbp_parse_args( $properties, array(
 			'id'      => 'default',
 			'name'    => 'bbPress Default',
-			'version' => bbp_get_version(),
+			'version' => bbp_get_asset_version(),
 			'dir'     => trailingslashit( bbpress()->themes_dir . 'default' ),
 			'url'     => trailingslashit( bbpress()->themes_url . 'default' ),
 		), 'default_theme' ) );

commit 1ebdd60ef76b2f2f0a594397c0398e5292b1e2c2

Appearance: only add `no-js` body class on bbPress pages.
This change addresses a regression introduced in r7190, causing the `bbpress` body class to unintentionally be added to all pages triggering layout issues in some WordPress themes.

This commit makes sure that the `bbpress` body class is only added when intended, and moves the `no-js` body class into the same conditional check.

In trunk for 2.7. See #3443.

git-svn-id: https://bbpress.svn.wordpress.org/trunk@7220 9866e705-20ec-0310-96e7-cbb4277adcfb

src/includes/common/template.php
@@ -965,10 +965,10 @@ function bbp_is_edit() {
  * @param array $custom_classes
  * @return array Body Classes
  */
-function bbp_body_class( $wp_classes, $custom_classes = false ) {
+function bbp_body_class( $wp_classes = array(), $custom_classes = false ) {
 
-	// Default classes
-	$bbp_classes = array( 'no-js' );
+	// Default to no classes
+	$bbp_classes = array();
 
 	/** Archives **************************************************************/
 
@@ -1077,12 +1077,15 @@ function bbp_body_class( $wp_classes, $custom_classes = false ) {
 		$bbp_classes[] = 'bbp-shortcode';
 	}
 
-	/** Clean up **************************************************************/
+	/** General ***************************************************************/
 
-	// Add bbPress class if we are within a bbPress page
+	// Any page with bbPress content
 	if ( ! empty( $bbp_classes ) ) {
 		$bbp_classes[] = 'bbpress';
+		$bbp_classes[] = 'no-js';
 	}
+
+	/** Clean up **************************************************************/
 
 	// Merge WP classes with bbPress classes and remove any duplicates
 	$classes = array_unique( array_merge( (array) $bbp_classes, (array) $wp_classes ) );