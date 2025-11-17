ae9d67378386d45fec34bc0b4018174e9d80afa4
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2025-11-17 00:30:54 +0000

    Theme Compatibility: Make sure topic archive titles use the WordPress Page title, if one exists.
    This commit modifies `bbp_template_include_theme_compat()` to use the existing `$new_title` variable logic instead of the hard-coded Topic Archive title, allowing for the Page title to take its place.
    
    Props rollybueno.
    
    In trunk, for 2.7.
    
    Fixes #3604.
    
    git-svn-id: https://bbpress.svn.wordpress.org/trunk@7363 9866e705-20ec-0310-96e7-cbb4277adcfb

--- src/includes/core/theme-compat.php
+++ src/includes/core/theme-compat.php
@@ -641,13 +641,13 @@ function bbp_template_include_theme_compat( $template = '' ) {
 
 		// ...or use the existing page title?
 		} else {
-			$new_title = apply_filters( 'the_title',   $page->post_title   );
+			$new_title = apply_filters( 'the_title', $page->post_title, $page->ID );
 		}
 
 		// Reset post
 		bbp_theme_compat_reset_post( array(
 			'ID'             => ! empty( $page->ID ) ? $page->ID : 0,
-			'post_title'     => bbp_get_topic_archive_title(),
+			'post_title'     => $new_title,
 			'post_author'    => 0,
 			'post_date'      => bbp_get_empty_datetime(),
 			'post_content'   => $new_content,
