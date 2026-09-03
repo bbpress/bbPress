803955c1d3497d7850beefbb8fcf3235f71b5837
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2026-09-03 17:12:31 +0000

    Bump 2.6 branch to 2.6.15, and WP tested-up-to to 7.1.
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.6@7416 9866e705-20ec-0310-96e7-cbb4277adcfb

--- bbpress.php
+++ bbpress.php
@@ -23,8 +23,8 @@
  * Domain Path:       /languages/
  * Requires PHP:      5.6.20
  * Requires at least: 6.0
- * Tested up to:      6.9
- * Version:           2.6.14
+ * Tested up to:      7.1
+ * Version:           2.6.15
  */
 
 // Exit if accessed directly
--- src/bbpress.php
+++ src/bbpress.php
@@ -23,8 +23,8 @@
  * Domain Path:       /languages/
  * Requires PHP:      5.6.20
  * Requires at least: 6.0
- * Tested up to:      6.9
- * Version:           2.6.14
+ * Tested up to:      7.1
+ * Version:           2.6.15
  */
 
 // Exit if accessed directly
@@ -207,7 +207,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.6.14';
+		$this->version    = '2.6.15';
 		$this->db_version = '263';
 
 		/** Paths *************************************************************/
