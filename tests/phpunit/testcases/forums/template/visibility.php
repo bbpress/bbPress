490304eb358a50de644d687d3706c5a3d342cdeb
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2026-09-10 00:55:12 +0000

    Forums: Enforce inherited forum visibility.
    Forum descendants inherit the most restrictive visibility in their ancestor chain. This change resolves the source forum before displaying aggregate freshness and author links, preventing private or hidden descendant details from appearing through a public parent while preserving them for users who can read every restricted forum in the hierarchy.
    
    In 2.6, for 2.6.16.
    
    Props im_rootkid.
    
    
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.6@7470 9866e705-20ec-0310-96e7-cbb4277adcfb

--- tests/phpunit/testcases/forums/template/visibility.php
+++ tests/phpunit/testcases/forums/template/visibility.php
@@ -59,23 +59,123 @@ class BBP_Tests_Forums_Template_Visibility extends BBP_UnitTestCase {
 
 	/**
 	 * @covers ::bbp_suppress_private_forum_meta
-	 * @todo   Implement test_bbp_suppress_private_forum_meta().
 	 */
 	public function test_bbp_suppress_private_forum_meta() {
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+		$public_forum = $this->factory->forum->create();
+		$keymaster    = $this->factory->user->create( array(
+			'role' => bbp_get_keymaster_role(),
+		) );
+
+		foreach ( array( bbp_get_private_status_id(), bbp_get_hidden_status_id() ) as $status ) {
+			$restricted_forum = $this->factory->forum->create( array(
+				'post_parent' => $public_forum,
+				'post_status' => $status,
+			) );
+			$topic            = $this->factory->topic->create( array(
+				'post_parent' => $restricted_forum,
+				'topic_meta'  => array(
+					'forum_id' => $restricted_forum,
+				),
+			) );
+			$reply            = $this->factory->reply->create( array(
+				'post_parent' => $topic,
+				'post_title'  => 'Restricted reply title',
+				'reply_meta'  => array(
+					'forum_id' => $restricted_forum,
+					'topic_id' => $topic,
+				),
+			) );
+
+			bbp_update_forum_last_reply_id( $public_forum, $reply );
+			bbp_update_forum_last_active_id( $public_forum, $reply );
+			bbp_update_forum_last_active_time( $public_forum, get_post_field( 'post_date', $reply ) );
+
+			wp_set_current_user( 0 );
+			$this->assertSame( '-', bbp_get_forum_freshness_link( $public_forum ) );
+
+			wp_set_current_user( $keymaster );
+			$this->assertStringContainsString( 'Restricted reply title', bbp_get_forum_freshness_link( $public_forum ) );
+		}
+	}
+
+	/**
+	 * @covers ::bbp_suppress_private_forum_meta
+	 * @covers ::bbp_suppress_private_author_link
+	 */
+	public function test_bbp_suppress_private_forum_meta_inherits_ancestor_visibility() {
+		$public_forum = $this->factory->forum->create();
+		$keymaster    = $this->factory->user->create( array(
+			'role' => bbp_get_keymaster_role(),
+		) );
+
+		foreach ( array( bbp_get_private_status_id(), bbp_get_hidden_status_id() ) as $status ) {
+			$restricted_parent = $this->factory->forum->create( array(
+				'post_parent' => $public_forum,
+				'post_status' => $status,
+			) );
+			$public_child      = $this->factory->forum->create( array(
+				'post_parent' => $restricted_parent,
+			) );
+			$topic             = $this->factory->topic->create( array(
+				'post_parent' => $public_child,
+				'topic_meta'  => array(
+					'forum_id' => $public_child,
+				),
+			) );
+			$reply             = $this->factory->reply->create( array(
+				'post_parent' => $topic,
+				'post_title'  => 'Inherited restricted reply title',
+				'reply_meta'  => array(
+					'forum_id' => $public_child,
+					'topic_id' => $topic,
+				),
+			) );
+
+			bbp_update_forum_last_reply_id( $public_forum, $reply );
+			bbp_update_forum_last_active_id( $public_forum, $reply );
+			bbp_update_forum_last_active_time( $public_forum, get_post_field( 'post_date', $reply ) );
+
+			wp_set_current_user( 0 );
+			$this->assertSame( '-', bbp_get_forum_freshness_link( $public_forum ) );
+			$this->assertSame( '', bbp_get_author_link( $reply ) );
+
+			wp_set_current_user( $keymaster );
+			$this->assertStringContainsString( 'Inherited restricted reply title', bbp_get_forum_freshness_link( $public_forum ) );
+			$this->assertNotSame( '', bbp_get_author_link( $reply ) );
+		}
 	}
 
 	/**
 	 * @covers ::bbp_suppress_private_author_link
-	 * @todo   Implement test_bbp_suppress_private_author_link().
 	 */
 	public function test_bbp_suppress_private_author_link() {
-		// Remove the following lines when you implement this test.
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+		$keymaster = $this->factory->user->create( array(
+			'role' => bbp_get_keymaster_role(),
+		) );
+
+		foreach ( array( bbp_get_private_status_id(), bbp_get_hidden_status_id() ) as $status ) {
+			$restricted_forum = $this->factory->forum->create( array(
+				'post_status' => $status,
+			) );
+			$topic            = $this->factory->topic->create( array(
+				'post_parent' => $restricted_forum,
+				'topic_meta'  => array(
+					'forum_id' => $restricted_forum,
+				),
+			) );
+			$reply            = $this->factory->reply->create( array(
+				'post_parent' => $topic,
+				'reply_meta'  => array(
+					'forum_id' => $restricted_forum,
+					'topic_id' => $topic,
+				),
+			) );
+
+			wp_set_current_user( 0 );
+			$this->assertSame( '', bbp_get_author_link( $reply ) );
+
+			wp_set_current_user( $keymaster );
+			$this->assertNotSame( '', bbp_get_author_link( $reply ) );
+		}
 	}
 }
