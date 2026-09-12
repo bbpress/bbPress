15dd43fde83ff29bf5e06eb81f0e9099b80355c1
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2026-09-12 21:16:45 +0000

    BuddyPress Groups: Enforce restricted forum visibility.
    Apply BuddyPress group membership checks to private and hidden Group Forums outside group requests, including feeds and aggregate queries. Preserve ancestor restrictions, inactive-user denials, moderator filters, subscription checks, and current-group object boundaries.
    
    This is a backport of [7495] to the 2.6 branch.
    
    In branches/2.6, for 2.6.16.
    
    Props djsplatt.
    
    
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.6@7496 9866e705-20ec-0310-96e7-cbb4277adcfb

--- tests/phpunit/testcases/extend/buddypress/groups.php
+++ tests/phpunit/testcases/extend/buddypress/groups.php
@@ -13,6 +13,7 @@ class BBP_Tests_Extend_BuddyPress_Groups extends BBP_UnitTestCase {
 	protected $old_request;
 	protected $old_server;
 	protected $old_errors;
+	protected $old_allow_forum_mods;
 	protected $group_extension;
 
 	public function setUp(): void {
@@ -22,6 +23,7 @@ class BBP_Tests_Extend_BuddyPress_Groups extends BBP_UnitTestCase {
 		$this->old_request = $_REQUEST;
 		$this->old_server  = $_SERVER;
 		$this->old_errors  = bbpress()->errors;
+		$this->old_allow_forum_mods = get_option( '_bbp_allow_forum_mods', null );
 	}
 
 	public function tearDown(): void {
@@ -30,9 +32,17 @@ class BBP_Tests_Extend_BuddyPress_Groups extends BBP_UnitTestCase {
 		$_SERVER          = $this->old_server;
 		bbpress()->errors = $this->old_errors;
 
+		if ( null === $this->old_allow_forum_mods ) {
+			delete_option( '_bbp_allow_forum_mods' );
+		} else {
+			update_option( '_bbp_allow_forum_mods', $this->old_allow_forum_mods );
+		}
+
 		if ( isset( $this->group_extension ) ) {
 			remove_filter( 'bbp_map_meta_caps', array( $this->group_extension, 'map_group_forum_meta_caps' ), 10 );
 			remove_filter( 'bbp_map_meta_caps', array( $this->group_extension, 'map_group_forum_meta_caps' ), 99 );
+			remove_filter( 'bbp_map_meta_caps', array( $this->group_extension, 'map_group_forum_read_meta_caps' ), 20 );
+			remove_filter( 'bbp_get_excluded_forum_ids', array( $this->group_extension, 'exclude_group_forum_ids' ), 20 );
 			remove_filter( 'bbp_subscription_user_can_view_forum', array( $this->group_extension, 'subscription_user_can_view_forum' ), 10 );
 		}
 
@@ -42,6 +52,213 @@ class BBP_Tests_Extend_BuddyPress_Groups extends BBP_UnitTestCase {
 		buddypress()->current_action    = '';
 
 		parent::tearDown();
+	}
+
+	/**
+	 * @covers ::BBP_Forums_Group_Extension::user_can_view_group_forum
+	 * @covers ::BBP_Forums_Group_Extension::map_group_forum_read_meta_caps
+	 * @covers ::BBP_Forums_Group_Extension::exclude_group_forum_ids
+	 */
+	public function test_restricted_group_forum_access_outside_group_context() {
+		$creator_id        = $this->factory->user->create();
+		$member_id         = $this->factory->user->create();
+		$banned_id         = $this->factory->user->create();
+		$outsider_id       = $this->factory->user->create();
+		$moderator_id      = $this->factory->user->create();
+		$forum_mod_id      = $this->factory->user->create();
+		$group_mod_id      = $this->factory->user->create();
+		$former_id         = $this->factory->user->create();
+		$other_member_id   = $this->factory->user->create();
+		$group_id          = $this->bp_factory->group->create( array( 'creator_id' => $creator_id ) );
+		$other_group_id    = $this->bp_factory->group->create();
+		$private_forum_id  = $this->factory->forum->create();
+		$hidden_forum_id   = $this->factory->forum->create();
+		$normal_forum_id   = $this->factory->forum->create();
+		$private_normal_id = $this->factory->forum->create();
+
+		foreach ( array( $creator_id, $member_id, $banned_id, $outsider_id, $forum_mod_id, $group_mod_id, $former_id, $other_member_id ) as $user_id ) {
+			bbp_set_user_role( $user_id, bbp_get_participant_role() );
+		}
+		bbp_set_user_role( $moderator_id, bbp_get_moderator_role() );
+
+		groups_join_group( $group_id, $member_id );
+		groups_join_group( $group_id, $banned_id );
+		groups_join_group( $group_id, $group_mod_id );
+		groups_promote_member( $group_mod_id, $group_id, 'mod', $creator_id );
+		groups_join_group( $group_id, $former_id );
+		groups_leave_group( $group_id, $former_id );
+		groups_join_group( $other_group_id, $other_member_id );
+		groups_ban_member( $banned_id, $group_id, $creator_id );
+		$this->attach_forum_to_group( $private_forum_id, $group_id );
+		$this->attach_forum_to_group( $private_forum_id, $other_group_id );
+		$this->attach_forum_to_group( $hidden_forum_id, $group_id );
+		bbp_privatize_forum( $private_forum_id );
+		bbp_hide_forum( $hidden_forum_id );
+		bbp_privatize_forum( $private_normal_id );
+		bbp_add_moderator( $private_forum_id, $forum_mod_id );
+
+		$this->group_extension = new BBP_Forums_Group_Extension();
+
+		$this->assertTrue( user_can( $creator_id, 'read_forum', $private_forum_id ) );
+		$this->assertTrue( user_can( $member_id, 'read_forum', $private_forum_id ) );
+		$this->assertTrue( user_can( $member_id, 'read_forum', $hidden_forum_id ) );
+		$this->assertTrue( user_can( $moderator_id, 'read_forum', $private_forum_id ) );
+		$this->assertTrue( user_can( $forum_mod_id, 'read_forum', $private_forum_id ) );
+		$this->assertTrue( user_can( $group_mod_id, 'read_forum', $private_forum_id ) );
+		$this->assertTrue( user_can( $other_member_id, 'read_forum', $private_forum_id ) );
+		$this->assertFalse( user_can( $banned_id, 'read_forum', $private_forum_id ) );
+		$this->assertFalse( user_can( $former_id, 'read_forum', $private_forum_id ) );
+		$this->assertFalse( user_can( $outsider_id, 'read_forum', $private_forum_id ) );
+		$this->assertFalse( user_can( 0, 'read_forum', $private_forum_id ) );
+		$this->assertTrue( user_can( $outsider_id, 'read_forum', $normal_forum_id ) );
+		$this->assertTrue( user_can( $outsider_id, 'read_forum', $private_normal_id ) );
+
+		$this->assertTrue( bbp_user_can_view_forum( array( 'user_id' => $member_id, 'forum_id' => $private_forum_id ) ) );
+		$this->assertFalse( bbp_user_can_view_forum( array( 'user_id' => $banned_id, 'forum_id' => $private_forum_id ) ) );
+		$this->assertFalse( bbp_user_can_view_forum( array( 'user_id' => $outsider_id, 'forum_id' => $private_forum_id ) ) );
+
+		$this->set_current_user( $member_id );
+		$this->assertNotContains( $private_forum_id, bbp_get_excluded_forum_ids() );
+		$this->assertNotContains( $hidden_forum_id, bbp_get_excluded_forum_ids() );
+
+		$this->set_current_user( $outsider_id );
+		$this->assertContains( $private_forum_id, bbp_get_excluded_forum_ids() );
+		$this->assertContains( $hidden_forum_id, bbp_get_excluded_forum_ids() );
+		$this->assertNotContains( $private_normal_id, bbp_get_excluded_forum_ids() );
+
+		$this->set_current_user( $banned_id );
+		$this->assertContains( $private_forum_id, bbp_get_excluded_forum_ids() );
+		$this->assertContains( $hidden_forum_id, bbp_get_excluded_forum_ids() );
+	}
+
+	/**
+	 * @covers ::BBP_Forums_Group_Extension::map_group_forum_read_meta_caps
+	 * @covers ::BBP_Forums_Group_Extension::exclude_group_forum_ids
+	 * @covers ::BBP_Forums_Group_Extension::subscription_user_can_view_forum
+	 */
+	public function test_group_forum_access_preserves_ancestor_restrictions() {
+		$creator_id       = $this->factory->user->create();
+		$member_id        = $this->factory->user->create();
+		$group_id         = $this->bp_factory->group->create( array( 'creator_id' => $creator_id ) );
+		$other_group_id   = $this->bp_factory->group->create();
+		$hidden_parent_id = $this->factory->forum->create();
+		$denied_forum_id  = $this->factory->forum->create( array( 'post_parent' => $hidden_parent_id ) );
+		$group_forum_id   = $this->factory->forum->create();
+		$other_forum_id   = $this->factory->forum->create();
+		$public_child_id  = $this->factory->forum->create( array( 'post_parent' => $group_forum_id ) );
+		$public_grandchild_id = $this->factory->forum->create( array( 'post_parent' => $public_child_id ) );
+		$denied_topic_id  = $this->factory->topic->create( array( 'post_parent' => $denied_forum_id ) );
+		$allowed_topic_id = $this->factory->topic->create( array( 'post_parent' => $public_grandchild_id ) );
+
+		bbp_set_user_role( $creator_id, bbp_get_participant_role() );
+		bbp_set_user_role( $member_id, bbp_get_participant_role() );
+		groups_join_group( $group_id, $member_id );
+		$this->attach_forum_to_group( $denied_forum_id, $group_id );
+		$this->attach_forum_to_group( $group_forum_id, $group_id );
+		$this->attach_forum_to_group( $other_forum_id, $other_group_id );
+		bbp_hide_forum( $hidden_parent_id );
+		bbp_privatize_forum( $denied_forum_id );
+		bbp_privatize_forum( $group_forum_id );
+		bbp_privatize_forum( $other_forum_id );
+
+		$this->set_group_context( $group_id, $member_id );
+
+		$this->assertNotContains( $hidden_parent_id, bbp_get_group_forum_ids() );
+		$this->assertFalse( user_can( $member_id, 'read_forum', $hidden_parent_id ) );
+		$this->assertFalse( user_can( $member_id, 'read_forum', $denied_forum_id ) );
+		$this->assertFalse( user_can( $member_id, 'read_forum', $other_forum_id ) );
+		$this->assertTrue( user_can( $member_id, 'read_forum', $group_forum_id ) );
+		$this->assertTrue( user_can( $member_id, 'read_forum', $public_child_id ) );
+		$this->assertTrue( user_can( $member_id, 'read_forum', $public_grandchild_id ) );
+		$this->assertSame(
+			array(),
+			array_values( bbp_filter_subscription_user_ids( array( $member_id ), $denied_forum_id, $denied_topic_id ) )
+		);
+
+		$this->set_current_user( $member_id );
+		$excluded = bbp_get_excluded_forum_ids();
+		$this->assertContains( $hidden_parent_id, $excluded );
+		$this->assertContains( $denied_forum_id, $excluded );
+		$this->assertNotContains( $group_forum_id, $excluded );
+		$this->assertNotContains( $public_child_id, $excluded );
+		$this->assertNotContains( $public_grandchild_id, $excluded );
+
+		bbp_has_topics(
+			array(
+				'post_parent'    => 'any',
+				'posts_per_page' => -1,
+				'show_stickies'  => false,
+			)
+		);
+		$topic_ids = wp_list_pluck( bbpress()->topic_query->posts, 'ID' );
+
+		$this->assertContains( $allowed_topic_id, $topic_ids );
+		$this->assertNotContains( $denied_topic_id, $topic_ids );
+	}
+
+	/**
+	 * @covers ::BBP_Forums_Group_Extension::map_group_forum_meta_caps
+	 */
+	public function test_group_context_does_not_map_caps_for_another_group_forum() {
+		$creator_id       = $this->factory->user->create();
+		$group_mod_id     = $this->factory->user->create();
+		$group_id         = $this->bp_factory->group->create( array( 'creator_id' => $creator_id ) );
+		$other_group_id   = $this->bp_factory->group->create();
+		$group_forum_id   = $this->factory->forum->create();
+		$other_forum_id   = $this->factory->forum->create();
+
+		bbp_set_user_role( $creator_id, bbp_get_participant_role() );
+		bbp_set_user_role( $group_mod_id, bbp_get_participant_role() );
+		groups_join_group( $group_id, $group_mod_id );
+		groups_promote_member( $group_mod_id, $group_id, 'mod', $creator_id );
+		$this->attach_forum_to_group( $group_forum_id, $group_id );
+		$this->attach_forum_to_group( $other_forum_id, $other_group_id );
+		bbp_privatize_forum( $group_forum_id );
+		bbp_privatize_forum( $other_forum_id );
+		$this->set_group_context( $group_id, $group_mod_id );
+
+		$this->assertTrue( bbp_group_is_mod() );
+		$this->assertTrue( user_can( $group_mod_id, 'read_forum', $group_forum_id ) );
+		$this->assertFalse( user_can( $group_mod_id, 'read_forum', $other_forum_id ) );
+		$this->assertFalse( user_can( $group_mod_id, 'moderate', $other_forum_id ) );
+	}
+
+	/**
+	 * @covers ::BBP_Forums_Group_Extension::user_can_view_group_forum
+	 * @covers ::BBP_Forums_Group_Extension::map_group_forum_read_meta_caps
+	 */
+	public function test_group_forum_access_preserves_moderator_and_inactive_user_semantics() {
+		$creator_id       = $this->factory->user->create();
+		$stale_mod_id     = $this->factory->user->create();
+		$filtered_mod_id  = $this->factory->user->create();
+		$custom_mod_id    = $this->factory->user->create();
+		$inactive_user_id = $this->factory->user->create();
+		$group_id         = $this->bp_factory->group->create( array( 'creator_id' => $creator_id ) );
+		$forum_id         = $this->factory->forum->create();
+
+		foreach ( array( $creator_id, $stale_mod_id, $filtered_mod_id, $custom_mod_id ) as $user_id ) {
+			bbp_set_user_role( $user_id, bbp_get_participant_role() );
+		}
+		bbp_set_user_role( $inactive_user_id, bbp_get_blocked_role() );
+		groups_join_group( $group_id, $inactive_user_id );
+		$this->attach_forum_to_group( $forum_id, $group_id );
+		bbp_privatize_forum( $forum_id );
+		bbp_add_moderator( $forum_id, $stale_mod_id );
+		get_user_by( 'id', $custom_mod_id )->add_cap( 'moderate' );
+		update_option( '_bbp_allow_forum_mods', 0 );
+
+		$filter_moderator = function( $retval, $user_id, $forum_id ) use ( $filtered_mod_id ) {
+			return ( $filtered_mod_id === $user_id ) ? true : $retval;
+		};
+		add_filter( 'bbp_is_user_forum_moderator', $filter_moderator, 10, 3 );
+		$this->group_extension = new BBP_Forums_Group_Extension();
+
+		$this->assertFalse( user_can( $stale_mod_id, 'read_forum', $forum_id ) );
+		$this->assertTrue( user_can( $filtered_mod_id, 'read_forum', $forum_id ) );
+		$this->assertTrue( user_can( $custom_mod_id, 'read_forum', $forum_id ) );
+		$this->assertFalse( user_can( $inactive_user_id, 'read_forum', $forum_id ) );
+
+		remove_filter( 'bbp_is_user_forum_moderator', $filter_moderator, 10 );
 	}
 
 	protected function set_group_context( $group_id, $user_id ) {
@@ -115,6 +332,7 @@ class BBP_Tests_Extend_BuddyPress_Groups extends BBP_UnitTestCase {
 		$banned_id        = $this->factory->user->create();
 		$former_id        = $this->factory->user->create();
 		$moderator_id     = $this->factory->user->create();
+		$spectator_id     = $this->factory->user->create();
 		$other_creator_id = $this->factory->user->create();
 		$other_member_id  = $this->factory->user->create();
 		$group_id         = $this->bp_factory->group->create( array( 'creator_id' => $creator_id ) );
@@ -131,16 +349,21 @@ class BBP_Tests_Extend_BuddyPress_Groups extends BBP_UnitTestCase {
 		bbp_set_user_role( $banned_id, bbp_get_participant_role() );
 		bbp_set_user_role( $former_id, bbp_get_participant_role() );
 		bbp_set_user_role( $moderator_id, bbp_get_moderator_role() );
+		bbp_set_user_role( $spectator_id, bbp_get_spectator_role() );
 		bbp_set_user_role( $other_creator_id, bbp_get_participant_role() );
 		bbp_set_user_role( $other_member_id, bbp_get_participant_role() );
 
 		groups_join_group( $group_id, $member_id );
 		groups_join_group( $group_id, $banned_id );
+		groups_join_group( $group_id, $spectator_id );
 		groups_ban_member( $banned_id, $group_id, $creator_id );
 		groups_join_group( $other_group_id, $other_member_id );
 		$this->attach_forum_to_group( $forum_id, $group_id );
 		$this->attach_forum_to_group( $other_forum_id, $other_group_id );
 		$this->set_group_context( $group_id, $creator_id );
+
+		$this->assertFalse( $this->group_extension->user_can_view_group_forum( $member_id, $other_forum_id ) );
+		$this->assertFalse( user_can( $member_id, 'read_forum', $other_forum_id ) );
 
 		$this->assertSame(
 			array( $other_member_id ),
@@ -149,7 +372,7 @@ class BBP_Tests_Extend_BuddyPress_Groups extends BBP_UnitTestCase {
 
 		$this->assertSame(
 			array( $creator_id, $member_id, $moderator_id ),
-			array_values( bbp_filter_subscription_user_ids( array( $creator_id, $member_id, $banned_id, $former_id, $moderator_id ), $forum_id ) )
+			array_values( bbp_filter_subscription_user_ids( array( $creator_id, $member_id, $banned_id, $former_id, $moderator_id, $spectator_id ), $forum_id ) )
 		);
 
 		unset( buddypress()->groups->current_group );
@@ -159,7 +382,7 @@ class BBP_Tests_Extend_BuddyPress_Groups extends BBP_UnitTestCase {
 
 		$this->assertSame(
 			array( $creator_id, $member_id, $moderator_id ),
-			array_values( bbp_filter_subscription_user_ids( array( $creator_id, $member_id, $banned_id, $former_id, $moderator_id ), $forum_id ) )
+			array_values( bbp_filter_subscription_user_ids( array( $creator_id, $member_id, $banned_id, $former_id, $moderator_id, $spectator_id ), $forum_id ) )
 		);
 	}
 
