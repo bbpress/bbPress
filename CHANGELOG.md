46b8f1a1da3fe5093108b22b1a36a9e00b76c160
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2026-09-16 04:41:42 +0000

    Build: Prepare bbPress 2.6.16.
    Update package and runtime versions, the stable tag, and the changelog for the 2.6.16 release.
    
    In branches/2.6, for 2.6.16.
    
    
    git-svn-id: https://bbpress.svn.wordpress.org/branches/2.6@7520 9866e705-20ec-0310-96e7-cbb4277adcfb

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,8 @@ a WordPress plugin. Earlier standalone releases are preserved in the
 Development for the next bbPress release is in progress. See the active
 [Trac milestones](https://bbpress.trac.wordpress.org/roadmap) for planned work.
 
+## 2.6.16 - 2026-09-15
+
 ### Added
 
 - Added a populated WordPress Playground preview for the Plugin Directory.
--- src/bbpress.php
+++ src/bbpress.php
@@ -24,7 +24,7 @@
  * Requires PHP:      5.6.20
  * Requires at least: 6.0
  * Tested up to:      7.1
- * Version:           2.6.15
+ * Version:           2.6.16
  */
 
 // Exit if accessed directly
@@ -224,7 +224,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.6.15';
+		$this->version    = '2.6.16';
 		$this->db_version = '263';
 
 		/** Paths *************************************************************/
